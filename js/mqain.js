7e1279eaea2e5bca727a0ec296cc540d6c07faae
VladimirEG <ev.0726,a@gmail.com>
2022-08-13 16:30:13 +0300

    gsd

--- js/mqain.js
+++ js/mqain.js
@@ -11,7 +11,8 @@ function calc(a, b, action) {
 		case '**':
 			return a ** b
 		case '%':
-
+			return a % b
+		case '%':
 	}
 }
 
@@ -19,4 +20,5 @@ console.log(calc(3, 1, '+'))
 console.log(calc(3, 1, '-'))
 console.log(calc(3, 1, '*'))
 console.log(calc(3, 1, '/'))
-console.log(calc(3, 1, '**'))
+console.log(calc(7, 4, '**'))
+console.log(calc(7, 4, '%'))

7ac303a9289190130a9096d198da681a89a4da2b
AlexBerdTMN <colttmn@gmail.com>
2022-08-15 21:03:29 +0500

    first

---
+++ AlexBerdTMN/script.js
@@ -0,0 +1,9 @@
+function calc(op, a, b){
+    switch(op){
+        case '+': return (+a) + (+b);
+        case '-': return a - b;
+        case '*': return a * b;
+        default: return 'Вы ввели неправельный оператор';
+    }
+}
+console.log(calc('+', 12, 26));

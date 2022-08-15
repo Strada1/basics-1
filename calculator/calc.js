ed86f0716898146a46c2348e5b3240019b4eee56
pipupopi <sonalavrushin@gmail.com>
2022-08-15 20:04:48 +0300

    теперь работает лучше

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,18 +1,15 @@
-function calc(komanda, a, b) {
-    switch (komanda) {
-      case "add":
-        console.log(a + b);
-        break;
-  
-      case "multi":
-        console.log(a - b);
-        break;
-      case "subtract":
-        console.log(a * b);
-        break;
-    }
+function calc(action, a, b) {
+  switch (action) {
+    case "add":
+      return (a + b);
+
+    case "multi":
+      return (a - b);
+
+    case "subtract":
+      return (a * b);
   }
-  console.log(calc("add", 5, 2));
-  console.log(calc("multi", 10, 5));
-  console.log(calc("subtract", 6, 3));
-  
+}
+console.log(calc("add", 5, 2));
+console.log(calc("multi", 10, 5));
+console.log(calc("subtract", 6, 3));

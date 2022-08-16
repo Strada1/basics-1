861de4c6ae1a878d8d8b1751ff625fcb39dd6354
Nikita <ifyoucan1337@yandex.ru>
2022-08-17 00:16:05 +0300

    fixed

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 function calc(operator, a, b) {
 	switch (operator) {
-		case 'add':
-			return a + b
-		case 'multi':
-			return a * b
-		case 'subtract':
-			return a - b
+		case "add":
+			return a + b;
+		case "multi":
+			return a * b;
+		case "subtract":
+			return a - b;
 		default:
-			console.log('Ошибка')
+			console.log("Ошибка");
 	}
 }
-calc('add', 3, 2)
+calc("add", 3, 2);

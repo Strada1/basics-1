b01d49c8046d6e38ae688ae1bec938f322b1466c
Dmitry Grishuk <dmitrygrishuk@gmail.com>
2022-08-16 22:36:21 +0800

    Add object Operations for function Calc

--- dgrishuk/main.js
+++ dgrishuk/main.js
@@ -1,16 +1,24 @@
-function сalc (operator, a, b) {
-    switch (operator) {
-      case 'add':
-        return Number(a) + Number(b)
-      case 'multi':
-        return Number(a) * Number(b)
-      case 'subtract':
-        return Number(a) - Number(b)
-      default:
-        return 'Error'
+let operations = {
+    add: '+',
+    multi: '*',
+    sub: '-',
+}
+
+function сalc(operator, a, b) {
+    if (a === Number(a) && b === Number(b)) {
+        switch (operator) {
+            case operations.add:
+                return a + b
+            case operations.multi:
+                return a * b
+            case operations.sub:
+                return a - b
+        }
+    } else {
+        return 'Error in operands'
     }
-  }
-  
-  console.log(сalc('add', 1, 2));
-  console.log(сalc('multi', 1, 2));
-  console.log(сalc('subtract', 3, 2));
+}
+
+console.log(сalc(operations.add, 1, 2));
+console.log(сalc(operations.multi, 1, 2));
+console.log(сalc(operations.sub, 3, 2));

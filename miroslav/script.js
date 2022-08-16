759af48b10be3877a8e9ffe9b21bcb8b031d9c81
Miroslav <mirautkin@gmail.com>
2022-08-16 10:13:21 +0300

    chanched

--- miroslav/script.js
+++ miroslav/script.js
@@ -1,20 +1,20 @@
 function calc(a, b, i) {
-    if (i === "add") {
-        return a + b;
-    } else if (i == "multi") {
-        return a * b;
-    } else if (i == "subtract") {
-        return a - b;
-    }
-
-    // switch(i) {
-    //     case "add":
-    //         return a + b;
-    //     case "multi":
-    //         return a * b
-    //     case "subtract":
-    //         return a - b
+    // if (i === "add") {
+    //     return a + b;
+    // } else if (i == "multi") {
+    //     return a * b;
+    // } else if (i == "subtract") {
+    //     return a - b;
     // }
+
+    switch (i) {
+        case "add":
+            return a + b;
+        case "multi":
+            return a * b;
+        case "subtract":
+            return a - b;
+    }
 }
 
 console.log(calc(1, 2, "add"));

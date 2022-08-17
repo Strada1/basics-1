ae3d5a975dc8ac7fbf1789bc97454e8b14533664
Jarvis-DeNero <github.nikiforov94@gmail.com>
2022-08-17 12:23:15 +0300

    Переделан функционал калькулятора в соответствии с заданием №15

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+const operations = {
+   add: 'add',
+   multi: 'multiply',
+   sub: 'subtract',
+}
+
 function calc(action, a, b) {
 
    /* проверка на NaN */
@@ -5,18 +12,21 @@ function calc(action, a, b) {
       return console.error('Второй и третий аргумент должны быть числами!');
    }
 
-   const operations = {
-      add: +a + +b,
-      multi: a * b,
-      sub: a - b,
+   switch (action) {
+      case operations.add:
+         return +a + +b;
+      case operations.multi:
+         return a * b;
+      case operations.sub:
+         return a - b;
+      default:
+         return console.error(`Введите одно из доступных действий: ${operations.add} / ${operations.multi} / ${operations.sub}`);
    }
-
-   for (let key in operations) {
-      if (key == action) {
-         return operations[key];
-      }
-   }
-
-   return console.error('Введите одно из доступных действий: add (+) / multi (*) / sub (-)');
 
 }
+
+console.log(calc(operations.multi, 2, '3'));
+console.log(calc('add', 2, '4'));
+console.log(calc('multiply', 's', 2));
+console.log(calc('subtract', '2', '4'));
+console.log(calc('subsssss', '2', '4'));

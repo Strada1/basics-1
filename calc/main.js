82178fd0223c2239a5f168a5cfca7c64d9f55faf
toqzun <78686968+toqzun@users.noreply.github.com>
2022-08-17 20:55:37 +0300

    add object

--- calc/main.js
+++ calc/main.js
@@ -1,25 +1,30 @@
+const operations = {
+    add: '+',
+    multi: '*',
+    sub: '-',
+};
+
 function makeCalculation(operator, num1, num2) {
     if (typeof num1 != 'number' || typeof num2 != 'number') {return console.log('Ошибка, используйте только числовые значения')};
     switch (operator) {
-        case 'add':
+        case operations.add:
             console.log(num1 + num2);
             break;
-        case 'subtract':
+        case operations.sub:
             console.log(num1 - num2);
             break;
-        case 'multi':
+        case operations.multi:
             console.log(num1 * num2);
             break;
         default:
-            console.log("Ошибка, введите 'add' для сложения, 'subtract' для вычитания, 'multi' для умножения")
+            console.log("Ошибка, введите operations.add для сложения, operations.sub для вычитания, operations.multi для умножения")
             break;
     }
 };
 
-
-makeCalculation('add', 4, 7);
-makeCalculation('multi', 4, 7);
-makeCalculation('subtract', 4, 7);
-makeCalculation('divide', 4, 7);
-makeCalculation('add', 'qweq', 8);
-makeCalculation('add', 2, 'tutu');
+makeCalculation(operations.add, 4, 7);
+makeCalculation(operations.multi, 4, 7);
+makeCalculation(operations.sub, 4, 7);
+makeCalculation(operations.divide, 4, 7);
+makeCalculation(operations.add, 'qweq', 8);
+makeCalculation(operations.add, 2, 'tutu');

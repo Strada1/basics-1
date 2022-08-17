b0b720cd20e3c693881f2f9b6c55d2f9de492f62
AlexBerdTMN <colttmn@gmail.com>
2022-08-17 22:28:46 +0500

    Добавлен объект operations

--- AlexBerdTMN/script.js
+++ AlexBerdTMN/script.js
@@ -1,10 +1,21 @@
+
 function calc(op, a, b){
+    if(isNaN(a) || isNaN(b)) return 'Не правильный операнд';
+    
+    const operations = {
+    add: 'add',
+    multi: 'mul',
+    sub: 'sub',
+    }
+
     switch(op){
-        case '+': return (+a) + (+b);
-        case '-': return a - b;
-        case '*': return a * b;
+        case operations.add: return (+a) + (+b);
+        case operations.sub: return a - b;
+        case operations.multi: return a * b;
         default: return 'Вы ввели неправельный оператор';
     }
 }
 
-console.log(calc('+', '12', 26));
+console.log(calc('add', 'b', '5'));
+console.log(calc('sub', '2', '5'));
+console.log(calc('mul', '2', '5'));

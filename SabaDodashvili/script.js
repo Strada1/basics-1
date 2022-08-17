a0832d6aa6b21d94bc4dbb8d837e6e9930d4cfe9
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-08-17 23:27:32 +0400

    added operationsObj argument in validation func

--- SabaDodashvili/script.js
+++ SabaDodashvili/script.js
@@ -2,10 +2,14 @@ let a = prompt('enter the first number', '0');
 let b = prompt('enter the second number', '0');
 let operation = prompt('enter one of the operators: add, sub, multi', 'add');
 
-function validateValues(operation, numbOne, numbTwo) {
+function validateValues(operation, numbOne, numbTwo, operationsObj) {
 	let checkOnSpaces = numbOne === ' ' || numbTwo === ' ';
 	let notNumber = (numbOne !== '0' && isNaN(numbOne / numbOne)) || (numbTwo !== '0' && isNaN(numbTwo / numbTwo));
-	let operationIsCorrect = operation === 'add' || operation === 'multi' || operation === 'sub';
+	let operationIsCorrect = false;
+
+	for (const key in operationsObj) {
+		if (key === operation) operationIsCorrect = true;
+	}
 
 	if (checkOnSpaces || notNumber) return 'for the program to work correctly, enter the numbers';
 	else if (!operationIsCorrect) return 'enter the correct operator';
@@ -13,19 +17,27 @@ function validateValues(operation, numbOne, numbTwo) {
 }
 
 function calc(operation, numbOne, numbTwo) {
-	if (validateValues(operation, numbOne, numbTwo) === 'correct validation') {
-		switch (operation) {
-			case 'add':
+	let operations = {
+		add: '+',
+		sub: '-',
+		multi: '*',
+	};
+
+	let validationResult = validateValues(operation, numbOne, numbTwo, operations);
+
+	if (validationResult === 'correct validation') {
+		switch (operations[operation]) {
+			case operations.add:
 				return Number(numbOne) + Number(numbTwo);
-			case 'sub':
+			case operations.sub:
 				return Number(numbOne) - Number(numbTwo);
-			case 'multi':
+			case operations.multi:
 				return Number(numbOne) * Number(numbTwo);
 			default:
 				return 'unexpected error please try again';
 		}
 	} else {
-		return validateValues(operation, numbOne, numbTwo);
+		return validationResult;
 	}
 }
 

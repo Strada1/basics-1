1f3e56e02e76768b33fc70b02c3baddd2dc0d228
Jarvis-DeNero <github.nikiforov94@gmail.com>
2022-08-17 15:59:55 +0300

    Добавлена анонимная функция сбора данных от пользователя, оптимизирон код.

--- script.js
+++ script.js
@@ -3,13 +3,21 @@ const operations = {
    add: 'add',
    multi: 'multiply',
    sub: 'subtract',
-}
+};
+
+const operationsString = function () {
+   let resultStr = '';
+   for (let key in operations) {
+      resultStr += operations[key] + ' | ';
+   }
+   return resultStr;
+};
 
 function calc(action, a, b) {
 
    /* проверка на NaN */
    if (isNaN(+a) || isNaN(+b)) {
-      return console.error('Второй и третий аргумент должны быть числами!');
+      return `Второй и третий аргумент должны быть числами!`;
    }
 
    switch (action) {
@@ -20,10 +28,21 @@ function calc(action, a, b) {
       case operations.sub:
          return a - b;
       default:
-         return console.error(`Введите одно из доступных действий: ${operations.add} / ${operations.multi} / ${operations.sub}`);
+         return `Введите одно из доступных действий: ${operationsString()}`;
+
    }
 
 }
+
+(function () {
+
+   let userAction = prompt(`Укажите одно из доступных действий калькулятора: ${operationsString()}`, '');
+   let userFirstNum = prompt(`Укажите первое число`, '0');
+   let userSecondNum = prompt(`Укажите второе число`, '0');
+
+   alert('Результат: ' + calc(userAction, userFirstNum, userSecondNum));
+
+})();
 
 console.log(calc(operations.multi, 2, '3'));
 console.log(calc('add', 2, '4'));

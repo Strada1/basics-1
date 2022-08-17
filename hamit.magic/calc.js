3e74b003b0186afee1d90cbd718fb93b2e224631
MiSFiTS <hamit.magic@gmail.com>
2022-08-17 18:10:03 +0600

    lerned objects

--- hamit.magic/calc.js
+++ hamit.magic/calc.js
@@ -1,18 +1,24 @@
+const operations = {
+    add: '+',
+    multi: '*',
+    sub: '-',
+};
+
 function calc(first, second, operand) {
     switch(operand) {
-        case "add":
-            return(+first + +second);
-        case "multi":
-            return(+first * +second);
-        case "subtract":
-            return(+first - +second);
+        case "+":
+            return(first + second);
+        case "*":
+            return(first * second);
+        case "-":
+            return(first - second);
         default:
             return("что-то я вас не понимаю");
     }
-}
+};
 
 let first = prompt("Введите первое число", "0");
 let second = prompt("Введите второе число", 0);
-let operand = prompt("Введите +(add), -(subtract), *(multi)", "add");
+let operand = prompt("Введите +(add), -(sub), *(multi)", "add");
 
-alert(calc(first, second, operand));
+alert(calc(+first, +second, operations[operand]));

d94b788a1be03b1df444a211fde434752fb62aca
terlyga78 <telyga78@gmail.com>
2022-08-17 11:41:05 +0300

    Second commit (Add object list of operation)

--- terlyga78/script.js
+++ terlyga78/script.js
@@ -1,25 +1,22 @@
-// function сalc (operation, num1, num2) {
-//   let result;
-//   switch(operation) {
-//     case 'add':
-//       result = num1 + num2;
-//       break;
-//     case 'multi':
-//       result = num1 * num2;
-//       break;
-//     case 'subtract':
-//       result = num1 - num2;
-//       break;
-//        };
-//   return result;
-// }
-
 function calc(operation, num1, num2) {
-    return (operation == 'add') ? num1 + num2 :
-        (operation == 'multi') ? num1 * num2 :
-            (operation == 'subtract') ? num1 - num2 : undefined;
+    const operations = { add: '+', multi: '*', subtract: '-' };
+    let result;
+    switch (operation) {
+        case operations.add:
+            result = num1 + num2;
+            break;
+        case operations.multi:
+            result = num1 * num2;
+            break;
+        case operations.subtract:
+            result = num1 - num2;
+            break;
+        default:
+            result = undefined;
+    };
+    return result;
 }
 
-document.writeln(`1 + 2 = ${calc('add', 1, 2)} <br>`);
-document.writeln(`1 * 2 = ${calc('multi', 1, 2)} <br>`);
-document.writeln(`3 - 2 = ${calc('subtract', 3, 2)} <br>`);
+document.writeln(`1 + 2 = ${calc('+', 1, 2)} <br>`);
+document.writeln(`1 * 2 = ${calc('*', 1, 2)} <br>`);
+document.writeln(`3 - 2 = ${calc('-', 3, 2)} <br>`);

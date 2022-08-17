bdc9c2d03eb7d24ff7501097fc137cc8f0746293
strogovpv <aveliv1@mail.ru>
2022-08-17 20:01:02 +0800

    Калькулятор v2 объект + eval()

--- strogovpv/calc.js
+++ strogovpv/calc.js
@@ -1,10 +1,17 @@
-function getCalc(a, b, operator) {
-  switch (operator) {
-    case '+':
-      return a + b;
-    case '-':
-      return a - b;
-    case '*':
-      return a * b;
+function calc(action, a, b) {
+  const operations = {
+    add: '+',
+    sub: '-',
+    multy: '*'
   }
+
+  if (operations[action] === undefined) return 'Action is not supported';
+  if (typeof a !== 'number' || typeof b !== 'number') return 'Only numbers, please';
+
+  return eval(a + operations[action] + b);
 }
+
+console.log( calc('multy', 'АВпррп', 7) );
+console.log( calc('multy', '8', 7) );
+console.log( calc('mul', 5, 7) );
+console.log( calc('multy', 5, 7) );

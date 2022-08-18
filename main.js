061c31acfe440882d22d52966b8ed96fb58a8a2f
pteechkin <errorbird9001@gmail.com>
2022-08-18 17:30:46 +0800

    calc func updated

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-function calc (a, action, b) {
-    switch(action) {
-        case '+': return (a + b); 
-        case '-': return (a - b); 
-        case '*': return (a * b); 
-        default:  return ('некорректный оператор');
+//calc v1.2 (using switch/object)
+function calc (firstOp, action, secondOp) {
+    let operations = {
+        add: 'add',
+        sub: 'sub',
+        multi: 'multi',
     }
-}
-console.log(calc (1000, '-', 7));
+      switch(action) {
+        case operations.add: return firstOp + secondOp;
+        case operations.sub: return firstOp - secondOp;
+        case operations.multi: return firstOp * secondOp;
+      }  
+    }
+console.log(calc (1000, 'sub', 7));
+

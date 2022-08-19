50ce0d3bf7278e29ad24eec069ba419f4f9e71ca
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-08-19 18:36:42 +0400

    checkStatus func

--- SabaDodashvili/todo.js
+++ SabaDodashvili/todo.js
@@ -3,11 +3,17 @@ let todo = {
 	'find job': 'in progress',
 	'make todo List': 'done',
 
+	statuses: {
+		status_1: 'not yet time',
+		status_2: 'in progress',
+		status_3: 'done',
+	},
+
 	changeStatus(task, newStatus) {
-		if (this.keyExists(task)) this[task] = newStatus;
+		if (this.keyExists(task) && this.checkStatus(newStatus)) this[task] = newStatus;
 	},
 	addTask(taskName, status) {
-		if (!this.keyExists(taskName)) this[taskName] = status;
+		if (!this.keyExists(taskName) && this.checkStatus(status)) this[taskName] = status;
 	},
 	deleteTask(task) {
 		if (this.keyExists(task)) delete this[task];
@@ -25,9 +31,17 @@ let todo = {
 	keyExists(keyName) {
 		if (keyName in this) return true;
 	},
+
+	checkStatus(status) {
+		for (const key in this.statuses) {
+			if (status === this.statuses[key]) return true;
+		}
+		return false;
+	},
 };
 
 todo.changeStatus('buy products', 'in progress');
-todo.add('do PR', 'in progress');
+todo.addTask('do PR', 'in progress');
 todo.deleteTask('find job');
 todo.showList();
+console.log(todo);

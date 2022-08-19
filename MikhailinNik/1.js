171608a727c7b15c90550d38c4a4229ea1115b81
Nikita <ifyoucan1337@yandex.ru>
2022-08-19 11:02:56 +0300

    Fixed words

--- MikhailinNik/1.js
+++ MikhailinNik/1.js
@@ -1,22 +1,22 @@
 const operation = {
-	add: 'add',
-	multi: 'multi',
-	substract: 'substract',
-}
+	add: "add",
+	multi: "multi",
+	subtract: "subtract",
+};
 
 function calculate(operand, a, b) {
 	switch (operand) {
 		case operation.add:
-			return a + b
+			return a + b;
 		case operation.multi:
-			return a * b
-		case operation.substract:
-			return a - b
+			return a * b;
+		case operation.subtract:
+			return a - b;
 		default:
-			alert('Ошибка')
+			alert("Ошибка");
 	}
 }
 
-console.log(calculate('add', 2, 1))
-console.log(calculate('multi', 1, 2))
-console.log(calculate('substract', 3, 2))
+console.log(calculate("add", 2, 1));
+console.log(calculate("multi", 1, 2));
+console.log(calculate("subtract", 3, 2));

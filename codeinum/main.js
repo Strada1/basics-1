fa7dfc3b5693fc1c75f7a7bb1ef190c8835c147e
codeinium <timofiei.tarasov@gmail.com>
2022-08-19 17:51:09 +0500

    added ToDoList sctipt

---
+++ codeinum/main.js
@@ -0,0 +1,28 @@
+let todoList = new Object();
+
+function changeStatus (nameTask) {
+    todoList[nameTask] = !this[nameTask];
+}
+function addTask (nameTask) {
+    todoList[nameTask] = false;
+}
+function deleteTask (nameTask) {
+    delete todoList[nameTask]; 
+}
+function showList () {
+    console.log('My ToDo \n');
+    for (key in todoList) {
+        console.log(`${key} - ` + `${todoList[key]}`);
+    }
+}
+
+addTask('working on the garden')
+console.log(todoList)
+addTask('code practice')
+console.log(todoList)
+addTask('playing computer games')
+console.log(todoList)
+deleteTask('code practice')
+changeStatus('working on the garden')
+console.log(todoList)
+showList()

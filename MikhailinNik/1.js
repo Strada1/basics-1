49e6fabf63f24ef5a4427d8e2695889a762e398c
Nikita <ifyoucan1337@yandex.ru>
2022-08-19 14:37:35 +0300

    create ToDo list

--- MikhailinNik/1.js
+++ MikhailinNik/1.js
@@ -20,3 +20,29 @@ function calculate(operand, a, b) {
 console.log(calculate("add", 2, 1));
 console.log(calculate("multi", 1, 2));
 console.log(calculate("subtract", 3, 2));
+
+let toDo = {};
+
+toDo.addTask = function (task, status = "doing") {
+	this[task] = status;
+};
+
+toDo.changeTask = function (task, status) {
+	this[task] = status;
+};
+
+toDo.deleteTask = function (task) {
+	delete this[task];
+};
+
+toDo.showList = function () {
+	for (let key in this) {
+		if (typeof (this[key]) != "function") {
+			console.log('Задача: '  + key + `\n` + 'Статус задачи: ' + this[key]);
+		}
+	}
+};
+
+toDo.addTask("tea", "do");
+toDo.addTask("coffee", "done");
+toDo.showList();

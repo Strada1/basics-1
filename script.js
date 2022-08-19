2ebb3a2792df6783f1dd09a7dc6ba77223e28a38
Stanovov Mikhail <98664376+CTEPXMGN@users.noreply.github.com>
2022-08-19 13:31:27 +0500

    to do without UI

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-function calc(operator, a, b) {
+let toDoList = {};
 
-    switch (operator) {
-        case 'add':
-            return Number(a) + Number(b);
-        case 'multi':
-            return a * b;
-        case 'subtract':
-            return a - b;
-        default:
-            break;
-    }
+function addTask(list, task, status) {
+    return list[task] = status;
 }
 
-console.log( calc('add', 2, 5) );
-console.log( calc('multi', 2, 5) );
-console.log( calc('subtract', 2, 5) );
-console.log( calc('add', 15, 3) );
+function changeStatus(list, task, status) {
+    return list[task] = status;
+}
+
+function showList(list) {
+    let inProgress = 'In progress: \n';
+    let done = 'Done: \n';
+
+    for (let key in list) {
+        if (list[key] == 'in progress') {
+            inProgress = inProgress + '  ' + key + '\n';
+        }
+        if (list[key] == 'done') {
+            done = done + '  ' + key + '\n';
+        }
+    }
+    let result = inProgress + done;
+    
+    return console.log(result);;
+}
+
+function deleteTask(list ,task) {
+    delete list[task];
+}
+
+addTask(toDoList, 'to run', 'in progress');
+addTask(toDoList, 'to buy a bread', 'in progress');
+addTask(toDoList, 'to drink a cup of coffee', 'done');
+addTask(toDoList, 'to make a bed', 'in progress');
+changeStatus(toDoList, 'to run', 'done');
+deleteTask(toDoList, 'to drink a cup of coffee');
+showList(toDoList);

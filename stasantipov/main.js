d7e9bb7c65fc87a0c27912bb3d3ab086f095ac35
Stanislav Antipov <antistas@yandex.ru>
2022-08-20 08:41:41 +0300

    Added ToDoList

--- stasantipov/main.js
+++ stasantipov/main.js
@@ -1,20 +1,38 @@
-function calc(operator, a, b) {
-    switch (operator) {
-        case 'add':
-            return a + b;
-        case 'multi':
-            return a * b;
-        case 'subtract':
-            return a - b;
-        default:
-            return 'Ошибка';
+const toDoList = {
+  list: {
+    "create a new practice task": "To Do",
+    "make a bed": "To Do",
+    "write a post": "In Progress",
+  },
+  addTask: function(task) {
+    this.list[task] = 'To Do';
+  },
+  changeStatus: function(task, status) {
+    this.list[task] = status;
+  },
+  deleteTask: function(task) {
+    delete this.list[task];
+  },
+  showList: function() {
+    console.log('To Do:');
+    for(key in this.list) {
+      if(this.list[key] === 'To Do') {
+        console.log('\t' + key);
+      }
     }
+    console.log('In Progress:');
+    for(key in this.list) {
+      if(this.list[key] === 'In Progress') {
+        console.log('\t' + key);
+      }
     }
+    console.log('Done:');
+    for(key in this.list) {
+      if(this.list[key] === 'Done') {
+        console.log('\t' + key);
+      }
+  }
+}
+}
 
-
-
-
-console.log(calc('add', 1, 2));
-console.log(calc('multi', 1, 2));
-console.log(calc('subtract', 3, 2));
-
+toDoList.showList();

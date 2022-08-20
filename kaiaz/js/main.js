9d5b5e995a42057447944d1e94b28a9726b3070f
Kai <azrailkai@gmail.com>
2022-08-20 17:58:14 +0600

    add new statuses for tasks in ToDo app, rewrite functions

--- kaiaz/js/main.js
+++ kaiaz/js/main.js
@@ -1,40 +1,62 @@
 const toDo = {
-  "Помыть посуду": true,
-  "Купить продукты": false,
-  "Покормить кота": true,
+  statuses: {
+    inProgress: "In Progress",
+    done: "Done",
+    todo: "To Do",
+  },
+  tasks: {
+    "Помыть посуду": "In Progress",
+    "Купить продукты": "Done",
+    "Покормить кота": "To Do",
+    "Покормить рыбок": "Done",
+  },
   addTask() {
     let task;
     task = prompt("Введите задачу!", "Написать приложение");
-    this[task] = false;
-    console.log("========  Добавлена задача " + task + " ========");
+    this.tasks[task] = this.statuses.todo;
+    console.log(`========  Добавлена задача "${task}" ========`);
   },
   deleteTask(task) {
     let textTask = task;
-    delete this[task];
+    delete this.tasks[task];
     console.log('======== Задача "' + textTask + '" удалена ========');
   },
-  changeStatus(task) {
-    this[task] = !this[task];
+  changeStatus(task, status) {
+    for (let key in this.tasks) {
+      if (key === task) {
+        for (let key in this.statuses) {
+          if (key !== status) {
+            this.tasks[task] = status;
+          }
+        }
+      }
+    }
     console.log(
-      '========  Статус этой задачи  "' + task + '" изменен ========'
+      `========  Статус этой задачи "${task}" изменен на "${status}" ========`
     );
   },
+
   showList() {
     console.log("======== Список Задач ========");
-    for (let task in this) {
-      if (typeof this[task] === "function") {
-        continue;
-      } else {
-        console.log(task, ":", this[task]);
+    for (let status in this.statuses) {
+      console.log(status.toUpperCase() + ":");
+      for (let task in this.tasks) {
+        // console.log(this.tasks[task]);
+        if (this.statuses[status] === this.tasks[task]) {
+          console.log(task + " : ", status);
+        }
       }
     }
   },
 };
 
 toDo.showList();
+toDo.showList();
+
 toDo.addTask();
 toDo.showList();
-toDo.changeStatus("Купить продукты");
+toDo.changeStatus("Купить продукты", "In Progress");
 toDo.showList();
-toDo.deleteTask("Помыть посуду");
+toDo.deleteTask('"Помыть посуду');
+
 toDo.showList();

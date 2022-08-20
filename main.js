112ec71effbe880eb916a155923bd6c2d8bfb5ae
Kirill <Ch1lim@yandex.ru>
2022-08-20 20:22:11 +1000

    First commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const repositoryToDo = {
+    
+};
+
+function changeStatus(task, statusTask) {
+    if ( statusTask == 'done' ) {  
+        repositoryToDo[task] += ' done'
+    } else {
+        console.log("Введите значение done, если задача выполнена");
+    }
+}
+
+function addTask(numberTask, description) {
+    (repositoryToDo["task" + numberTask]) = description;
+}
+
+function deleteTask(task) {
+    task = delete repositoryToDo[task];
+}
+
+function showList() {
+    for (let key in repositoryToDo) {
+        console.log( key );
+        console.log( repositoryToDo[key] );
+    }
+}
+
+
+addTask(2, 'Сделать яичницу');
+changeStatus("task2", " done")
+showList();
+

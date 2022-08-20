5f4e2e7c8b452e7b57ee97611e21fd72ff113106
Михаил Трухин <meatrue@gmail.com>
2022-08-20 10:13:30 +0300

    Добавляет приложение todo-list

---
+++ todo-list.js
@@ -0,0 +1,62 @@
+const todoList = {
+  tasks: {
+  },
+
+  statuses: {
+    toDo: 'To Do',
+    inProgress: 'In Progress',
+    done: 'Done'
+  },
+
+  changeStatus(task, status) {
+    if (this.tasks[task] && this.tasks[task] !== status && Object.values(this.statuses).includes(status)) {
+      this.tasks[task] = status;
+    }
+  },
+
+  addTask(task) {
+    if (task && !this.tasks[task]) {
+      this.tasks[task] = this.statuses.toDo;
+    }
+  },
+
+  deleteTask(task) {
+    if (this.tasks[task]) {
+      delete this.tasks[task];
+    }
+  },
+
+  showList() {
+    for (key in this.statuses) {
+      console.log(`${this.statuses[key]}: `);
+      this.showListByStatus(this.statuses[key]);
+    }
+  },
+
+  showListByStatus(status) {
+    if (status && Object.values(this.statuses).includes(status)) {
+      for (task in this.tasks) {
+        if (this.tasks[task] === status) {
+          console.log(`   "${task}"`);
+        }
+      }
+    }
+  }
+}
+
+todoList.addTask('Погулять');
+todoList.addTask('Побриться');
+todoList.addTask('Почистить зубы');
+todoList.addTask('Дочитать книгу');
+
+
+todoList.showList();
+
+todoList.changeStatus('Погулять', 'Done');
+todoList.changeStatus('Почистить зубы', 'In Progress');
+
+todoList.showList();
+
+todoList.deleteTask('Побриться');
+
+todoList.showList();

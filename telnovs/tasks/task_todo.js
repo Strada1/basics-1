a540616797b1f14b76e23f18072c12aa62e7f0f5
Telnov Sergei <telnov-ss@mail.ru>
2022-08-20 22:50:37 +0700

    task_toDo refactoring

--- telnovs/tasks/task_todo.js
+++ telnovs/tasks/task_todo.js
@@ -1,42 +1,70 @@
-const toDo={};
+const STATUS ={
+  To_Do: 'To Do',
+  Done: 'Done',
+  In_Progress: 'In Progress'
+}
 
-const addTask = (task) =>   {
-    toDo[task] = 'В работе';
+const list={
+  "create a new practice task": STATUS.In_Progress,
+	"make a bed": STATUS.Done,
+	"write a post": STATUS.To_Do,
+};
+
+const addTask = (task ) => {
+  if(task !== undefined){
+     return list[task] = task;
+  }else {
+    console.log('Введите задачу')
+  }
 }
 const deleteTask = (task) => {
-  delete toDo[task];
-}
-
-const changeStatus = (task) => {
-  if (toDo[task] === 'В работе'){
-    return toDo[task] = ' Выполнено';
-  }else 
-    return toDo[task] = ' В работе';
-}
-
-const showList = (toDo) =>{
-
-  for(let key in toDo ){
-    console.log( 'Задача:' + [key] + '  -  Статус выполнения: ' + toDo[key] );    
-   } 
+  if(task !== undefined){
+    return delete list[task];
+  }else {
+    console.log('Введите задачу')
   }
- 
- 
-addTask('Учиться');
-changeStatus('Учиться');
-changeStatus('Учиться');
-addTask('Писать код');
-changeStatus('Писать код');
-changeStatus('Писать код');
-addTask('Купить молоко');
-changeStatus('Купить молоко');
-addTask('Заехать в магазин');
-changeStatus('Заехать в магазин');
-addTask('почитать книгу ');
-addTask('позаниматься с сыном');
+  
+}
 
-showList(toDo);
-deleteTask('Купить молоко');
-deleteTask('Заехать в магазин');
+const changeStatus = (task, status) => {
+  if(task && status !== undefined){  
+     if( task in list){
+      return list[task] = status;
+     }else{
+      return console.log(" Нет задачи такой ");
+     }   
+  }
+  return console.log("Введите задаче и статус");
+}
+const showList = (list) =>{
+  let in_progress = 'In Progress';
+  let to_do = 'To Do:';
+  let done ='Done:';
+  if(list !== undefined){  
+    for( let key in list ){
+      if(list[key] === STATUS.Done){
+        done += `\n \t${key}`
+      }
+      if(list[key] === STATUS.To_Do){
+        to_do += `\n \t${key}`
+      }
+      if(list[key] === STATUS.In_Progress){
+        in_progress += `\n \t${key}`
+      }
+    }
+    let result= `${to_do} \n`+ `${in_progress}\n`+`${done}`
+    return console.log(result);
+  }
+  return console.log("Нет листа ТО ДУ")
+}
+addTask('creat To');
+addTask('creat ');
+addTask('creat T');
+console.log(list);
+changeStatus('creat To',STATUS.Done)
+changeStatus('creat ',STATUS.Done);
+changeStatus('creat T',STATUS.Done);
+showList(list)
 
-showList(toDo);
+
+

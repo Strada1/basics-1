d52ac8723ea3b2fb45c6a0dc19dc251ff51715d9
evvgenchik <yevgenchikk@mail.ru>
2022-08-21 21:19:11 +0300

    Fix ToDo

--- evvgenchik-calc/main.js
+++ evvgenchik-calc/main.js
@@ -1,29 +1,61 @@
 'use stict'
 
 
-const toDo = {
-	study: 'complete',
-	exercise: 'uncomplete',
-	changeStatus(task, status) {
-		this[task] = status || 'complete'
-	},
-	addTask(task) {
-		this[task] = 'uncomplete'
-	},
-	deleteTask(task) {
-		delete this[task]
-	},
-	showList() {
-		for (let key in this) {
-			if (typeof this[key] != 'function') {
-				alert(`Задача ${key}: ${this[key]}`)
-			}
+
+const list = {
+	'stydy': 'In progress',
+	'gym': 'ToDo',
+	'eat': 'Done',
+}
+function changeStatus(task, status) {
+	for (let key in list) {
+		if (status == 'In progress' || status == 'ToDo' || status == 'Done') {
+			return list[task] = status
+		}
+	}
+	alert('don\'t has this status')
+}
+function addTask(task) {
+	list[task] = 'ToDo'
+}
+function deleteTask(task) {
+	for (let key in list) {
+		if (key == task) {
+			return delete list[task]
+		}
+	}
+	alert('don\'t has this task')
+}
+
+function showList() {
+	console.log('ToDo:');
+	for (let key in list) {
+		if (list[key] == 'ToDo') {
+			console.log(`
+							${key}`);
+		}
+	}
+	console.log('In progress:');
+	for (let key in list) {
+		if (list[key] == 'In progress') {
+			console.log(`
+							${key}`);
+		}
+	}
+	console.log('Done:');
+	for (let key in list) {
+		if (list[key] == 'Done') {
+			console.log(`
+							${key}`);
 		}
 	}
 }
 
-toDo.addTask('shower')
-toDo.changeStatus('shower', 'wait')
-toDo.addTask('breakfast')
-toDo.deleteTask('breakfast')
-toDo.showList()
+addTask('take shower')
+addTask('masturbate')
+addTask('fight')
+changeStatus('take shower', 'Done')
+changeStatus('fight', 'I progress')
+deleteTask('eat')
+deleteTask('big')
+showList()

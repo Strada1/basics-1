a9b79bbc82aa39647dfa1e35759a231cdc021bb7
AlexBerdTMN <colttmn@gmail.com>
2022-08-21 13:23:46 +0500

    Изменена функция showList

--- ToDo/ToDo.js
+++ ToDo/ToDo.js
@@ -16,28 +16,26 @@ function addTask(taskName){
     }
     else{
     todoList[taskName] = STATUS.TO_DO;
+    console.log(`Задача "${taskName}" добалена`);
     }
 }
 
 function showList(){
+    
+    function sortStatus(status){
+        for(let key in todoList){
+            if(status == todoList[key]){
+                console.log('   ' + key);
+            }
+        }
+    }
+    
     console.log('To Do:')
-    for(let key in todoList){
-        if(STATUS.TO_DO== todoList[key]){
-            console.log('   ' + key);
-        }
-    }
+    sortStatus(STATUS.TO_DO);
     console.log('In progress:');
-    for(let key in todoList){
-        if(STATUS.IN_PROGRESS== todoList[key]){
-            console.log('   ' + key);
-        }
-    }
+    sortStatus(STATUS.IN_PROGRESS);
     console.log('Done:');
-    for(let key in todoList){
-        if(STATUS.DONE== todoList[key]){
-            console.log('   ' + key);
-        }
-    }
+    sortStatus(STATUS.DONE);
     console.log('--------------------------------------------------');
 }
 
@@ -49,6 +47,7 @@ function changeStatus(taskName, taskStatus){
     if(taskStatus in STATUS){
         if(taskName in todoList){
             todoList[taskName] = taskStatus;
+            console.log(`Статус задачи "${taskName}" заменён на "${taskStatus}"`);
         }
         else{
             console.log('Такой задачи не существует.');
@@ -63,7 +62,8 @@ function changeStatus(taskName, taskStatus){
 
 function deleteTask(taskName){
     if(taskName in todoList){
-        delete todoList[taskName]
+        delete todoList[taskName];
+        console.log(`Задача "${taskName}" удалена`);
     }
     else{
         console.log('Такого задания не существует');
@@ -83,4 +83,5 @@ changeStatus('Купить хлеб', STATUS.IN_PROGRESS);
 changeStatus('Заплатить за квартиру', STATUS.IN_PROGRESS);
 changeStatus('Покормить кота', STATUS.DONE);
 changeStatus('Постирать бельё', STATUS.DONE);
+deleteTask('Покормить кота');
 showList();

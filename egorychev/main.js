dcd0ef9a50530480989611e6e3283bd46249ee4f
Djeksrakagrey <egory4ev@mail.ru>
2022-08-21 22:38:26 +0800

    add calk in obj and ToDo in obj

--- egorychev/main.js
+++ egorychev/main.js
@@ -1,35 +1,65 @@
-console.log('Hello World!');
+// console.log('Hello World!');
 
-function calk(operator, a, b) {
+// function calk(operator, a, b) {
+//     const isNotValid = (typeof a !== 'number') || (typeof b !== 'number') || (operator === '');
+//
+//     if(isNotValid) {
+//         return 'Error' ;
+//     } else if(operator === 'add') {
+//         return a + b;
+//     } else if(operator === 'sub') {
+//         return a - b;
+//     } else if(operator === 'multy') {
+//         return a * b;
+//     } else if(operator === 'div') {
+//         return a / b;
+//     }
+// }
+//
+// console.log(calk('add', 33, 4));
+//
+// function Calk(operator, a, b) {
+//     switch (operator) {
+//         case 'add':
+//             return a + b;
+//             break;
+//         case 'multy':
+//             return a * b;
+//             break;
+//         case 'div':
+//             return a - b;
+//             break;
+//     }
+// }
+//
+// console.log(Calk('div', 15, 4));
+
+
+// for (let i = 1; i < 20; ++i) {
+//     console.log(i);
+// }
+
+const operations = {
+    add: '+',
+    sub: '-',
+    multy: '*',
+    div: '/',
+};
+
+function Calk(operator, a, b) {
     const isNotValid = (typeof a !== 'number') || (typeof b !== 'number') || (operator === '');
 
     if(isNotValid) {
         return 'Error' ;
-    } else if(operator === 'add') {
+    } else if(operator === operations.add) {
         return a + b;
-    } else if(operator === 'sub') {
+    } else if(operator === operations.sub) {
         return a - b;
-    } else if(operator === 'multy') {
+    } else if(operator === operations.multy) {
         return a * b;
-    } else if(operator === 'div') {
+    } else if(operator === operations.div) {
         return a / b;
     }
 }
 
-console.log(calk('add', 33, 4));
-
-function Calk(operator, a, b) {
-    switch (operator) {
-        case 'add':
-            return a + b;
-            break;
-        case 'multy':
-            return a * b;
-            break;
-        case 'div':
-            return a - b;
-            break;
-    }
-}
-
-console.log(Calk('div', 15, 4));
+console.log(Calk('+', 5, 5));
---
+++ egorychev/toDo.js
@@ -0,0 +1,52 @@
+const list = {
+    "create a new practice task": "In Progress",
+    "make a bed": "Done",
+    "write a post": "To Do",
+}
+
+function changeStatus(task, status) {
+    for(let key in list) {
+    if(key === task) {
+        list[key] = status;
+        }
+    }
+}
+
+function addTask(task) {
+    list[task] = 'To Do';
+}
+
+function deleteTask(task) {
+    delete list[task];
+}
+
+function findTask(status) {
+    for(let key in list) {
+        if(list[key] === status) {
+            console.log(`\t${key}`);
+        }
+    }
+}
+
+function showList() {
+    console.log('To Do:')
+    findTask('To Do')
+
+    console.log('In Progress:')
+    findTask('In Progress');
+
+    console.log('Done:');
+    findTask('Done');
+}
+
+changeStatus('make a bed', 'In Progress');
+addTask('read book');
+addTask('walk on the street');
+changeStatus('walk on the street', 'Done');
+deleteTask('create a new practice task');
+
+showList();
+
+
+
+

af8e7cd3991f183255132d06807d6678abd84557
Alexey Popov <alexnn0987@mail.ru>
2022-08-21 14:35:32 +0300

    add functions

--- calctest/todo.js
+++ calctest/todo.js
@@ -3,3 +3,25 @@ const list = {
   'make a bed': 'Done',
   'write a post': 'To Do',
 };
+
+// Меняем статус задачи (меняем значение ключа)
+function changeStatus(task, status) {
+  list[task] = status;
+}
+changeStatus('write a post', 'Done');
+// console.log(list);
+
+// Добавляем новую задачу (добавляем новый ключ)
+function addTask(task) {
+  list[task] = 'To Do';
+}
+addTask('have a walk');
+// console.log(list);
+
+// Удаляем задачу (удаляем ключ)
+function deleteTask(task) {
+  delete list[task];
+}
+
+deleteTask('have a walk');
+// console.log(list);

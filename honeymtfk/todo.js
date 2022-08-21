e5804b4255804ae2c56160e22e9169a2c52faf56
Peter <103073470+HoneyMTFK@users.noreply.github.com>
2022-08-22 01:47:56 +0300

    Fixed objects & functions
    Была добавлена константа статусов. Также были добавлены проверки во все функции из ТЗ. Изначально я хотел сделать проверку статуса в одной функции, которую можно было бы использовать в showList (). Так как у меня не получилось этого сделать, я разделил эту функцию на 3 части. Так и не понял как решить проблему с дефисом. Пытался прописывать в проверке статусов, но когда запускаю код, дефисов становится неадекватно много и все они разбросаны по списку в хаотичном порядке ¯\_(ツ)_/¯

--- honeymtfk/todo.js
+++ honeymtfk/todo.js
@@ -1,58 +1,81 @@
+const STATUS = {
+	TO_DO: "To Do",
+	IN_PROGRESS: "In Progress",
+	DONE: "Done"
+}
+
 const list = {
-	"Make some js work": "To Do",
-	"Make some HTML/CSS work": "In Progress",
-	"Open computer": "Done"
+	"Make some js work": STATUS.TO_DO,
+	"Make some HTML/CSS work": STATUS.IN_PROGRESS,
+	"Open computer": STATUS.DONE
 }
 
 function changeStatus(task, status) {
-	if (status == "To Do" || "In Progress" || "Done") {
-		list[task] = status;
+	if (task in list && status in STATUS) {
+		list[task] = STATUS[status]
 	} else {
-		console.log("error");
+		console.log ('This is impossible, try again!')
 	}
 }
 
 function addTask(new_task) {
-	new_task in list;
-	list[new_task] = "To Do";
+	if ((typeof new_task) == 'string') {
+		list[new_task] = STATUS.TO_DO;
+	} else {
+		console.log('This is impossible, try again!');
+	}
 }
 
 function deleteTask (task) {
 	if (task in list) {
 		delete list[task];
+	} else {
+		console.log('This is impossible, try again!');
+	}
+}
+
+function todoStatusCheck() {
+	for (let task in list) {
+		switch (list[task]) {
+			case STATUS.TO_DO:
+				console.log("	" + task);
+				break;
+		}
+	}
+}
+
+function inProgStatusCheck() {
+	for (let task in list) {
+		switch (list[task]) {
+			case STATUS.IN_PROGRESS:
+				console.log("	" + task);
+				break;
+		}
+	}
+}
+
+function doneStatusCheck() {
+	for (let task in list) {
+		switch (list[task]) {
+			case STATUS.DONE:
+				console.log("	" + task);
+				break;
+		}
 	}
 }
 
 function showList () {
 	console.log("To Do:");
-	for (let item in list) {
-		switch (list[item]) {
-			case "To Do":
-				console.log("	" + item);
-				break;
-		}
-	}
+	todoStatusCheck();
 	console.log("In Progress:");
-	for (let item in list) {
-		switch (list[item]) {
-			case "In Progress":
-				console.log("	" + item);
-				break;
-		}
-	}
+	inProgStatusCheck();
 	console.log("Done:");
-	for (let item in list) {
-		switch (list[item]) {
-			case "Done":
-				console.log("	" + item);
-				break;
-		}
-	}
+	doneStatusCheck();
 }
-addTask("Make a homework");
-addTask("Dinner");
-addTask("sleep")
+
+changeStatus("Make some HTML/CSS work", "DONE");
+changeStatus("Make some js work", "IN_PROGRESS");
+addTask("Make some functions in TODO");
+addTask("Close computer");
 deleteTask("Open computer");
-changeStatus("Make some js work", "Done");
-changeStatus("Make some HTML/CSS work", "Done");
 showList();

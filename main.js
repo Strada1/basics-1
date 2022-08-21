b268f3c51bc0a01849af3cb16e3d945d94463145
Agitobl <vlad.agito@gmail.com>
2022-08-21 11:44:40 +0300

    crete todo file

--- main.js
+++ main.js
@@ -1,57 +1,36 @@
-let list = {
-	"create a new practice task": "In Progress",
-	"make a bed": "Done",
-	"write a post": "To Do",
-  "write a post1": "To Do",
+const operations = {
+  add: "add",
+  multi: "multi",
+  sub: "subtract",
 }
 
-const changeStatus = (task, status) => {
-  list = {...list, [task]: status};
-}
+function calc(operator, a, b) {
+  const {add, multi, sub} = operations;
 
-const addTask = (task) => {
-  list = {...list, [task]: "To Do"};
-}
-
-const deleteTask = (task) => {
-  if (task in list) {
-   return delete list[task];
+  switch (operator) {
+    case add:
+      return a + b;
+    case multi:
+      return a * b;
+    case sub:
+      return a - b;
+    default:
+      console.log("Ошибка");
   }
-  console.log('No such task');
+}
+calc(operations.add, 3, 2);
+
+/////////////////////////////////
+
+for (let i = 1; i <= 19; i++) {
+  console.log(i);
 }
 
-const showList = () => {
-  const result = {
-    "To Do": [],
-    "In Progress": [],
-    "Done": [],
-  }
+/////////////////////////////////
 
-  const arr = Object.keys(list);
+let i = 1;
 
-  arr.forEach(item => {
-    switch (list[item]) {
-      case 'To Do':
-        return result['To Do'].push(item);
-
-      case 'In Progress':
-        return result['In Progress'].push(item);
-
-      case 'Done':
-        return result['Done'].push(item);
-    
-      default:
-        break;
-    }
-  })
-
-  console.log(`To Do: ${result['To Do']}`);
-  console.log(`In Progress: ${result['In Progress']}`);
-  console.log(`Done: ${result['Done']}`);
+while(i <= 19) {
+  console.log(i);
+  i++;
 }
-
-changeStatus("write a post", "Done");
-addTask("kill him");
-deleteTask("make a bed");
-showList();
-
---
+++ todo.js
@@ -0,0 +1,56 @@
+let list = {
+	"create a new practice task": "In Progress",
+	"make a bed": "Done",
+	"write a post": "To Do",
+  "write a post1": "To Do",
+}
+
+const changeStatus = (task, status) => {
+  list = {...list, [task]: status};
+}
+
+const addTask = (task) => {
+  list = {...list, [task]: "To Do"};
+}
+
+const deleteTask = (task) => {
+  if (task in list) {
+   return delete list[task];
+  }
+  console.log('No such task');
+}
+
+const showList = () => {
+  const result = {
+    "To Do": [],
+    "In Progress": [],
+    "Done": [],
+  }
+
+  const arr = Object.keys(list);
+
+  arr.forEach(item => {
+    switch (list[item]) {
+      case 'To Do':
+        return result['To Do'].push(item);
+
+      case 'In Progress':
+        return result['In Progress'].push(item);
+
+      case 'Done':
+        return result['Done'].push(item);
+    
+      default:
+        break;
+    }
+  })
+
+  console.log(`To Do: ${result['To Do']}`);
+  console.log(`In Progress: ${result['In Progress']}`);
+  console.log(`Done: ${result['Done']}`);
+}
+
+changeStatus("write a post", "Done");
+addTask("kill him");
+deleteTask("make a bed");
+showList();

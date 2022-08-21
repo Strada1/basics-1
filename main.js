15a2b92242365cf1a0ce6c7d54b1dab9514dd7c8
AlinaGost <alinka14011998@mail.ru>
2022-08-21 14:45:31 +0300

    refactor todoList

--- main.js
+++ main.js
@@ -1,43 +1,72 @@
+const STATUS = {
+  'TO_DO': 'todo',
+  'IN_PROGRESS': 'in progress',
+  'DONE': 'done',
+}
+
 const todoList = {};
 
 // добавлене новой задачи
 const addTask = (task) => {
-  todoList[task] = false;
+  todoList[task] = STATUS.TO_DO;
 }
 
 // удаление задачи
 const deleteTask = (task) => {
-  delete todoList[task];
+  for (let key in todoList) {
+    if (task === key) {
+      delete todoList[task]
+    }
+  }
 }
 
 // меняет статус задачи
-const changeStatus = (task) => {
-  todoList[task] === false ? todoList[task] = true : todoList[task] = false;
+const changeStatus = (task, status) => {
+  todoList[task] = status;
 }
 
 // вывод всех задач в консоль
 const showList  = () => {
- for (let key in todoList) {
-   console.log(key,':',todoList[key]);
- }
+  console.log('TODO');
+  console.log('**********************************');
+  for (let key in todoList) {
+    if (todoList[key] === 'todo') {
+      console.log(`${key} \n`);
+    }
+  }
+
+  console.log('IN PROGRESS');
+  console.log('**********************************');
+  for (let key in todoList) {
+    if (todoList[key] === 'in progress') {
+      console.log(`${key} \n`);
+    }
+  }
+
+  console.log('DONE');
+  console.log('**********************************');
+  for (let key in todoList) {
+    if (todoList[key] === 'done') {
+      console.log(`${key} \n`);
+    }
+  }
 }
 
 addTask('купить хлеб');
 addTask('купить молоко');
 addTask('сходить к врачу');
 addTask('забрать вещи с пункта выдачи');
-showList();
-console.log('-----------------------------------------------');
 deleteTask('купить хлеб');
-changeStatus('сходить к врачу');
-showList();
-console.log('-----------------------------------------------');
+changeStatus('сходить к врачу', 'done');
 addTask('дать лекарство коту');
-changeStatus('купить молоко');
-changeStatus('сходить к врачу');
-showList();
-console.log('-----------------------------------------------');
+changeStatus('купить молоко', 'todo');
 addTask('помыть кота');
-changeStatus('дать лекарство коту');
+changeStatus('дать лекарство коту', 'in progress');
 deleteTask('сходить к врачу');
+deleteTask('помыть кота');
+addTask('сходить в магазин');
+changeStatus('сходить в магазин', 'in progress');
+addTask('вернуть платье');
+changeStatus('вернуть платье', 'done');
+
 showList();

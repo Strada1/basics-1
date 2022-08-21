9cc6693579852f6d4b0f9bad8e5dcfbf06312b1c
Alina <krakhmaalina@gmail.com>
2022-08-21 23:17:40 +0300

    Added checks (ToDo List (obj))

--- alikraa/ToDo List (obj).js
+++ alikraa/ToDo List (obj).js
@@ -1,30 +1,40 @@
-const statuses = {
-    to_do: 'To Do',
-    in_progress: 'In Progress',
-    done: 'Done'
+const STATUSES = {
+    TO_DO: 'To Do',
+    IN_PROGRESS: 'In Progress',
+    DONE: 'Done'
 }
 
-const defaultStatus = statuses.to_do;
+const DEFAULT_STATUS = STATUSES.TO_DO;
 
 const list = {
-    'read the book': 'To Do',
-    'create a new practice task': 'In Progress',
-    'have a workout': 'To Do',
-    'write a post': 'To Do',
+    'read the book': STATUSES.TO_DO,
+    'create a new practice task': STATUSES.IN_PROGRESS,
+    'have a workout': STATUSES.TO_DO,
+    'write a post': STATUSES.TO_DO
 }
 
 function changeStatus(task, status) {
-    if(task === task) {
+    if (task in list) {
         list[task] = status;
+    } else {
+        console.log('Error: task is not found');
     }
 }
 
 function addTask(task) {
-    list[task] = defaultStatus;
+    if (task in list) {
+        console.log('Error: there is already a task');
+    } else {
+        list[task] = DEFAULT_STATUS;
+    }
 }
 
 function deleteTask(task) {
-   delete list[task];
+    if (task in list) {
+        delete list[task];
+    } else {
+        console.log('Error: task is not found');
+    }
 }
 
 function showList(list) {
@@ -33,41 +43,40 @@ function showList(list) {
     let done = '';
 
     for (let task in list) {
-        if (list[task] === statuses.to_do) {
+        if (list[task] === STATUSES.TO_DO) {
             toDo += `'${task}'\n\t`;
-        } else if (list[task] === statuses.in_progress) {
+        } else if (list[task] === STATUSES.IN_PROGRESS) {
             inProgress += `'${task}'\n\t`;
         } else {
             done += `'${task}'\n\t`;
         }
-}
+    }
 
     if (toDo !== '') {
-        console.log(`To Do:\n\t` + toDo)
+        console.log(`To Do:\n\t` + toDo);
     } else {
-        console.log(`To Do:\n\t-`)
+        console.log(`To Do:\n\t-`);
     }
 
-    if(inProgress !== '') {
-        console.log(`In Progress:\n\t` + inProgress) 
+    if (inProgress !== '') {
+        console.log(`In Progress:\n\t` + inProgress);
     } else {
-        console.log(`In Progress:\n\t-`)
+        console.log(`In Progress:\n\t-`);
     }
 
-    if(done !== '') {
-        console.log(`Done:\n\t` + done) 
+    if (done !== '') {
+        console.log(`Done:\n\t` + done);
     } else {
-        console.log(`Done:\n\t-`)
+        console.log(`Done:\n\t-`);
     }
-    
-} 
+}
 
-changeStatus('create a new practice task', statuses.done)
-changeStatus('read the book', statuses.done)
-changeStatus('have a workout', statuses.done)
+changeStatus('create a new practice task', STATUSES.DONE);
+changeStatus('read the book', STATUSES.DONE);
+changeStatus('have a workout', STATUSES.DONE);
 
-addTask('have a walk')
+addTask('have a walk');
 
-deleteTask('write a post')
+deleteTask('write a post');
 
-showList(list)
+showList(list);

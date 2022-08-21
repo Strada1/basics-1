0a320e14af02acd3e80fea18e5c95b38847f529e
hollywood787 <hollywood7878@yandex.ru>
2022-08-21 14:46:37 +0300

    ToDolist without methods, but with functions

--- danis/todotz.js
+++ danis/todotz.js
@@ -1,64 +1,60 @@
-const todo = {
-    status: {
-      inProgress: "In Progress",
-      done: "Done",
-      toDo: "To Do",
-    },
-    taskitems: {
-  
-    },
-    addTask(nameTask) {
-      this.taskitems[nameTask] = this.status.toDo;
-    },
-    deleteTask(nameTask) {
-      delete this.taskitems[nameTask];
-    },
-    changeStatus(nameTask, status) {
-      for (let key in this.taskitems) {
-        if (nameTask != key) {
-          continue;
-        } else {
-          this.taskitems[nameTask] = status;
-          break;
-        }
+const STATUS = {
+  inProgress: 'In Progress',
+  done: 'Done',
+  toDo: 'To Do',
+}
+
+const list = {
+  "create a new practice task": "In Progress",
+  "make a bed": "Done",
+  "write a post": "To Do",
+};
+
+function addTask(nameTask) {
+  if (nameTask in list) {
+    console.log('Такая задача уже существует');
+  } else {
+    if ((nameTask !== "") && (nameTask.trim().length > 0)) {
+      list[nameTask] = STATUS.toDo;
+    } else {
+      console.log('Некорректный ввод');
+    }
+  }
+}
+
+function deleteTask(nameTask) {
+  if (nameTask in list) {
+    delete list[nameTask];
+  } else {
+    console.log('Такой задачи не существует');
+  }
+}
+
+function changeStatus(nameTask, status) {
+  if (nameTask in list) {
+    list[nameTask] = status;
+  } else {
+    console.log('Такой задачи не существует');
+  }
+}
+
+function showList() {
+  for (let nameStatus in STATUS) {
+    console.log(`${STATUS[nameStatus]}: `);
+    for (let key in list) {
+      if (list[key] == STATUS[nameStatus]) {
+        console.log(' ' + key);
       }
-  
-    },
-    showList() {
-      let toDoResult = '',
-        inProgresResult = '',
-        doneResult = '';
-      for (let key in this.taskitems) {
-        switch (this.taskitems[key]) {
-          case this.status.toDo:
-            toDoResult += key + '\n';
-            break;
-          case this.status.inProgress:
-            inProgresResult += key + '\n';
-            break;
-          default:
-            doneResult += key + '\n';
-        }
-      }
-      console.log(`${this.status.toDo}:\n${toDoResult}`);
-      console.log(`${this.status.inProgress}:\n${inProgresResult}`);
-      console.log(`${this.status.done}:\n${doneResult}`);
-    },
-  };
-  
-  todo.addTask("Сделать домашнее задание");
-  todo.addTask("Покушать");
-  todo.addTask("Попрыгать");
-  todo.addTask("Побегать");
-  todo.addTask("Позаниматься");
-  todo.addTask("Полить цветок");
-  todo.addTask("Поиграть в компьютер");
-  todo.addTask("Поучить js");
-  todo.deleteTask("Поучить js");
-  todo.changeStatus("Поиграть в компьютер", todo.status.done);
-  todo.changeStatus("Позаниматься", todo.status.inProgress);
-  todo.changeStatus("Полить цветок", todo.status.done);
-  todo.changeStatus("Этой задачи не существует", todo.status.done);
-  
-  todo.showList();
-  
+    }
+  }
+}
+
+
+addTask('Пойти бегать');
+addTask('Пойти кушать');
+addTask('Пойти прыгать');
+deleteTask('Пойти кушать');
+changeStatus('Пойти бегать', STATUS.inProgress);
+
+showList();
+

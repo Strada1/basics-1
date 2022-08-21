33509557c2d2af7fcaa86ac57888b3cfdb8e1cb3
Alexey Popov <alexnn0987@mail.ru>
2022-08-21 16:20:54 +0300

    done to check

--- calctest/todo.js
+++ calctest/todo.js
@@ -4,6 +4,7 @@ const list = {
   'write a post': 'To Do',
 };
 
+///////////////////////////////////////////////////////////
 // Меняем статус задачи (меняем значение ключа)
 function changeStatus(task, status) {
   list[task] = status;
@@ -11,6 +12,7 @@ function changeStatus(task, status) {
 changeStatus('write a post', 'Done');
 // console.log(list);
 
+///////////////////////////////////////////////////////////
 // Добавляем новую задачу (добавляем новый ключ)
 function addTask(task) {
   list[task] = 'To Do';
@@ -18,6 +20,7 @@ function addTask(task) {
 addTask('have a walk');
 // console.log(list);
 
+///////////////////////////////////////////////////////////
 // Удаляем задачу (удаляем ключ)
 function deleteTask(task) {
   delete list[task];
@@ -25,3 +28,34 @@ function deleteTask(task) {
 
 deleteTask('have a walk');
 // console.log(list);
+
+////////////////////////////////////////////////////////////
+function showList() {
+  console.log(`Todo: `);
+  for (let key in list) {
+    if (list[key] === 'To Do') {
+      console.log(` \t ${key}`);
+    }
+  }
+  console.log(`In Progress: `);
+  for (let key in list) {
+    if (list[key] === 'In Progress') {
+      console.log(` \t ${key}`);
+    }
+  }
+  console.log(`Done: `);
+  for (let key in list) {
+    if (list[key] === 'Done') {
+      console.log(` \t ${key}`);
+    }
+  }
+}
+
+changeStatus('create a new practice task', 'To Do');
+changeStatus('make a bed', 'To Do');
+changeStatus('write a post', 'In Progress');
+addTask('cleaning in the apartment');
+changeStatus('cleaning in the apartment', 'Done');
+
+// console.log(list);
+showList();

99dee849a8e1b865f350ffa4cdd7b827ae0d6a3f
MaxOldGood <maxoldgood@gmail.com>
2022-08-22 09:26:49 +0700

    ToDo с помощью объектов

---
+++ maxoldgood/todo.js
@@ -0,0 +1,64 @@
+const list = {}
+
+toDo="To Do";
+inProgress="In Progress";
+done="Done";
+
+function changeStatus (task,status) {
+    list[task]=status;
+}
+
+function addTask (newTask) {
+    list[newTask]=toDo;
+}
+
+function deleteTask(taskToDelete) {
+    delete list[taskToDelete];
+}
+
+addTask("Удалить Доту");
+addTask("Изучить JS");
+addTask("Стать крепким джуном");
+addTask("Сделать тудушку");
+addTask("Перестать беспокоиться и начать жить");
+
+changeStatus("Удалить Доту",done);
+changeStatus("Изучить JS",inProgress);
+changeStatus("Сделать тудушку",inProgress);
+changeStatus("Перестать беспокоиться и начать жить",toDo);
+changeStatus("Сделать тудушку",done);
+
+console.log(list);
+
+toDoList= {};
+inProgressList={};
+doneList={};
+
+for (let prop in list) {
+    if (list[prop]===toDo) {
+        toDoList[prop]="added";
+    } else if (list[prop]===inProgress) {
+        inProgressList[prop]="added";
+    } else if (list[prop]===done) {
+        doneList[prop]="added";
+    }
+}
+
+function showList () {
+    console.log (` \n Todo:`);
+    for (let key in toDoList) {
+        console.log(`   "${key}"`);
+    }
+    console.log(`\n In Progress:`);
+    for (let key in inProgressList) {
+        console.log(`   "${key}"`);
+    }
+    console.log(` \n Done:`);
+    for (let key in doneList) {
+        console.log(`   "${key}"`);
+    }
+}
+
+showList();
+
+

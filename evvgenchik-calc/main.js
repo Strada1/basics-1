2b682434caeab3d68d11cf0c7be4a6733ddd8bbf
evvgenchik <yevgenchikk@mail.ru>
2022-08-23 21:47:01 +0300

    String task

--- evvgenchik-calc/main.js
+++ evvgenchik-calc/main.js
@@ -1,61 +1,13 @@
 'use stict'
 
 
-
-const list = {
-	'stydy': 'In progress',
-	'gym': 'ToDo',
-	'eat': 'Done',
-}
-function changeStatus(task, status) {
-	for (let key in list) {
-		if (status == 'In progress' || status == 'ToDo' || status == 'Done') {
-			return list[task] = status
+function showVerticalMessage(str) {
+	for (let i = 0; i < str.length && i < 7; i++) {
+		if (str[0] == 's') {
+			str = str[0].toUpperCase() + str.slice(1)
 		}
-	}
-	alert('don\'t has this status')
-}
-function addTask(task) {
-	list[task] = 'ToDo'
-}
-function deleteTask(task) {
-	for (let key in list) {
-		if (key == task) {
-			return delete list[task]
-		}
-	}
-	alert('don\'t has this task')
-}
-
-function showList() {
-	console.log('ToDo:');
-	for (let key in list) {
-		if (list[key] == 'ToDo') {
-			console.log(`
-							${key}`);
-		}
-	}
-	console.log('In progress:');
-	for (let key in list) {
-		if (list[key] == 'In progress') {
-			console.log(`
-							${key}`);
-		}
-	}
-	console.log('Done:');
-	for (let key in list) {
-		if (list[key] == 'Done') {
-			console.log(`
-							${key}`);
-		}
+		console.log(str[i]);
 	}
 }
 
-addTask('take shower')
-addTask('masturbate')
-addTask('fight')
-changeStatus('take shower', 'Done')
-changeStatus('fight', 'I progress')
-deleteTask('eat')
-deleteTask('big')
-showList()
+showVerticalMessage('stradazzz')

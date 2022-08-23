78cc56fabc5ec11669767ea7703032ea71eebb03
Roman <mikhailovromaaan@gmail.com>
2022-08-23 15:11:44 +0200

    practice3

--- 3ple6xxx/practice1/main.js
+++ 3ple6xxx/practice1/main.js
@@ -1,13 +1,32 @@
-function calc(value,a,b){
-switch (value) {
-    case 'add':
-    return +a + +b;
-    case 'multi':
-    return a * b;
-    case 'subtract':
-    return a - b;
-}
+const operations = {
+    add: '+',
+    multi:'*',
+    subtract: '-',
+    delenie: '/',
+    stepen: '**',
+    ostatok: '%',
+};
+function calc(operator,a,b) {
+    const num1 = Number(a);
+    const num2 = Number(b);
+    switch(operations[operator]) {
+        case '+':
+        return a+b;
+        case '*':
+        return a*b;
+        case '-':
+        return a-b;
+        case '/':
+        return a/b;
+        case '**':
+        return a**b;
+        case '%':
+        return a%b;
+    };
 };
 console.log(calc('add',1,2));
 console.log(calc('multi',1,2));
 console.log(calc('subtract',3,2));
+console.log(calc('delenie',6,2));
+console.log(calc('stepen',3,2));
+console.log(calc('ostatok',7,2));

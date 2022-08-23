0755c36b02162ce89c2b5cc0863a462034223aa4
meVarensev <vitalik.unit@gmial.com>
2022-08-23 15:31:49 +0500

    remade function  showVerticalMessage

--- string/main.js
+++ string/main.js
@@ -1,18 +1,21 @@
 
 function  showVerticalMessage(str) {
     if(!str){
-        console.log('Введите строку');
+        console.log( 'Введите строку' );
         return str;
     }
-    let newStr = str.slice(0,7);
-    newStr = newStr[0].toUpperCase() + newStr.slice(1);
-    if (newStr.startsWith('s') || newStr.startsWith('S')){
-        for (let char  of newStr){
+    let newStr = str.trim();
+    newStr = newStr.slice(0,7);
+    if ( newStr.startsWith('s') || newStr.startsWith('S') ){
+        newStr = newStr[0].toUpperCase() + newStr.slice(1);
+        for ( let char of newStr ){
             console.log(`${char}`);
         }
     } else {
-        console.log(`Cтрока начинается с буквы ${str[0]}, напишите строку начинающуюся с буквы S`)
+        for ( let char of newStr ){
+            console.log(`${char}`);
+        }
     }
 }
 
-showVerticalMessage('stradakkkkkk' );
+showVerticalMessage('stradakkkkkk  авп  пвап  вапв' );

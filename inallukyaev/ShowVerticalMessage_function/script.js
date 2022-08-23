2f86b816bd922c725ee7b5520444c1e0dddbf67c
Inal Lukyaev <lukyaevinal@gmail.com>
2022-08-23 19:45:27 +0300

    fix: refactor of code

--- inallukyaev/ShowVerticalMessage_function/script.js
+++ inallukyaev/ShowVerticalMessage_function/script.js
@@ -1,5 +1,5 @@
 function showVerticalMessage(str = '') {
-  str = str.length > 7 ? str.slice(0, 7) : str;
+  str = str.length > 7 ? str.trim().slice(0, 7) : str.trim();
   let result = '';
   for (let i = 0; i < str.length; i++) {
     if (str[i] === 's' && i === 0) {

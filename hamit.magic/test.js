b9df4d776e7858763d523644df53bd9089baf987
MiSFiTS <hamit.magic@gmail.com>
2022-08-23 12:47:07 +0600

    added  check

--- hamit.magic/test.js
+++ hamit.magic/test.js
@@ -1,10 +1,14 @@
 function showVerticalMessage(str) {
-    let length = str.length < 7 ? str.length : 7;
     let result = '';
-    for (let i = 0; i < length; i++) {
-        if (i == 0) result += str[i].toUpperCase(); 
-        else result += '\n' + str[i].toLowerCase();
+    for (let i = 0; i < Math.min(7, str.length); i++) {
+        if (i == 0) {
+            if (str[i] == `s`) {
+                result += str[i].toUpperCase();
+            }
+            else result += str[i];
+        }
+        else result += '\n' + str[i];
     }
     return result;
 }
-console.log(showVerticalMessage('strYda'));
+console.log(showVerticalMessage('stradaet'));

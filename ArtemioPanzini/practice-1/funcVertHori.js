2560c9750801ba9339b418a258339899ce0b483c
ArtemioPanzini <panferovartemv@gmail.com>
2022-08-23 17:17:27 +0300

    UpperCase: s => S, !s = s;

--- ArtemioPanzini/practice-1/funcVertHori.js
+++ ArtemioPanzini/practice-1/funcVertHori.js
@@ -1,14 +1,19 @@
 function horizntalToVerical (word){
+
+    if(word[0]=='s'){
     console.log(`${word[0]}`.toUpperCase());
+
     for (let i = 1; i < 7; i++){
         if (word[i]!=undefined){
-        console.log(`${word[i]}\t`)
-        }
-    }
+        console.log(`${word[i]}\t`)}}}
+
+    else {for (let i = 0; i < 7; i++){
+        if (word[i]!=undefined){
+        console.log(`${word[i]}\t`)}}}
 }
 
 horizntalToVerical('strada');
 console.log('tamam');
 horizntalToVerical('nestradaem');
 console.log('tamam');
-horizntalToVerical('st123')
+horizntalToVerical('st123ssxd');

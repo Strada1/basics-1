d98a74399c4de2b782461370b2ed646f25cbc372
MiSFiTS <hamit.magic@gmail.com>
2022-08-23 11:39:31 +0600

    fixed problem with unfedind

--- hamit.magic/test.js
+++ hamit.magic/test.js
@@ -1,10 +1,10 @@
 function showVerticalMessage(str) {
     let length = str.length < 7 ? str.length : 7;
-    let result;
+    let result = '';
     for (let i = 0; i < length; i++) {
-        if (i == 0) result += '\n' + str[i].toUpperCase(); 
-        else result += '\n' + str[i];
+        if (i == 0) result += str[i].toUpperCase(); 
+        else result += '\n' + str[i].toLowerCase();
     }
     return result;
 }
-console.log(showVerticalMessage('strada'));
+console.log(showVerticalMessage('strYda'));

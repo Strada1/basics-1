97b40c019e07da770a6c717808b0426da4efda97
Olga <ovovdm@mail.ru>
2022-08-23 19:59:41 +0300

    Add new file

---
+++ calc/string.js
@@ -0,0 +1,10 @@
+function showVerticalMessage(str) {
+  str = str.substring(0, 7);
+  for (let elements of str) console.log(elements);
+
+  if (str[0] === "s") {
+    str = str[0].toUpperCase() + str.slice(1);
+    for (let elements of str) console.log(elements);
+  }
+}
+showVerticalMessage("affrfst");

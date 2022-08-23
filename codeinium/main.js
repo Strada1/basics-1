30fc95da2f85cbdcdf698a0e48a14a9425d5a6d1
codeinium <timofiei.tarasov@gmail.com>
2022-08-23 17:31:39 +0500

    added vertical message function

---
+++ codeinium/main.js
@@ -0,0 +1,20 @@
+function showVerticalMessage(str) {
+    if (str) {
+        str = String(str);
+        str = str[0].toUpperCase() + str.slice(1);
+        if (str.length >= 7) {
+            for (let char of str.slice(0, 7)) {
+                console.log(char); 
+            }
+        } else {
+            for (let char of str) {
+                console.log(char); 
+            }
+        }
+    } else {
+        console.log(str);
+    }
+    
+}
+
+showVerticalMessage();

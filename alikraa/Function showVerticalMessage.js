89455f225422be53b5df21ada0f60d8d9723f82b
Alina <krakhmaalina@gmail.com>
2022-08-24 13:38:46 +0300

    Fixed variable (Function showVerticalMessage)

--- alikraa/Function showVerticalMessage.js
+++ alikraa/Function showVerticalMessage.js
@@ -1,16 +1,16 @@
 function showVerticalMessage(message) {
-    let maxlength = 7;
+    let maxLength = 7;
     let newMessage = '';
 
     if (typeof message !== 'string' || message === '') return false;
 
     if (message[0] === 's') {
-        newMessage = message[0].toUpperCase() + message.slice(1, maxlength)
+        newMessage = message[0].toUpperCase() + message.slice(1, maxLength)
         for (let char of newMessage) {
             console.log(char)
         }
     } else {
-        newMessage = message.slice(0, maxlength)
+        newMessage = message.slice(0, maxLength)
         for (let char of newMessage) {
             console.log(char)
         }

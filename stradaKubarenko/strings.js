ebc4a1bdc8bb5d58578b79d037f51af674328df0
AndreyKubarenko <akubarenko1986@gmail.com>
2022-08-24 16:09:19 +0300

    Буква s по прежнему в нижнем регистре

--- stradaKubarenko/strings.js
+++ stradaKubarenko/strings.js
@@ -2,15 +2,13 @@ function showVerticalMessage(str){
    str = str.toLowerCase();
    let length = str.length;
 
-
-
    if (length => 7) {
       for (let char of str.slice(0,7)) {
          console.log(char);
       }
 
       if (str[0] == 's') {
-         str[0].toUpperCase();
+         str = str[0].toUpperCase + str.slice(1,7);
       } else {}
       
    } else if (length < 7) {

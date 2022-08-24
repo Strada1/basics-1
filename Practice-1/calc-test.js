a32fb56f7ae79dec566474aba2dab49e4327c183
Denis <moonstrike2018@gmail.com>
2022-08-24 11:59:36 +0300

    Practice

---
+++ Practice-1/calc-test.js
@@ -0,0 +1,41 @@
+function showVerticalMessage(str) {
+let strUP = str[0].toUpperCase();
+str = str.substr(1, 6);
+for (let char of strUP) {
+    console.log(char);
+}
+for (let char of str) {
+    console.log(char);
+}
+}
+let str = prompt();
+showVerticalMessage(str);
+/*let calculator = {
+    
+    read(){
+         this.oper1= +prompt("Введите первый операнд", "");
+         this.oper2 = +prompt("Введите второй операнд", "");
+    },
+    
+    opers(){
+     this.operation = prompt("Введите оператор", "");
+    },
+    
+    valid(operation) {
+        if(this.operation == "add") {
+            return this.oper1 + this.oper2;
+        } else if(this.operation == "mult") {
+            return this.oper1 * this.oper2;
+        } else if(this.operation == "sub") {
+            return this.oper1 - this.oper2;
+        } else if(this.operation == "div") {
+            return this.oper1 / this.oper2;
+        }
+    }
+  
+}; Practica
+
+calculator.read();
+calculator.opers();
+console.log(calculator.valid());
+*/

572e86899382cff0c86d9ba805e97b61babf88af
parcanec <igarek200706@gmail.com>
2022-08-24 12:40:35 +0300

    stradaVertical fix

--- stradaVertical.js
+++ stradaVertical.js
@@ -2,10 +2,12 @@ function showVerticalMessage (word) {
     let symbols = word.length
     let newWord
     if (word[0] === 's') {
-        newWord = `${word[0].toUpperCase()}`+`${word.slice(1,word.length)}`
+        newWord = word[0].toUpperCase()+word.slice(1,word.length)
+    } else {
+        newWord = word
     }
     for (i=0; i<7 && i<symbols; i++) {
         console.log(newWord[i])
     }
 }
-showVerticalMessage('stradastrada')
+showVerticalMessage('helloWorld')

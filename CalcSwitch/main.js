3b5f8a599ae5c57d4cd31b8927bc1ca13c12f63b
Ildar <rizvanovir@gmail.com>
2022-08-24 21:01:20 +0300

    Update main.js

--- CalcSwitch/main.js
+++ CalcSwitch/main.js
@@ -1,16 +1,54 @@
+let operations = {
+  add: "+",
+  multi: "*",
+  sub: "-",
+};
 function calc(a, b, action) {
   switch (action) {
-    case "add":
+    case operations.add:
       return a + b;
       break;
-    case "substruct":
+    case operations.sub:
       return a - b;
       break;
-    case "multi":
+    case operations.multi:
       return a * b;
       break;
   }
 }
-console.log(calc(4, 5, "add"));
-console.log(calc(4, 5, "substruct"));
-console.log(calc(4, 5, "multi"));
+console.log(calc(55, 5, operations.add));
+
+/*
+const operations = {
+  add: "+",
+  sub: '-',
+  multi: '*',
+}
+function calc(operator, a, b){
+  if(operator in operations){
+      switch(operations[operator]){
+          case '+':
+              return a + b
+          case '-':
+              return a - b
+          case '*':
+              return a * b;
+      }
+  }else{
+      return null
+  }
+}
+*/
+
+/*let user = {
+  name: "John",
+  age: 30,
+  isAdmin: true
+};
+
+for (let a in user) {
+  // ключи
+  console.log( a );  // name, age, isAdmin
+  // значения ключей
+console.log( user[a] ); // John, 30, true
+}*/

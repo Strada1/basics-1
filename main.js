dfbed42481f139c7795dcb1536ee527a11af9d37
AlinaGost <alinka14011998@mail.ru>
2022-08-24 15:40:32 +0300

    tasks on the theme "Array"

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Строки
 const usFirst = (str) => {
   if (!str) return '-';
   return str[0].toUpperCase() + str.slice(1);
@@ -6,7 +7,6 @@ const usFirst = (str) => {
 console.log(usFirst('nanana'));
 console.log(usFirst('qwerty'));
 console.log(usFirst(''));
-
 
 const checkSpam = (str) => {
   return str.includes('viagra') || str.includes('xxx');
@@ -30,3 +30,50 @@ const showVerticalMessage = (str) => {
 
 showVerticalMessage('slolololali');
 showVerticalMessage('stradada');
+
+// Массивы
+
+const styles = ['джас', 'блюз'];
+styles.push('рок-н-рол');
+console.log('1', styles);
+styles[Math.ceil((styles.length - 1) / 2)] = 'Классика';
+console.log('2', styles);
+const firstElement = styles.shift();
+console.log('3', firstElement);
+console.log('4', styles);
+styles.unshift('Реп', 'Регги');
+console.log('5', styles);
+
+const sumInput = () => {
+ let arrNumbers = [];
+ while (true) {
+   let num = prompt('Введте число', '0');
+   if (num === '' || num === false || num === null) {
+     break;
+   }
+   arrNumbers.push(+num);
+ }
+ console.log(arrNumbers);
+
+ let sum = 0;
+  for (let number of arrNumbers) {
+    sum += number;
+  }
+  return sum;
+}
+
+console.log(sumInput());
+
+const getMaxSubSum = (arr) => {
+  let maxSum = 0;
+  let currentSum = 0;
+
+  for (const item of arr) {
+    currentSum += item;
+    maxSum = Math.max(maxSum, currentSum);
+    if (currentSum < 0) currentSum = 0;
+  }
+  return maxSum;
+}
+
+console.log(getMaxSubSum([1, 2, -2, 3, 4, 0, -5, 4]))

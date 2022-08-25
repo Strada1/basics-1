42fb885300ccf0811459919a441fe9856945e883
MaxOldGood <maxoldgood@gmail.com>
2022-08-25 16:18:26 +0700

    Укоротил код проверки и вывода первого символа

--- maxoldgood/verticalMessage.js
+++ maxoldgood/verticalMessage.js
@@ -1,5 +1,5 @@
 function showVerticalMessage(str) {
-    (str[0]==="s") ? console.log (str[0].toUpperCase()) : console.log (str[0]);
+    console.log ( (str[0]==="s") ? str[0].toUpperCase() : str[ 0]);
     for (let pos=1; pos <= 6; pos++) {
         if (!str[pos]) {break;} 
         else console.log (str[pos]);

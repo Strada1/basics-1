26c9ee9276f8ac619f3d455a1aafe56a0a8019fb
YaRolly <Ea4GoodRu@gmail.com>
2022-08-25 16:37:23 +0300

    homework

---
+++ Array.js
@@ -0,0 +1,13 @@
+function camelize(str) {
+  let strArray = str.split('-');
+  let newStrArray = strArray.map(function (element, index) {
+    if (index !== 0) {
+      element[0].toUpperCase() + element.slice(1);
+    }
+  });
+  let newStr = newStrArray.join();
+  return newStr;
+}
+console.log(camelize('background-color'));
+console.log(camelize('list-style-image') == 'listStyleImage');
+console.log(camelize('-webkit-transition') == 'WebkitTransition');

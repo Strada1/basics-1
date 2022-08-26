cc1dd5103d6eb192c2a943bfca7cdf97e9d6f687
pushtotalkme <x_killer90@mail.ru>
2022-08-26 17:17:51 +0300

    ToDo like array

--- PushToTalk/Todo.js
+++ PushToTalk/Todo.js
@@ -1,39 +1,129 @@
-const Todo = {
-  tasks: {
-    "Task 1": false,
-    "Task 2": false,
-    "Task 3": false,
+const list = [
+  {
+    name: 'create a post', 
+    status: 'In progress', 
+    priority: 'low',
   },
-  
-  changeStatus(task) {
-    if ((task in this.tasks)){
-      this.tasks[task] = !this.tasks[task]
+  {
+    name: 'test', 
+    status: 'Done', 
+    priority: 'medium',
+  },
+  {
+    name: 'write a book', 
+    status: 'To Do', 
+    priority: 'high',
+  },
+  {
+    name: 'buy a car', 
+    status: 'In progress', 
+    priority: 'low',
+  },
+  {
+    name: 'gift for friend', 
+    status: 'Done', 
+    priority: 'high',
+  },
+  {
+    name: 'birthday',  
+    status: 'To Do', 
+    priority: 'medium',
+  },
+]
+
+function changeStatus(name, status, priority) {
+  if (typeof name !== 'string') {
+    throw new Error(`${name} is not a string`);
+  } else if (typeof status !== 'string') {
+    throw new Error(`${status} is not a string`);
+  } else if (typeof priority !== 'string') {
+    throw new Error(`${priority} is not a string`);
+  } else {
+    let index = list.findIndex(post => post.name === name)
+    list[index] = {
+      name: name,
+      status: status,
+      priority: priority
     }
-  },
-  addTask(task) {
-    if (!(task in this.tasks)) {
-      this.tasks[task] = false
-    }
-  },
-  deleteTask(task) {
-    if ((task in this.tasks)){
-      delete this.tasks[task]
-    }
-  },
-  showList() {
-    for (let key in this.tasks) {
-      if (this.tasks[key] === false) {
-        console.log(`${key}: выполняется`)
-      } else {
-        console.log(`${key}: сделано`)
-      }
-    }
-  },
+  }
 }
 
-Todo.changeStatus('Task 1')
-Todo.changeStatus('Task 1')
-Todo.changeStatus('Task 2')
-Todo.deleteTask('Task 3') 
-Todo.addTask('Task 4')
-Todo.showList()
+function addTask(name, status, priority) {
+  if (typeof name !== 'string') {
+    throw new Error(`${name} is not a string`);
+  } else if (typeof status !== 'string') {
+    throw new Error(`${status} is not a string`);
+  } else if (typeof priority !== 'string') {
+    throw new Error(`${priority} is not a string`);
+  } else {
+    list.push({
+      name: name,
+      status: status,
+      priority: priority,
+    })
+  }
+}
+
+function deleteTask(name) {
+  if (typeof name !== 'string') {
+    throw new Error(`${name} is not a string`);
+  } else {
+    let post = list.findIndex(post => post.name === name);
+    list.splice(post, 1)
+  }
+}
+
+function showList() {
+  const toDo = list.filter(post => post.status.includes('To Do'))
+  const inProgress = list.filter(post => post.status.includes('In progress'))
+  const done = list.filter(post => post.status.includes('Done'))
+
+  let toDoStr = 'To Do:\n'
+  let inProgressStr = 'In progress:\n'
+  let doneStr = 'Done:\n'
+
+  if (toDo.length === 0) {
+    doneStr += '   ' + '-'
+  }
+  if (inProgress.length === 0) {
+    doneStr += '   ' + '-'
+  } 
+  if (done.length === 0) {
+    doneStr += '   ' + '-'
+  } 
+
+  toDo.forEach((post, index) => {
+    if (index === toDo.length - 1) {
+      toDoStr += ` ${post.priority.toUpperCase().slice(0, 1)} ` + post.name
+    } else {
+      toDoStr += ` ${post.priority.toUpperCase().slice(0, 1)} ` + post.name + ',' + '\n'
+    }
+  })
+
+  inProgress.forEach((post, index) => {
+    if (index === inProgress.length - 1) {
+      inProgressStr += ` ${post.priority.toUpperCase().slice(0, 1)} ` + post.name
+    } else {
+      inProgressStr += ` ${post.priority.toUpperCase().slice(0, 1)} ` + post.name + ',' + '\n'
+    }
+  })
+
+  done.forEach((post, index) => {
+    if (index === done.length - 1) {
+      doneStr += `   ` + post.name
+    } else {
+      doneStr += `   ` + post.name + ',' + '\n'
+    }
+  })
+
+  console.log(toDoStr)
+  console.log(inProgressStr)
+  console.log(doneStr)
+}
+
+changeStatus('create a post', 'To Do', 'medium')
+changeStatus('gift for friend', 'In progress', 'high')
+changeStatus('buy a car', 'Done', 'low')
+deleteTask('test')
+addTask('watch YouTube video', 'To Do', 'low')
+showList()

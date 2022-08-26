fb207ef3b211a192f4accbe0a73cd054d6bb44a8
Kirill108 <kirill108bol@gmail.com>
2022-08-26 16:34:30 +0200

    need help

--- Kirill108/(26)To do on the massive/main.js
+++ Kirill108/(26)To do on the massive/main.js
@@ -12,7 +12,7 @@ const PRIORITY = {
 
 const list = [  
   {
-    Name: 'create a post', 
+    Name: 'chat with mom', 
     status: STATUS.In_Progress,
     priority: PRIORITY.Low,
   },
@@ -31,12 +31,51 @@ function addTask(task, status, priority) {
 }
 
 function changeStatus(task, NewStatus) {
-   let Index_task = list.findIndex(task);
-   list[Index_task].status = NewStatus;
+   let MyObj = list.find(function(item) {
+   return item.Name == task;
+   })
+
+   MyObj.status = NewStatus;
+}
+
+function changePriority(task, NewPriopity) {
+  let MyObj = list.find(function(item) {
+    return item.Name == task;
+  })
+
+  MyObj.priority = NewPriopity;
+}
+
+function deleteTask(task) {
+  let IndexObj = list.findIndex(function(item) {
+    return item.Name == task;
+  })
+
+  list.splice(IndexObj, 1)
+}
+
+function showList() {
+
+  let GroupTO_Do = '';
+  let GroupIn_Progress= '';
+  let Group_Done = '';
+
+  let obj = list.filter(function(item){
+    return item.status == STATUS.TO_DO;
+  })
+  
+  GroupTO_Do += obj.Name
+  console.log(GroupTO_Do)
 }
 
 
 addTask("Make to do", "In_Progress", "high")
+addTask("test", STATUS.TO_DO, 'medium')
+addTask("tess", STATUS.TO_DO, 'medium')
 changeStatus("test", "In_Progress")
+changeStatus("chat with mom", "To Do")
+changePriority("chat with mom", "High")
+deleteTask("test")
 
+showList(STATUS.TO_DO)
 console.log(list)

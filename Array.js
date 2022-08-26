4c031a71b5dc6bb50ace87f9939049b3a5d14de8
YaRolly <Ea4GoodRu@gmail.com>
2022-08-26 12:21:33 +0300

    ToDo on Array v2.0

--- Array.js
+++ Array.js
@@ -1,30 +1,85 @@
-// learnJs №1
-function camelize(str) {
-  let strArray = str.split('-');
-  let newStrArray = strArray.map(function (element, index) {
-    if (index !== 0) {
-      return element[0].toUpperCase() + element.slice(1);
-    } else {
-      return element;
+const STATUS_TODO = {
+  TO_DO: 'To do',
+  IN_PROGRESS: 'In progress',
+  DONE: 'Done',
+};
+const PRIORITY_TODO = {
+  LOW: 'low',
+  HIGH: 'high',
+};
+const list = [
+  { name: 'create a post', status: 'In progress', priority: 'low' },
+  { name: 'test', status: 'Done', priority: 'high' },
+];
+function changeStatusPriority(task, status, priority) {
+  let pos = list.findIndex((item) => item.name == task);
+  if (status in STATUS_TODO && priority in PRIORITY_TODO && pos !== -1) {
+    list[pos].status = STATUS_TODO[status];
+    list[pos].priority = PRIORITY_TODO[priority];
+  } else {
+    console.log('Error change status priority');
+  }
+}
+function addTask(task, priority = 'LOW') {
+  let pos = list.findIndex((item) => item.name == task);
+  if (pos === -1 && priority in PRIORITY_TODO) {
+    list.push({ name: task, status: STATUS_TODO.TO_DO, priority: priority });
+  } else {
+    console.log('Error add task');
+  }
+}
+function deleteTask(task) {
+  let pos = list.findIndex((item) => item.name == task);
+  if (pos !== -1) {
+    list.splice([pos], 1);
+  } else {
+    console.log('Error delete task');
+  }
+}
+function showList() {
+  console.log(STATUS_TODO.TO_DO + ':');
+  list.forEach((item) => {
+    switch (item.status) {
+      case STATUS_TODO.TO_DO:
+        console.log(' ' + item.name + ',');
+        break;
     }
   });
-  let newStr = newStrArray.join('');
-  return newStr;
-}
-console.log(camelize('background-color'));
-console.log(camelize('list-style-image') == 'listStyleImage');
-console.log(camelize('-webkit-transition') == 'WebkitTransition');
-
-// learnJs №2
-function filterRange(arr, a, b) {
-  return arr.filter(function (item) {
-    return item >= a && item <= b;
+  console.log(STATUS_TODO.IN_PROGRESS + ':');
+  list.forEach((item) => {
+    switch (item.status) {
+      case STATUS_TODO.IN_PROGRESS:
+        console.log(' ' + item.name + ',');
+        break;
+    }
+  });
+  console.log(STATUS_TODO.DONE + ':');
+  list.forEach((item) => {
+    switch (item.status) {
+      case STATUS_TODO.DONE:
+        console.log(' ' + item.name + ',');
+        break;
+    }
+  });
+  console.log(PRIORITY_TODO.HIGH + ':');
+  list.forEach((item) => {
+    switch (item.priority) {
+      case PRIORITY_TODO.HIGH:
+        console.log(' ' + item.name + ',');
+        break;
+    }
+  });
+  console.log(PRIORITY_TODO.LOW + ':');
+  list.forEach((item) => {
+    switch (item.priority) {
+      case PRIORITY_TODO.LOW:
+        console.log(' ' + item.name + ',');
+        break;
+    }
   });
 }
-let arr = [5, 3, 8, 1];
 
-let filtered = filterRange(arr, 1, 4);
-
-console.log(filtered);
-
-console.log(arr);
+changeStatusPriority('create a post', 'TO_DO', 'HIGH');
+addTask('read book', 'HIGH');
+deleteTask();
+showList();

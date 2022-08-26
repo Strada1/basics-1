ec44791e1d73498e39c13f3e19e8c1faebf91200
Akrom-555 <teboev.92@mail.ru>
2022-08-26 21:52:12 +0300

    todo

--- akrom/main.js
+++ akrom/main.js
@@ -6,7 +6,7 @@ const STATUS = {
 
 
 const list =  [{ name: 'create a post', status:STATUS.IN_PROGRESS,  priority: 'low' },
-                   {name: 'test', status: 'Done', priority: 'high'  } 
+                   {name: 'test', status:STATUS.DONE, priority: 'high'  } 
 ] 
 
 
@@ -14,4 +14,5 @@ function addTask (name) {
   list.push({name: name, status:STATUS.TO_DO, priority: 'low'})
 }
 addTask('have a walk')
+
 console.log(list)

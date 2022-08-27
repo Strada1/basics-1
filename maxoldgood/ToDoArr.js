60757431505d63a50d53b8d1cab6d3a0ae261f42
MaxOldGood <maxoldgood@gmail.com>
2022-08-27 11:05:59 +0700

    Added showlist() function

--- maxoldgood/ToDoArr.js
+++ maxoldgood/ToDoArr.js
@@ -1,6 +1,6 @@
 const list = [];
 
-let toDo = "Сделать";
+let toDo = "To Do";
 let inProgress = "In Progress";
 let done = "Done";
 
@@ -30,3 +30,34 @@ function deleteTask(task) {
     let taskToDelete = list.findIndex (item => item.name === task); // Чтобы удалить таск через splice, нам нужен не сам объект, а его индекс в массиве
     list.splice(taskToDelete,1);
 }
+
+
+// 1. Найти все таски со свойством Status="toDo"
+// 2. Получили массив, состоящий только из элементов, где статус toDo
+// 3. Вывести свойство name каждого элемента массива
+// 4. Повторить для остальных статусов
+
+function showlist() {
+    console.log (`\n${toDo}:`)
+        let tasksToDo = list.filter (item => item.status === toDo);
+            for (let tasks of tasksToDo) {
+                console.log (`  ${tasks.name}`);
+            }
+    console.log (`\n${inProgress}:`)
+        let tasksInProgress = list.filter (item => item.status === inProgress);
+             for (let tasks of tasksInProgress) {
+                console.log (`  ${tasks.name}`);
+            }
+    console.log (`\n${done}:`)
+        let tasksDone = list.filter (item => item.status === done);
+             for (let tasks of tasksDone) {
+                console.log (`  ${tasks.name}`);
+            }
+}
+
+addTask("Позавтракать", done, mediumPriority);
+addTask("Почесать жопку", toDo, lowPriority);
+addTask("Поучиться", inProgress, highPriority);
+addTask("Пойти на работу", toDo, highPriority);
+
+showlist();

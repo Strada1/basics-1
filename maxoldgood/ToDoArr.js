82e42e5bb99173a5c92547097324b26c53a33f90
MaxOldGood <maxoldgood@gmail.com>
2022-08-27 10:26:51 +0700

    Added function to Delete a task

--- maxoldgood/ToDoArr.js
+++ maxoldgood/ToDoArr.js
@@ -1,8 +1,8 @@
 const list = [];
 
-let toDo="Сделать";
-let inProgress="In Progress";
-let done="Done";
+let toDo = "Сделать";
+let inProgress = "In Progress";
+let done = "Done";
 
 let lowPriority = "low";
 let highPriority = "high";
@@ -17,12 +17,16 @@ function addTask (newTask, taskStatus, taskPriority) {
 }
 
 function changeStatus (task, newStatus) {
-    let taskToFind = list.find(item => item.name===task); // 1. Найти позицию объекта (элемента массива), в котором свойство name = искомая task
+    let taskToFind = list.find(item => item.name === task); // 1. Найти позицию объекта (элемента массива), в котором свойство name = искомая task
     taskToFind.status = newStatus; // 2. Поменять у найденного объекта свойство status на newStatus
 }
 
 function changePrior(task, newPriority) {
-    let taskToFind = list.find (item =>item.name===task); //1. Найти позицию объекта (элемента массива), в котором свойство name = искомая task
-    taskToFind.priority = newPriority; //2. Поменять у найденного объекта свойство priority на newPriority
+    let taskToFind = list.find (item => item.name === task); 
+    taskToFind.priority = newPriority; 
 }
 
+function deleteTask(task) {
+    let taskToDelete = list.findIndex (item => item.name === task); // Чтобы удалить таск через splice, нам нужен не сам объект, а его индекс в массиве
+    list.splice(taskToDelete,1);
+}

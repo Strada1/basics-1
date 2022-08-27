3790ca864ad38b4eb2b4f67f2af219f007f1e617
meVarensev <vitalik.unit@gmial.com>
2022-08-27 14:50:58 +0500

    todo with array

--- mainToDO.js
+++ mainToDO.js
@@ -10,29 +10,81 @@ const priority = {
 }
 
 let list = [ { name: 'create a post', status: status.STATUS_IN_PROGRESS, priority: priority.PRIORITY_HIGH  },
-             { name: 'test', status: status.STATUS_DONE, priority: priority.PRIORITY_LOW  } ]
+             { name: 'test', status: status.STATUS_TO_DO, priority: priority.PRIORITY_LOW  } ]
 
 
-function addTask (nameTask){
-    list.push({name:nameTask, status: status.STATUS_TO_DO, priority: priority.PRIORITY_LOW});
+function addTask (nameTask , priorities = priority.PRIORITY_LOW){
+    list.push({name:nameTask, status: status.STATUS_TO_DO, priority: priorities});
 }
 
 function deleteTask(nameTask){
-    let result = list.find(function(item) {
-        console.log(nameTask);
-        // test
-        // test
-        // test
-    });
+
+    let result = list.findIndex(item => nameTask === item.name);
+    if ( result === -1 ) {
+        console.log( `Вы не можете удалить ${nameTask} , так как не существует` );
+    } else {
+        return list.splice( result, 1 );
+    }
+
 }
 
 
-addTask("JS")
-deleteTask ("test");
+function changeStatus(nameTask, newStatus) {
 
-console.log(list);
+    let result = list.findIndex(item => nameTask === item.name);
+    if ( result === -1 ) {
+        console.log( `Вы не можете поменять статус ${newStatus} , так как не существует` );
+    } else {
+        return (list[result].status = newStatus);
+    }
+
+}
+
+function changePriority(nameTask, newPriority) {
+
+    let result = list.findIndex(item => nameTask === item.name);
+    if ( result === -1 ) {
+        console.log( `Вы не можете поменять статус ${newPriority} , так как не существует` );
+    } else {
+        return (list[result].priority = newPriority);
+    }
+
+}
 
 
+function showIndividual (status){
+    let count = false;
+    console.log (`\t ${status}:`);
+    let obj = list.filter(item => item.status === status);
+    //console.log(obj);
+    for (let task of obj){
+        if(obj[task] === status.name) {
+          console.log(`\t\t "${task.name}" \n\t\t\t priority: "${task.priority}"`);
+          count = true;
+        }
+    } if (!count) {
+        console.log (`\t\t-`);
+     }
+    count = false;
+}
 
+
+function showList() {
+
+    console.log('ToDO:')
+    showIndividual(status.STATUS_TO_DO);
+    showIndividual(status.STATUS_IN_PROGRESS);
+    showIndividual(status.STATUS_DONE);
+
+}
+addTask("JS",priority.PRIORITY_HIGH );
+addTask("VITALIK",priority.PRIORITY_HIGH );
+changeStatus ("JS" , status.STATUS_TO_DO);
+
+changeStatus ("JS" , status.STATUS_DONE);
+changePriority ("test", priority.PRIORITY_HIGH);
+changePriority ("test", priority.PRIORITY_LOW);
+deleteTask('JS');
+showList();
 
 

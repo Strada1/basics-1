8bb1c2d2edea90e4772e2392571297a129ce79e3
MaxOldGood <maxoldgood@gmail.com>
2022-08-27 10:10:58 +0700

    Created functions for changing status and priority of task

--- maxoldgood/ToDoArr.js
+++ maxoldgood/ToDoArr.js
@@ -1,20 +1,28 @@
 const list = [];
 
-let toDo="Сделать:";
-let inProgress="In Progress:";
-let done="Done:";
+let toDo="Сделать";
+let inProgress="In Progress";
+let done="Done";
 
 let lowPriority = "low";
 let highPriority = "high";
 let mediumPriority = "medium";
 
 function addTask (newTask, taskStatus, taskPriority) {
-    let addedTask = {}
+    let addedTask = {} // Новая задача будет объектом, содержащим три свойства
     addedTask.name = newTask;
     addedTask.status = taskStatus;
     addedTask.priority = taskPriority;
-    list.push(addedTask);
+    list.push(addedTask); // Добавляем созданный объект в качестве элемента в конец массива
 }
-addTask("Сделать задание", toDo, highPriority);
-console.log(list);
+
+function changeStatus (task, newStatus) {
+    let taskToFind = list.find(item => item.name===task); // 1. Найти позицию объекта (элемента массива), в котором свойство name = искомая task
+    taskToFind.status = newStatus; // 2. Поменять у найденного объекта свойство status на newStatus
+}
+
+function changePrior(task, newPriority) {
+    let taskToFind = list.find (item =>item.name===task); //1. Найти позицию объекта (элемента массива), в котором свойство name = искомая task
+    taskToFind.priority = newPriority; //2. Поменять у найденного объекта свойство priority на newPriority
+}
 

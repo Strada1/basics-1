ac059e5eb21ff02115cf2ea1c49bde4f15522f84
Nikita <ifyoucan1337@yandex.ru>
2022-08-27 17:40:31 +0300

    Create "To Do" list by using array

--- MikhailinNik/1.js
+++ MikhailinNik/1.js
@@ -1,5 +1,5 @@
 const STATUS = {
-	IN_PROGRESS: "In Progress",
+	IN_PROGRESS: "In progress",
 	DONE: "Done",
 	TO_DO: "To Do",
 };
@@ -10,38 +10,52 @@ const list = {
 	"write a post": "To Do",
 };
 
-addTask = function (task, status = "In Progress") {
-	list.key = task
-	list[task] = status
+const list1 = [
+	{ name: 'create a post', status: 'In progress', priority: 'low' },
+	{ name: 'test', status: 'Done', priority: 'high' }
+] 
 
-};
+function addTask(task, status, priority) {
+	const newTask = {
+		name: task,
+		status: status,
+		priority: priority,
+	}
 
-changeStatus = function (task, status) {
-	list[task] = status;
-};
+	return list1.push(newTask)
+}
 
-deleteTask = function (task) {
-	delete list[task];
-};
+function changeStatus(task, status = 'In Progress') {
+	const result = list1.find(item => item.name === task);
 
-showList = function () {
+	return result.status = status;
+}
+
+function deleteTask(task) {
+	const findItem = list1.findIndex(item => item.name === task);
+
+	list1.splice(findItem, findItem)
+}
+
+function showList() {
 	let progressList = "";
 	let doneList = "";
 	let toDoList = "";
 
-	for (key in list) {
-		if (list[key] === STATUS.IN_PROGRESS) {
-			progressList += key + "\n";
+	list1.forEach(item => {
+		if (item.status === STATUS.IN_PROGRESS) {
+			progressList += item.name + '\n';
+			
 		}
 
-		if (list[key] === STATUS.DONE) {
-			doneList += key + "\n";
+		if (item.status === STATUS.DONE) {
+			doneList += item.name + "\n";
 		}
 
-		if (list[key] === STATUS.TO_DO) {
-			toDoList += key + "\n";
+		if (item.status === STATUS.TO_DO) {
+			toDoList += item.name + "\n";
 		}
-	}
+	})
 
 	console.log(
 		STATUS.IN_PROGRESS +
@@ -62,13 +76,16 @@ showList = function () {
 			`\n` +
 			toDoList
 	);
-};
+}
 
-addTask("drink cup of tea", "In Progress");
-addTask("learn JS", "To Do");
-
-changeStatus("drink cup of tea", "Done");
-changeStatus("write a post", "Done");
+changeStatus('test', 'Done')
+addTask('learn Js', 'In Progress', 'high');
+addTask('learn Js', 'Done', 'low');
+addTask('cooking', 'To Do', 'high');
+deleteTask('learn Js')
 showList();
+console.log(list1)
+deleteTask('learn Js')
 
-deleteTask("create a new practice task");
+
+

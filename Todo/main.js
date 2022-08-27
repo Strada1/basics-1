46ef0c08e3be05eedec43bbd3a932313fda069dd
Alina239 <alinochka239@gmail.com>
2022-08-27 00:53:41 -0400

    Todo with arrays

--- Todo/main.js
+++ Todo/main.js
@@ -3,42 +3,83 @@ const STATUS = {
     IN_PROGRESS: 'In Progress',
     DONE: 'Done'
 };
-const list = {
-    'create a new practice task': STATUS.IN_PROGRESS,
-    'make a bed': STATUS.DONE,
-    'write a post': STATUS.TO_DO,
+const PRIORITY = {
+    HIGH: 'High',
+    MEDIUM: 'Medium',
+    LOW: 'Low',
 }
 
-
-function addTask(list, task, status = STATUS.TO_DO) {
-    list[task] = status;
+function addTask(list, task, status = STATUS.TO_DO, priority = PRIORITY.LOW) {
+    if (task !== undefined)
+    list.push({name: task, status: status, priority: priority});
 }
 
-function changeStatus(list, task, newStatus = STATUS.DONE) {
-    list[task] = newStatus;
+function changeStatus(list, task, newStatus = STATUS.DONE,) {
+    let changeStatusHappened = false;
+    for (let item in list) {
+        if (item !== undefined && list[item].name === task) {
+            list[item].status = newStatus;
+            changeStatusHappened = true;
+        }
+    }
+    if (!changeStatusHappened){
+        console.log('Status not changed');
+    }
 }
 
 function deleteTask(list, task) {
-    delete list[task];
+    let deleteHappened = false;
+    for (let item in list) {
+        if (item !== undefined && list[item].name === task) {
+            delete list[item];
+            deleteHappened = true;
+        }
+    }
+    if (!deleteHappened){
+        console.log("delete did not happen");
+    }
 }
 
-const space = '     ';
 
 function showList(list) {
     for (let keySTATUS in STATUS) {
         console.log(STATUS[keySTATUS]);
-        for (let key in list) {
-            if (list[key] === STATUS[keySTATUS]) {
-                console.log(space + key);
+        for (let item in list) {
+            if (list[item].status === STATUS[keySTATUS]) {
+                console.log('     ' + list[item].name);
             }
         }
     }
-    console.log('________________');
 }
 
+function showListOfPriority(list) {
+    for (let key in PRIORITY) {
+        console.log(PRIORITY[key]);
+        for (let item in list) {
+            if (list[item].priority === PRIORITY[key]) {
+                console.log('     ' + list[item].name);
+            }
+        }
+    }
+}
 
+const list = [
+    {
+        name: 'create a post',
+        status: STATUS.IN_PROGRESS,
+        priority: PRIORITY.LOW
+    },
+    {
+        name: 'test',
+        status: STATUS.DONE,
+        priority: PRIORITY.HIGH
+    }
+]
+
+showList(list);
 addTask(list, 'task1');
 addTask(list, 'task2', STATUS.IN_PROGRESS);
-showList(list);
+addTask(list, 'task3', STATUS.TO_DO, PRIORITY.MEDIUM);
 deleteTask(list, 'task2')
-showList(list);
+deleteTask(list, 'task2')
+showListOfPriority(list);

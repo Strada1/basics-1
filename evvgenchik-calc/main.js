66af44ef6a0db5522eae277474fa47c15d85c8fb
evvgenchik <yevgenchikk@mail.ru>
2022-08-28 10:40:17 +0300

    delete

--- evvgenchik-calc/main.js
+++ evvgenchik-calc/main.js
@@ -1,13 +1,3 @@
 'use stict'
 
 
-function showVerticalMessage(str) {
-	for (let i = 0; i < str.length && i < 7; i++) {
-		if (str[0] == 's') {
-			str = str[0].toUpperCase() + str.slice(1)
-		}
-		console.log(str[i]);
-	}
-}
-
-showVerticalMessage('strada')

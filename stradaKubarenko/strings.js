8b4d3248fd235faa9732300585c50d64e79dc1a4
AndreyKubarenko <akubarenko1986@gmail.com>
2022-08-28 16:30:20 +0300

    Вертикальный вывод слова Strada

--- stradaKubarenko/strings.js
+++ stradaKubarenko/strings.js
@@ -2,9 +2,11 @@ function showVerticalMessage(string){
 
    let stringNew = string.toLowerCase();
    stringNew = stringNew.slice(0,6)
+
    if (stringNew[0] === 's') {
       stringNew = stringNew[0].toUpperCase() + stringNew.slice(1,6)
    }
+   
    for (let char of stringNew) {
          console.log(char)
    }

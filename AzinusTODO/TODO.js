16203d4ba7985c6a6612019bf56ee15c4bc66677
Aziza Kadyrova <azziza.kadyrova@gmail.com>
2022-08-29 18:48:36 +0600

    Функция добавления драфт

--- AzinusTODO/TODO.js
+++ AzinusTODO/TODO.js
@@ -1,28 +1,15 @@
-const list = [ 
-    { name: 'create a post', status: 'In progress', priority: 'low'}, 
-    { name: 'test', status: 'Done', priority: 'high'  } ] ;
-
-const PRIORITY = {
-    low:"low",
-    high:"high",
-}
+const list = [
+    { name: 'create a post', status: 'In progress', priority: 'low' },
+    { name: 'test', status: 'Done', priority: 'high' }];
 
    function addTask(task){
  if (typeof task === 'string'){
-    list.push({name: task, status:toDO, priority: PRIORITY[low]});
+    list.push({name: 'New task', status:'New', priority:'low'});
     console.log ('Your task has been added');
  }else{
-    console.log('Error', +'/n' + 'There is no any task' )
- }
-    
-    }
-function deleteTask(task){
-    let result = list.findIndex(function(index) {
-        return index === task;
-        });
-    list.splice(result);
-    console.log("The task is deleted");
+    console.log('Error', +'/n' + 'There is no any task' );
+ }   
     }
 
-   
-   
+    addTask('New task');
+    console.log(list);

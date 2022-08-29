572857137d53bb7c241fb938314855c3f20070d1
IgorD <igordemyan@gmail.com>
2022-08-29 23:13:53 +0300

    ToDo massive fixes

--- ToDo/ToDo massive/main.js
+++ ToDo/ToDo massive/main.js
@@ -3,7 +3,7 @@ const STATUS = {
     INPROGRESS: 'In Progers',
     DONE: 'Done',
    }
-   
+
 const PRIORITY = {
 	HIGH: 'high',
 	LOW: 'low'
@@ -14,31 +14,46 @@ const list = [ {
 	name: 'test', status: STATUS.TODO, priority: PRIORITY.HIGH },{ 
 	name: 'read a book', status: STATUS.DONE, priority: PRIORITY.HIGH }
 ];
-	
+
+
+
+function taskName(name) {
+	return list.findIndex(item => item.name === name);
+	 
+}
+
+function isNotValid (name, status, priority) {
+	const isNotValid = (name !== undefined && status !== undefined && typeof name === 'string' && typeof status === 'string' && typeof priority === 'string');
+	return isNotValid;
+}
+
 function addTask(name, status = STATUS.INPROGRESS, priority = PRIORITY.HIGH) {
-	list.push({name, status, priority})
+	if (isNotValid(name, status, priority)) {
+		return list.push({name, status, priority})
+	} else {
+		console.log('Вы ввели не правельное значение')
+	}
 }
 
 function deleteTask(name) {
-	if (list.find(item => item.name == name)) {
-		list.splice(list.findIndex(item => name == item.name),1);
+	if (taskName) {
+		list.splice(taskName(name),1);
 	} else {
 		return console.log('Данной задачи нет в списке!')
 	}
-		
 }
 
-function changeStatus (name, status = STATUS.INPROGRESS) {
-	let result = list.findIndex(item => item.name == name) 
+function changeStatus (name, newStatus = STATUS.INPROGRESS) {
+	let result = taskName(name); 
 	if (result === -1 ) {
 		console.log('Данной задачи нет в списке. Вы не можете изменить статус для несуществующей задачи.')
 	} else {
-		return list[result].status = status;
+		return list[result].status = newStatus;
 	}
 }
 
 function changePriority (name, priority) {
-	let result = list.findIndex(item => item.name == name);
+	let result = taskName(name);
 	if (result === -1 ) {
 	console.log('Данной задачи нет в списке. Вы не можете изменить приоритет для несуществующей задачи.')
 	} else {
@@ -47,31 +62,19 @@ function changePriority (name, priority) {
 }
 
 function showList() {
-
-	console.log(`${STATUS.DONE}:`)
-	for (let task of list ) {
-		if (task.status === STATUS.DONE) {
-			console.log(` -Задача: ${task.name}, Приортет: ${task.priority}.`);
-		}
-	}
-	console.log(`${STATUS.TODO}:`)
-	for (let task of list ) {
-		if (task.status === STATUS.TODO) {
-			console.log(` -Задача: ${task.name}, Приортет: ${task.priority}.`);
-		}
-	}
-	console.log(`${STATUS.INPROGRESS}:`)
-	for (let task of list ) {
-		if (task.status === STATUS.INPROGRESS) {
-			console.log(` -Задача: ${task.name}, Приортет: ${task.priority}.`);
-		}
+	for (let nameStatus in STATUS){
+		console.log(`${STATUS[nameStatus]}:`);
+		list.filter(function (item) {
+			if(item.status === STATUS[nameStatus]) {
+			console.log(` -Задача: ${item.name}, Приортет: ${item.priority} `);
+			}
+		});
 	}
 }
 changePriority ('test', PRIORITY.LOW) ;	
 changeStatus ('test', STATUS.DONE);
 deleteTask('test');
-addTask('make a bad');
+addTask(123);
+addTask('time to rest');
 addTask('go for a walk', STATUS.TODO);
-
-
 showList();

ebf9b578d036f01bf6800353ecdf78510cad2270
Aziza Kadyrova <azziza.kadyrova@gmail.com>
2022-08-30 13:16:52 +0600

    функция добавления задачи с приоритетом

--- AzinusTODO/TODO.js
+++ AzinusTODO/TODO.js
@@ -2,18 +2,26 @@ const list = [
     { name: 'create a post', status: 'In progress', priority: 'low' },
     { name: 'test', status: 'Done', priority: 'high' }];
 
-   function addTask(task){
+   function addTask(task, priority){
  if (typeof task === 'string'){
-
-    list.push({name: task, status:'New', priority:'low'});
+    if (priority === 'high'){
+    priority.high;
+    }else{
+    priority = 'low';
+    }
+    list.push({name: task, status:'New', priority: priority,});
     console.log ('Your task has been added');
  }else{
     console.log('Error', +'/n' + 'There is no any task' );
  }   
     }
 
-    addTask('New task');
+
+    
+    addTask('New task', 'high');
     addTask('New task-2');
+    addTask('New task-3','high');
+
     console.log(list);
 
     

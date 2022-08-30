0cae8370c92a07702cadd52374e8c95ba699410c
Aziza Kadyrova <azziza.kadyrova@gmail.com>
2022-08-30 14:56:44 +0600

    функция добавления с тернарными операторами исправленный

--- AzinusTODO/TODO.js
+++ AzinusTODO/TODO.js
@@ -1,21 +1,21 @@
 const list = [
     { name: 'create a post', status: 'In progress', priority: 'low' },
-    { name: 'test', status: 'Done', priority: 'high' }];
+      { name: 'test', status: 'Done', priority: 'high' }];
 
-   function addTask(task, priority){
+function addTask(task, priority){
  if (typeof task === 'string'){
-    'priority' + (priority? 'high': 'low');
-    list.push({name: task, status:'New', priority: priority,});
+ (priority === 'high')?priority ='high':priority = 'low';
+ list.push({name: task, status:'New', priority: priority,});
     console.log ('Your task has been added');
  }else{
-    console.log('Error', +'/n' + 'There is no any task' );
+   console.log('Error', +'/n' + 'There is no any task' );
  }   
-    }
+ }
 
 
     
     addTask('New task', 'high');
-    addTask('New task-2','low');
+    addTask('New task-2',);
     addTask('New task-3','high');
 
     console.log(list);

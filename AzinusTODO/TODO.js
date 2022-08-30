0c6f899144e4fbb59e7e910aa7c29f75f3202b20
Aziza Kadyrova <azziza.kadyrova@gmail.com>
2022-08-30 22:26:24 +0600

    неправильный код проверки на вводимую задачу

--- AzinusTODO/TODO.js
+++ AzinusTODO/TODO.js
@@ -5,25 +5,22 @@ const list = [
 function addTask(task, priority){
  if (typeof task === 'string'){
    const taskPriority = priority === 'high' ?  'high': 'low';
-   for (let i = 0; i < list.length; i++){
-      if(task.indexOf(list [i]) === - 1){
-       console.log ('no, that has been listed already')
-      }
-   }
-   
+   let = taskActuality = list.find(el => el.length == -1);
+       console.log ('no, that has been listed')
+      
+    
   list.push({name: task, status:'New', priority: taskPriority,});
     console.log ('Your task has been added');
  }else{
    console.log('Error', +'/n' + 'There is no any task' );
  }   
- }
+ 
+}
 
-
-    addTask('New task-2',);
+    
     addTask('New task', 'high');
     addTask('New task-2',);
     addTask('New task-3','high');
 
     console.log(list);
 
-    

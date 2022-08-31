1e733ce4a9c13bb2c7e385fc138138be608a1876
YaRolly <Ea4GoodRu@gmail.com>
2022-08-31 16:14:14 +0300

    homework #31 v.1.1

--- main.js
+++ main.js
@@ -1,57 +1,25 @@
-const list = {
-  'read book': 'to begin',
-  'learn JS': 'in progress',
-  'buy bread': 'complete',
-};
-function changeStatus(task, status) {
-  list[task] = status;
-}
-function addTask(task) {
-  list[task] = 'to begin';
-}
-function deleteTask(task) {
-  delete list[task];
-}
-function showList() {
-  let result = '';
-  for (let task in list) {
-    result = result + task + ': ' + list[task] + '\n';
+// homeWork Learn.js №1 from setInterval
+function printNumbers(from, to) {
+  for (; from <= to; from++) {
+    console.log(from);
   }
-  return result;
 }
-changeStatus('read book', 'in progress');
-addTask('run');
-deleteTask('buy bread');
-console.log(showList());
-// ToDo on .This
-const list = {
-  'read book': 'to begin',
-  'learn JS': 'in progress',
-  'buy bread': 'complete',
-};
-function changeStatus(task, status) {
-  this[task] = status;
-}
-function addTask(task) {
-  this[task] = 'to begin';
-}
-function deleteTask(task) {
-  delete this[task];
-}
-function showList() {
-  let result = '';
-  for (let task in this) {
-    if (typeof this[task] === 'string') {
-      result = result + task + ': ' + this[task] + '\n';
-    }
+let timerId = setInterval(printNumbers, 100, 0, 10);
+setTimeout(() => clearInterval(timerId), 100);
+
+// from setTimeout
+
+function printNumbers(from, to) {
+  for (; from <= to; from++) {
+    console.log(from);
   }
-  return result;
 }
-list.changeStatus = changeStatus;
-list.addTask = addTask;
-list.deleteTask = deleteTask;
-list.showList = showList;
-list.changeStatus('read book', 'in progress');
-list.addTask('run');
-list.deleteTask('buy bread');
-console.log(list.showList());
+let timerId = setTimeout(
+  function run() {
+    printNumbers;
+    setTimeout(printNumbers, 100, 0, 10);
+  },
+  100,
+  0,
+  10,
+);

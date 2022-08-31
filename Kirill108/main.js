f251e07918718e82d76797275d9d2ff9b4ab6d4e
Kirill108 <kirill108bol@gmail.com>
2022-08-31 18:52:30 +0200

    need help

--- Kirill108/main.js
+++ Kirill108/main.js
@@ -1,26 +1,8 @@
 function printNumbers(from, to) {
-	
-	let arrFromTo = [from, to]
-
-	let i = 0;
-	while (i < to) {
-		if (arrFromTo[i] < to) {
-			arrFromTo.splice(i, 0, arrFromTo[i] + 1 )
-			i++;
-		}
+	for (let i = 0; from + i <= to; i++) {
+		let timerId = setInterval(() => console.log(from + i), 1000)
+		clearInterval(timerId)
 	}
-		
-	function printArr() {
-		for (i; i < to; i++) {
-			console.log(arrFromTo[i] + ",")
-		}
-	}
-
-	let timerId = setInterval(printArr, 1000)
-
-	if (i == to) {
-		clearTimeout(timerId)
-	}
-	console.log(arrFromTo)
 }
-printNumbers(1, 10)
+	
+printNumbers(4, 10)

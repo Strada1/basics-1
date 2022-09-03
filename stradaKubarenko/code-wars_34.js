fef829b4d2812fe3af3b9e9f5fca787608da794f
AndreyKubarenko <akubarenko1986@gmail.com>
2022-09-03 13:00:49 +0300

    как изменить код под тест на codewars?

--- stradaKubarenko/code-wars_34.js
+++ stradaKubarenko/code-wars_34.js
@@ -1,11 +1,11 @@
-const marks = [2,2,2,2];
+let marks = [2,2,2,2];
 
 function getAverage(marks){
    let sum = marks.reduce((accumulator, obj) => accumulator + obj,0);
    let average = sum / marks.length;
    let averageRound = Math.round(average);
    console.log(averageRound);
-}
+};
 getAverage(marks);
 
 marks = [1,2,3,4,5,];

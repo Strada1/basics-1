c674c183211c23b35cb66aad71622a3022ec1002
Kirill108 <kirill108bol@gmail.com>
2022-09-05 16:23:44 +0200

    w

--- Kirill108/main.js
+++
@@ -1,8 +0,0 @@
-let timerId = setTimeout(function printNumbers(from, to) {
-	let counter = from;
-	
-	console.log(counter);
-	counter++;
-
-	timerId = setTimeout(printNumbers, 1000, 4, 10); // (*)
-  }, 1000);

7bace70728944e1fa389949ecd4be4264892d91e
Kirill108 <kirill108bol@gmail.com>
2022-09-07 10:58:08 +0200

    updated

---
+++ калькулятор (2)/main.js
@@ -0,0 +1,26 @@
+let button = document.getElementById("button")
+let result = document.getElementById("result")
+
+button.addEventListener("click", calc)
+
+	function calc () {
+		let number1 = document.getElementById("number1").value;
+		let number2 = document.getElementById("number2").value;
+		let operation = document.getElementById("operation").value;
+		let results;
+		
+		switch (operation) {
+			case '+':
+				results = (+number1 + +number2);
+				result.innerHTML = +results
+				break;
+			case '*': 
+				results = (+number1 * +number2);
+				result.innerHTML = +results
+				break;
+			case '-': 	
+				results = (+number1 - +number2);
+				result.innerHTML = +results
+				break;
+	}
+}

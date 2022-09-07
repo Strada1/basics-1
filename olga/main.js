6cd55cbdadfa6bddb5be9918a106d298959b4010
Olga <ovovdm@mail.ru>
2022-09-07 10:02:44 +0300

    Add calculate files

---
+++ olga/main.js
@@ -0,0 +1,27 @@
+const result = document.querySelector(".result");
+const valueOne = document.querySelector(".calculate__value--one");
+const valueTwo = document.querySelector(".calculate__value--two");
+const calculateSelect = document.querySelector(".calculate__select");
+const total = document.querySelector("span");
+
+function calc(operator, a, b) {
+	console.log(a);
+	switch (operator) {
+		case "+":
+			return +a + +b;
+		case "*":
+			return a * b;
+		case "-":
+			return a - b;
+		default:
+			console.log("Ошибка");
+	}
+}
+
+result.addEventListener("click", function () {
+	if (valueOne.value != "" && valueTwo.value != "") {
+		total.textContent = +calc(calculateSelect.value, valueOne.value, valueTwo.value).toFixed(2);
+	} else {
+		total.textContent = "Заполните поле";
+	}
+});

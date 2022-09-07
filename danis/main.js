fdefe5cf35f1430a0e5aa4bb85d91bb48278f094
hollywood787 <hollywood7878@yandex.ru>
2022-09-07 08:02:11 +0300

    Calc + UI

---
+++ danis/main.js
@@ -0,0 +1,35 @@
+let equals = document.querySelector("#equals");
+let result = document.querySelector(".result");
+
+
+equals.addEventListener("click", function () {
+  let operator = document.querySelector("#identifier").value;
+  let firstNumber = document.querySelector(".a").value;
+  let secondNumber = document.querySelector(".b").value;
+
+  if ((firstNumber == "") || (secondNumber == "")) {
+    result.textContent = "Пожалуйста введите все числа";
+    result.style.color = "red";
+  } else {
+    result.textContent = calc(operator, firstNumber, secondNumber);
+    result.style.color = "green";
+  }
+});
+
+function calc(identifier, a, b) {
+  switch (identifier) {
+    case "+":
+      return Number(a) + Number(b);
+
+    case "*":
+      return a * b;
+
+    case "-":
+      return a - b;
+
+    case "/":
+      return a / b;
+
+  }
+}
+

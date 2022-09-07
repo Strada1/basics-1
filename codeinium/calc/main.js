d64266bfe3662f0511a07d37aced87feeb69602c
codeinium <timofiei.tarasov@gmail.com>
2022-09-07 20:31:00 +0500

    final version 2 calc

--- codeinium/calc/main.js
+++ codeinium/calc/main.js
@@ -8,24 +8,42 @@ function calc() {
     switch (operation) {
         case "+": 
             result = a + b;
-            answer.textContent = result;
-            break;
+            if (!isNaN(result)){
+                answer.textContent = result;
+                break;
+            } else {
+                answer.textContent = 'ошибка при сложении'
+                break;
+            }
         case "-": 
             result = a - b;
-            answer.textContent = result;
-            break
+            if (!isNaN(result)){
+                answer.textContent = result;
+                break;
+            } else {
+                answer.textContent = 'ошибка при вычитании'
+                break;
+            }
         case "*":
             result = a * b;
-            answer.textContent = result;
-            break;
+            if (!isNaN(result)) {
+                answer.textContent = result;
+                break;
+            } else {
+                answer.textContent = 'ошибка при умножении'
+                break;
+            }
         case "/":
             result = (a / b).toFixed(3);
-            answer.textContent = result;
-            break;
-        default: 
-            console.log('error');
+            if (!isFinite(result)) {
+                answer.textContent = 'ошибка при делении';
+                break;
+            } else {
+                result = (a / b).toFixed(3);
+                answer.textContent = result;
+                break;
+            }
     } 
 }
-
 
 button.addEventListener('click', calc);

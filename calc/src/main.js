f533b2067526f8f94a14a11df6fadcb9fcfcfdae
meVarensev <vitalik.unit@gmial.com>
2022-09-07 17:51:48 +0500

    no fixed

---
+++ calc/src/main.js
@@ -0,0 +1,79 @@
+let equals = document.querySelector(".equals");
+let result = document.querySelector(".result");
+
+
+function clear() {
+    document.querySelector(".firstNumber").value = '';
+    document.querySelector(".secondNumber").value = '';
+    document.querySelector(".result").value = result.textContent = '';
+
+}
+document.querySelector(".clear").onclick = clear;
+
+function checkInput(firstNumber,secondNumber) {
+    let checkInput = (firstNumber === "" || secondNumber === "" )
+    if (checkInput) {
+        return true;
+    }
+}
+
+
+// function MyaddEventListener(){
+//     let operation = document.querySelector(".operation").value;
+//     let firstNumber = document.querySelector(".firstNumber").value;
+//     let secondNumber = document.querySelector(".secondNumber").value;
+//     if ( checkInput(firstNumber,secondNumber) ) {
+//         console.log("Is not number");
+//         result.textContent = "Is not number";
+//     } else {
+//         let calc = calcObject(operation, firstNumber, secondNumber);
+//         result.textContent =  Number(calc.toFixed(5));
+//     }
+// }
+// //document.querySelector(".equals").onclick = MyaddEventListener;
+// equals.addEventListener('click' ,MyaddEventListener);
+
+equals.addEventListener("click", function () {
+    let operation = document.querySelector(".operation").value;
+    let firstNumber = document.querySelector(".firstNumber").value;
+    let secondNumber = document.querySelector(".secondNumber").value;
+
+    if ( checkInput(firstNumber,secondNumber) ) {
+        console.log("Is not number");
+        result.textContent = "Is not number";
+    } else {
+        let calc = calcObject(operation, firstNumber, secondNumber);
+        result.textContent =  Number(calc.toFixed(5));
+    }
+});
+
+
+
+const operations = {
+    add: 'add',
+    multi:'multi',
+    subtract: 'subtract',
+    divide: 'divide'
+};
+
+
+function calcObject( operation, firstNumber, secondNumber ) {
+
+    switch (operation) {
+        case (operations.add):
+            return +firstNumber + +secondNumber;
+        case (operations.multi):
+            return  firstNumber * secondNumber;
+        case (operations.subtract):
+            return  firstNumber - secondNumber;
+        case (operations.divide):
+            if (secondNumber === "0"){
+                result.textContent = "На ноль делить нельзя";
+            } else {
+                return  firstNumber / secondNumber;
+            }
+        default:
+            return "unknown operation";
+    }
+
+}

0e466e54cf90d247d57d7ae5111541dfa18b631d
terlyga78 <telyga78@gmail.com>
2022-09-08 12:10:49 +0300

    Simple calc

--- terlyga78/script.js
+++ terlyga78/script.js
@@ -1,5 +1,26 @@
+function runCalc() {
+    const firstNumber = document.getElementById("firstNumber");
+    const operation = document.getElementById("operation");
+    const secondNumber = document.getElementById("secondNumber");
+    const result = document.getElementById("result");
+
+    let firstValue = convertStringToNumber(firstNumber.value);
+    let secondValue = convertStringToNumber(secondNumber.value);
+
+    if ((isNaN(firstValue) || (isNaN(secondValue)))) {
+        alert("Operand must be Number");
+        return;
+    }
+
+    result.value = calc(operation.value, firstValue, secondValue);
+}
+
+function convertStringToNumber(text) {
+    return +text;
+}
+
 function calc(operation, num1, num2) {
-    const operations = { add: '+', multi: '*', subtract: '-' };
+    const operations = { add: '+', multi: '*', subtract: '-', divide: '/' };
     let result;
     switch (operation) {
         case operations.add:
@@ -11,12 +32,11 @@ function calc(operation, num1, num2) {
         case operations.subtract:
             result = num1 - num2;
             break;
+        case operations.divide:
+            result = num1 / num2;
+            break;
         default:
             result = undefined;
     };
     return result;
 }
-
-document.writeln(`1 + 2 = ${calc('+', 1, 2)} <br>`);
-document.writeln(`1 * 2 = ${calc('*', 1, 2)} <br>`);
-document.writeln(`3 - 2 = ${calc('-', 3, 2)} <br>`);

448e7a14f664746a45eb41d61d052874a8ca8b3a
Alexk3k <valderrama06@gmail.com>
2022-09-08 20:09:03 +0300

    reworked

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ function calc (){
     let number1 = document.getElementById("num1").value;
     let number2 = document.getElementById("num2").value;
     let operation = document.getElementById("operation").value;
-    let result = document.getElementById("result").value;
-    
+    let results
+
     
     if (number1 === "" || number2 === ""){
         alert("Write number");
@@ -17,27 +17,27 @@ function calc (){
         switch(operation){
             
             case "+":
-                result = +(+number1 + +number2);
-                result.textContent = +result.toFixed(10);
+                results = (+number1 + +number2);
+               result.innerHTML = +results
                 break;
                 
                 case "-":
-                    result = +(+number1 - +number2);
-                    result.textContent = +result.toFixed(10);
-                    break
+                    results = (+number1 - +number2);
+                    result.innerHTML = +results
+                    break;
                     
                     case "*":
-                        result = +(+number1 * +number2);
-                        result.textContent = +result.toFixed(10);
-                        break
+                        results = (+number1 * +number2);
+                       result.innerHTML = +results
+                        break;
                         
                         case "/":
-                            result = +(+number1 / +number2);
-                            result.textContent = +result.toFixed(10);
+                            results = (+number1 / +number2);
+                            result.innerHTML = +results
                             if (+number2 === 0 ){
                                 alert ("no!")
                             }
-                            break
+                            break;
                             
                         }
                     }

7364f76aa12a4018ae204c28b9748e2fe7c53dc8
Olga <ovovdm@mail.ru>
2022-09-09 09:29:47 +0300

    Transformation calculate

--- olga/main.js
+++ olga/main.js
@@ -3,7 +3,17 @@ const valueOne = document.querySelector(".calculate__value--one");
 const valueTwo = document.querySelector(".calculate__value--two");
 const calculateSelect = document.querySelector(".calculate__select");
 const total = document.querySelector("span");
+const resultElement = document.querySelector(".calculate__result");
 
+function createEl(totalValue) {
+	let div = document.createElement("div");
+	div.className = "total";
+	resultElement.append(div);
+	div.textContent = totalValue;
+	div.addEventListener("click", function () {
+		div.remove();
+	});
+}
 function calc(operator, a, b) {
 	console.log(a);
 	switch (operator) {
@@ -20,7 +30,9 @@ function calc(operator, a, b) {
 
 result.addEventListener("click", function () {
 	if (valueOne.value != "" && valueTwo.value != "") {
-		total.textContent = +calc(calculateSelect.value, valueOne.value, valueTwo.value).toFixed(2);
+		createEl(
+			(total.textContent = +calc(calculateSelect.value, valueOne.value, valueTwo.value).toFixed(2))
+		);
 	} else {
 		total.textContent = "Заполните поле";
 	}

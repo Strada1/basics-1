6d58888e6a355657e6d56424d0a5250f5439854d
Isaac <aziksimpson@gmail.com>
2022-09-10 01:50:03 +0300

    document modification

--- sinonaz/js/main.js
+++ sinonaz/js/main.js
@@ -5,6 +5,7 @@ const firstInput = calculator.querySelector(`.js-first-operand`);
 const secondInput = calculator.querySelector(`.js-second-operand`);
 const operator = calculator.querySelector(`.js-operator`);
 const value = calculator.querySelector(`.js-value`);
+const list = calculator.querySelector(`.js-list`);
 
 function calc() {
     let firstOperand = +firstInput.value;
@@ -25,6 +26,21 @@ function calc() {
         default:
             console.log(`Error`)
     }
+
+    list.insertAdjacentHTML(`beforeend`, `<li class="js-item">${value.innerHTML}</li>`);
+
+    const items = calculator.querySelectorAll(`.js-item`);
+
+    items.forEach((item) => {
+
+        function deleteItem() {
+            item.remove();
+        }
+
+        item.addEventListener(`click`, deleteItem);
+    });
 }
+
+
 
 calcButton.addEventListener(`click`, calc);

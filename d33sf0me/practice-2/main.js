623067c4d4dcb9b6edc77132a89e739e09b95c43
d33sf0me <katekrokh@gmail.com>
2022-09-09 18:00:19 +0300

    Adding calculator js+html

---
+++ d33sf0me/practice-2/main.js
@@ -0,0 +1,31 @@
+let button = document.getElementById('button');
+let result = document.querySelector(".result");
+button.addEventListener('click', calc)
+
+function calc() {
+    let a = document.getElementById('number1').value;
+    let b = document.getElementById('number2').value;
+    let operation = document.getElementById('operation').value;
+    let answer = 0;
+    if (a === '' || b === '') {
+        alert("Please enter numbers correctly!")
+    } else {
+        switch (operation) {
+            case "+":
+                answer = (+a + +b);
+                return result.innerHTML = Number(answer.toFixed(10));
+                
+            case "-":
+                answer = (+a - +b);
+                return result.innerHTML = Number(answer.toFixed(10));
+            case "*":
+                answer = (+a * +b);
+                return result.innerHTML = Number(answer.toFixed(10));
+            case "/":
+                if (b != 0) {
+                    answer = (+a / +b);
+                    return result.innerHTML = Number(answer.toFixed(10));
+                } else alert('Error. Can`t divide by zero.')
+        };
+    };
+}

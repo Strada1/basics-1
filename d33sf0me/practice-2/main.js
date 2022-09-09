6b34d68904d4554c15552b7e249c26f99dc28b8c
d33sf0me <katekrokh@gmail.com>
2022-09-09 18:43:00 +0300

    modification of doc

--- d33sf0me/practice-2/main.js
+++ d33sf0me/practice-2/main.js
@@ -1,6 +1,6 @@
 let button = document.getElementById('button');
 let result = document.querySelector(".result");
-button.addEventListener('click', calc)
+button.addEventListener('click', calc);
 
 function calc() {
     let a = document.getElementById('number1').value;
@@ -8,24 +8,37 @@ function calc() {
     let operation = document.getElementById('operation').value;
     let answer = 0;
     if (a === '' || b === '') {
-        alert("Please enter numbers correctly!")
+        alert('Please enter numbers correctly!');
     } else {
         switch (operation) {
             case "+":
                 answer = (+a + +b);
+                rememberAnswer(answer);
                 return result.innerHTML = Number(answer.toFixed(10));
-                
             case "-":
                 answer = (+a - +b);
+                rememberAnswer(answer);
                 return result.innerHTML = Number(answer.toFixed(10));
             case "*":
                 answer = (+a * +b);
+                rememberAnswer(answer);
                 return result.innerHTML = Number(answer.toFixed(10));
             case "/":
                 if (b != 0) {
                     answer = (+a / +b);
+                    rememberAnswer(answer);
                     return result.innerHTML = Number(answer.toFixed(10));
-                } else alert('Error. Can`t divide by zero.')
+                } else alert('Error. Can`t divide by zero.');
         };
     };
+};
+
+function rememberAnswer(answer) {
+    let div = document.createElement('div');
+    div.className = 'answers';
+    div.innerHTML = answer;
+    document.body.append(div);
+    div.onclick = function(event) {
+        event.currentTarget.remove();
+      };
 }

aad59009bdf12234bade0a7f2eeb475618e1ff58
PankJK <daidengy@yandex.ru>
2022-09-09 17:24:50 +0300

    new practice

--- 36_CalcUI/main.js
+++ 36_CalcUI/main.js
@@ -5,27 +5,42 @@ function calc() {
   let a = document.getElementById("number1").value;
   let b = document.getElementById("number2").value;
   let result = document.getElementById("result");
-  if((a == "") || (b == "")) {
-    result.innerHTML = 'error';
-    result.style.background ="red"
-  } else { 
-    result.style.background = null
-  switch (operation) {
-    case "+":
-        c = +a + +b
+  let history = document.querySelector(".history");
+  if (a == "" || b == "") {
+    result.innerHTML = "error";
+    result.style.background = "red";
+    setTimeout(() => (result.style.background = null), 1000);
+  } else {
+    let addDiv = function () {
+      let div = document.createElement("button");
+      div.className = "alert";
+      div.innerHTML = c;
+      history.append(div);
+      div.onclick = function(){
+        div.remove()
+      }
+    };
+    switch (operation) {
+      case "+":
+        c = +a + +b;
         result.innerHTML = c;
-        break;s
-    case "*":
-      c = a * b;
-      result.innerHTML = c;
-      break;
-    case "-":
-      c = a - b;
-      result.innerHTML = c;
-      break;
-    case "/":
-      c = a / b;
-      result.innerHTML = c;
-      break;
-  }}
+        addDiv();
+        break;
+      case "*":
+        c = a * b;
+        result.innerHTML = c;
+        addDiv();
+        break;
+      case "-":
+        c = a - b;
+        result.innerHTML = c;
+        addDiv();
+        break;
+      case "/":
+        c = a / b;
+        result.innerHTML = c;
+        addDiv();
+        break;
+    }
+  }
 }

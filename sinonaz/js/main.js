c2b4e76d86941913ed5326725e5f1d752479ad03
Isaac <aziksimpson@gmail.com>
2022-09-10 01:06:07 +0300

    add calculator

--- sinonaz/js/main.js
+++ sinonaz/js/main.js
@@ -1,28 +1,30 @@
-function calc(operator, firstOperand, secondOperand) {
-  if (operator == `add`) {
-    return firstOperand + secondOperand;
-  } else if (operator == `multi`) {
-    return firstOperand * secondOperand;
-  } else if (operator == `subtract`) {
-    return firstOperand - secondOperand;
-  }
+const calculator = document.querySelector(`.js-calculator`);
+
+const calcButton = calculator.querySelector(`.js-button`);
+const firstInput = calculator.querySelector(`.js-first-operand`);
+const secondInput = calculator.querySelector(`.js-second-operand`);
+const operator = calculator.querySelector(`.js-operator`);
+const value = calculator.querySelector(`.js-value`);
+
+function calc() {
+    let firstOperand = +firstInput.value;
+    let secondOperand = +secondInput.value;
+    switch(operator.value) {
+        case `sum`:
+            value.innerHTML = firstOperand + secondOperand
+            break;
+        case `sub`:
+            value.innerHTML = firstOperand - secondOperand;
+            break;
+        case `multi`:
+            value.innerHTML = firstOperand * secondOperand;
+            break;
+        case `div`:
+            value.innerHTML = firstOperand / secondOperand;
+            break;
+        default:
+            console.log(`Error`)
+    }
 }
 
-console.log(calc(`subtract`, 2, 2));
-
-function calc(operator, firstOperand, secondOperand) {
-  switch (operator) {
-    case `add`:
-      return firstOperand + secondOperand;
-      break;
-    case `multi`:
-      return firstOperand * secondOperand;
-      break;
-    case `subtract`:
-      return firstOperand - secondOperand;
-      break;
-    default:
-      console.log(`Не верное значение оператора`);
-  }
-}
-console.log(calc(`add`, 5, 5));
+calcButton.addEventListener(`click`, calc);
--- sinonaz/js/verticalMessage.js
+++
@@ -1,9 +0,0 @@
-function showVerticalMessage(message) {
-  message = message[0].toUpperCase() + message.slice(1, 6);
-
-  for (word of message) {
-    console.log(word);
-  }
-}
-
-showVerticalMessage("stradanie");

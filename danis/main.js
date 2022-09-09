06beee6396c1a64433977c41bc8423c38febc09f
hollywood787 <hollywood7878@yandex.ru>
2022-09-09 09:15:04 +0300

    Modified

--- danis/main.js
+++ danis/main.js
@@ -4,14 +4,33 @@ let operator = document.querySelector("#identifier");
 let firstNumber = document.querySelector(".a");
 let secondNumber = document.querySelector(".b");
 
-let test = document.querySelector(".container__saves-results");
+let containerForNewDiv = document.querySelector(".container__saves-results");
+
+//Создаем новый div, который будет содержать в себе сохраненный ответ
+function saveResult() {
+	
+	let myDiv = document.createElement("div");
+
+	//Вешаем на него сразу событие клика, чтобы по клику удалялся
+	myDiv.addEventListener("click", function () {
+		myDiv.remove();
+	});
+
+	//стили для создаваемого div
+	myDiv.style.cursor = "pointer";
+	myDiv.style.width = "fit-content";
+	myDiv.style.margin = "0 auto";
+
+	//Передаем в новый созданный div результат функции
+	myDiv.textContent = Number(calc(operator, firstNumber, secondNumber).toFixed(2));
+	containerForNewDiv.append(myDiv);
+}
 
 //вешаем событие на кнопку равно
 equals.addEventListener("click", function () {
 	operator = document.querySelector("#identifier").value;
 	firstNumber = document.querySelector(".a").value;
 	secondNumber = document.querySelector(".b").value;
-	let functionResult = Number(calc(operator, firstNumber, secondNumber).toFixed(2));
 
 	//проверяем все ли поля заполнены
 	if (firstNumber == "" || secondNumber == "") {
@@ -19,25 +38,10 @@ equals.addEventListener("click", function () {
 		result.style.color = "red";
 	} else {
 		//Передаем результат функции в span
-		result.textContent = functionResult;
+		result.textContent = Number(calc(operator, firstNumber, secondNumber).toFixed(2));
 		result.style.color = "green";
 
-		//Создаем новый div, который будет содержать в себе сохраненный ответ
-		let myDiv = document.createElement("div");
-
-		//Вешаем на него сразу событие клика, чтобы по клику удалялся
-		myDiv.addEventListener("click", function () {
-			myDiv.remove();
-		});
-
-    //стили для создаваемого div
-		myDiv.style.cursor = "pointer";
-		myDiv.style.width = "fit-content";
-		myDiv.style.margin = "0 auto";
-
-		//Передаем в новый созданный div результат функции
-		myDiv.textContent = functionResult;
-		test.append(myDiv);
+		saveResult();
 	}
 });
 

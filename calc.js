9dd8527db6bd0fb2c19a9590a43b445f2d4e3f96
YaRolly <Ea4GoodRu@gmail.com>
2022-09-10 14:42:52 +0300

    calculator with module v.1.1

--- calc.js
+++ calc.js
@@ -1,20 +1,25 @@
-export function сalc(operation, firstNumb, secondNumb) {
+const OPERATIONS = {
+  ADD: '+',
+  SUB: '-',
+  MULTI: '*',
+  DELETE: '/',
+};
+export default function сalc(operation, firstNumb, secondNumb) {
   if (firstNumb === '' || secondNumb === '') {
     return 'Введите число';
-  } else {
-    switch (operation) {
-      case '+':
-        return +(+firstNumb + +secondNumb).toFixed(10);
-      case '-':
-        return +(+firstNumb - +secondNumb).toFixed(10);
-      case '*':
-        return +(+firstNumb * +secondNumb).toFixed(10);
-      case '/':
-        if (+secondNumb === 0) {
-          return 'Ошибка';
-        } else {
-          return +(+firstNumb / +secondNumb).toFixed(10);
-        }
-    }
+  }
+  switch (operation) {
+    case OPERATIONS.ADD:
+      return +(+firstNumb + +secondNumb).toFixed(10);
+    case OPERATIONS.SUB:
+      return +(+firstNumb - +secondNumb).toFixed(10);
+    case OPERATIONS.MULTI:
+      return +(+firstNumb * +secondNumb).toFixed(10);
+    case OPERATIONS.DELETE:
+      if (+secondNumb === 0) {
+        return 'Ошибка';
+      } else {
+        return +(+firstNumb / +secondNumb).toFixed(10);
+      }
   }
 }
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-import { calc } from './calc.js';
+import calc from './calc.js';
+const OPERATION = document.querySelector('#calc');
+const FIRST_NUMBER = document.querySelector('#first-number');
+const SECONT_NUMBER = document.querySelector('#second-number');
+const DIV_RESULT = document.querySelector('.calc__result');
 function showResult() {
-  let result = calc(calc.value, firstnumber.value, secondnumber.value);
-  let divResult = document.querySelector('.calc__result');
+  let result = calc(OPERATION.value, FIRST_NUMBER.value, SECONT_NUMBER.value);
   let div = document.createElement('div');
   div.innerHTML = result;
-  divResult.prepend(div);
+  DIV_RESULT.prepend(div);
   div.addEventListener('click', () => div.remove());
 }
 btn.addEventListener('click', showResult);

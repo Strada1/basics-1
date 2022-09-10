cf7f729c832ece8805af844cb1ed1171044cb405
meVarensev <vitalik.unit@gmial.com>
2022-09-10 12:48:16 +0500

    fixded main.js createMyDiv and clear function

--- calc/src/main.js
+++ calc/src/main.js
@@ -1,53 +1,7 @@
 let equals = document.querySelector(".equals");
 let result = document.querySelector(".result");
-
-
-function clear() {
-    document.querySelector(".firstNumber").value = '';
-    document.querySelector(".secondNumber").value = '';
-    document.querySelector(".result").value = result.textContent = '';
-
-}
-document.querySelector(".clear").onclick = clear;
-
-function checkInput(firstNumber,secondNumber) {
-    let checkInput = (firstNumber === "" || secondNumber === "" )
-    if (checkInput) {
-        return true;
-    }
-}
-
-
-// function MyaddEventListener(){
-//     let operation = document.querySelector(".operation").value;
-//     let firstNumber = document.querySelector(".firstNumber").value;
-//     let secondNumber = document.querySelector(".secondNumber").value;
-//     if ( checkInput(firstNumber,secondNumber) ) {
-//         console.log("Is not number");
-//         result.textContent = "Is not number";
-//     } else {
-//         let calc = calcObject(operation, firstNumber, secondNumber);
-//         result.textContent =  Number(calc.toFixed(5));
-//     }
-// }
-// //document.querySelector(".equals").onclick = MyaddEventListener;
-// equals.addEventListener('click' ,MyaddEventListener);
-
-equals.addEventListener("click", function () {
-    let operation = document.querySelector(".operation").value;
-    let firstNumber = document.querySelector(".firstNumber").value;
-    let secondNumber = document.querySelector(".secondNumber").value;
-
-    if ( checkInput(firstNumber,secondNumber) ) {
-        console.log("Is not number");
-        result.textContent = "Is not number";
-    } else {
-        let calc = calcObject(operation, firstNumber, secondNumber).toFixed(5);
-        result.textContent =  Number(calc);
-    }
-});
-
-
+let history = document.querySelector(".history");
+let clear = document.querySelector(".clear");
 
 const operations = {
     add: 'add',
@@ -68,13 +22,59 @@ function calcObject( operation, firstNumber, secondNumber ) {
         case (operations.subtract):
             return  firstNumber - secondNumber;
         case (operations.divide):
+
             if (secondNum === "0"){
                 result.textContent = "can't divide by zero"
-            }else{
+            } else{
                 return  firstNumber / secondNumber;
             }
+
         default:
             return "unknown operation";
     }
 
 }
+
+function checkInput(firstNumber,secondNumber) {
+    let checkInput = (firstNumber === "" || secondNumber === "" )
+    if (checkInput) {
+        return true;
+    }
+}
+
+
+clear.addEventListener("click", function (){
+    document.querySelector(".firstNumber").value = '';
+    document.querySelector(".secondNumber").value = '';
+    result = result.textContent = '';
+});
+
+
+
+equals.addEventListener("click", function () {
+    let operation = document.querySelector(".operation").value;
+    let firstNumber = document.querySelector(".firstNumber").value;
+    let secondNumber = document.querySelector(".secondNumber").value;
+
+    if ( checkInput(firstNumber,secondNumber) ) {
+        console.log("Is not number");
+        result.textContent = "Is not number";
+    } else {
+        let calc = calcObject(operation, firstNumber, secondNumber).toFixed(5);
+        result.textContent =  Number(calc);
+    }
+});
+
+function createMyDiv(){
+    let div = document.createElement('div');
+
+    div.addEventListener("click", function () {
+        div.remove();
+    });
+
+    div.innerHTML = result.textContent;
+    history.append(div);
+    div.style.cursor = "pointer";
+}
+
+document.querySelector(".equals").onclick = createMyDiv;

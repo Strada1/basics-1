2fc77ca8bc97cab08dc2af5dc0a9853e2a61018d
kir563 <k563ke70@gmail.com>
2022-09-10 14:05:47 +0700

    результат дублируется в новом div и удаляется по нажатию на него

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let b = document.querySelector('.numberTwo');
 let operation = document.querySelector('.operator');
 let btn = document.querySelector('.result')
 let result = document.querySelector('.showResult')
+let div = document.querySelector('.row')
 
 btn.addEventListener('click', calc)
 
@@ -23,7 +24,18 @@ function calc() {
             case '/':
                 result.textContent = `${Number((a.value / b.value).toFixed(3))}`;
         }
+
+
+        div.insertAdjacentHTML('afterend', '<div class="show"></div>')
+        let newDiv = document.querySelector('.show')
+
+        newDiv.textContent = result.textContent
+
+        newDiv.addEventListener('click', (event) => {
+            event.currentTarget.remove()
+        })
     }
+
 
     if (a.value === '') {
         a.placeholder = 'enter number'
@@ -37,6 +49,8 @@ function calc() {
 
     }
 }
+
+
 
 
 

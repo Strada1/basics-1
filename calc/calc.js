bcf716eeee701321511ce904fb6a2aaf19b8a9f4
pipupopi <sonalavrushin@gmail.com>
2022-09-10 21:46:59 +0300

    эээээ

--- calc/calc.js
+++ calc/calc.js
@@ -1,24 +1,40 @@
-let answer = document.getElementById("answer");
+const answer = document.getElementById("answer");
 answer.addEventListener("click", calc);
 
 result1 = document.getElementById("result");
 
+const CHECKS_VALUE = {
+  DIVIDE_ZERO : 0,
+  EMPTY_STRING : '',
+
+}
+
+const CHECKS_TEXT =  {
+  EMPTY_STRING : 'введи число пжшка (─‿‿─)',
+  THIS_IS_LETTERS : 'введи число, это буковы (─‿‿─)',
+  DONT_DIVIDE_BY_ZERO : 'запрещаю делить \t на ноль (￣︿￣)'
+
+}
+
 function calc() {
-  let action = document.getElementById("action").value;
-  let a = document.getElementById("number1").value;
-  let b = document.getElementById("number2").value;
+  const action = document.getElementById("action").value;
+  const a = document.getElementById("number1");
+  const b = document.getElementById("number2");
   let result2;
 
+
    if (a == '' || b == '') {
-    return result1.textContent = 'введи число пжшка (─‿‿─)'
-   } if (isNaN(a)) {
-    return result1.textContent = 'введи число, это буковы (─‿‿─)'
-   } if (isNaN(b)) {
-    return result1.textContent = 'введи число, это буковы (─‿‿─)'
+    return result1.textContent = CHECKS_TEXT.EMPTY_STRING
+   }
+    if (isNaN(a)) {
+    return result1.textContent = CHECKS_TEXT.THIS_IS_LETTERS
+   }
+   if (isNaN(b)) {
+    return result1.textContent =  CHECKS_TEXT.THIS_IS_LETTERS
    } else {
     switch (action) {
         case "+":
-          result2 = (+a + +b);
+          result2 = Number(a) + Number(b);
           result1.innerHTML = result2;
          break;
 
@@ -47,4 +63,5 @@ answer.addEventListener("click", save);
  document.body.firstElementChild.nextElementSibling.append(saveResult)
  saveResult.textContent = result2
  }
+ //не смотри пж я думаю есчо
 }

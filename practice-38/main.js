edf8c5240461fdb7d9353e51190a62d9a00674d8
CallBackLater <a.a.vlasov88@gmail.com>
2022-09-10 17:25:58 +0500

    all files updated

--- practice-38/main.js
+++ practice-38/main.js
@@ -4,26 +4,30 @@ function calc() {
     let a = +document.getElementById('a').value;
     let b = +document.getElementById('b').value;
     let operator = document.getElementById('select').value;
-    let result = document.querySelector('.result')
-    switch(operator) {
-        case 'add':
-            result.innerHTML = a + b;
-            break;
-        case 'subtract':
-            result.innerHTML = a - b;
-            break;
-        case 'multi':
-            result.innerHTML = a * b;
-            break;
-        case 'divide':
-            result.innerHTML = a / b;
-            break;
-        default:
-            alert('Houston, we have a problem');   
+    let result = document.querySelector('.result');
+
+    if (isNaN(a) || isNaN(b) || a == ''|| b == '') {
+        container.classList.add('error');
+        setTimeout(() => container.classList.remove('error'), 300)
+    } else {
+        switch(operator) {
+            case 'add':
+                result.innerHTML = Number((a + b).toFixed(4));
+                break;
+            case 'subtract':
+                result.innerHTML = Number((a - b).toFixed(4));
+                break;
+            case 'multi':
+                result.innerHTML = Number((a * b).toFixed(4));
+                break;
+            case 'divide':
+                result.innerHTML = Number((a / b).toFixed(4));
+                break;
+                default:
+                    alert('Houston, we have a problem');
+                }
+        let div2 = result.cloneNode(true);
+        saveresult.append(div2);
+        div2.setAttribute('onclick', 'this.remove()');
     }
 }
-
-
-
-
-

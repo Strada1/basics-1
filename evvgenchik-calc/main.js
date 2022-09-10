4cef289ac3e5fd954267e5c22e4362bd2b40b25a
evvgenchik <yevgenchikk@mail.ru>
2022-09-10 15:07:59 +0300

    add module

---
+++ evvgenchik-calc/main.js
@@ -0,0 +1,55 @@
+
+
+'use strict'
+
+import { deletion } from "./deleteModule.js"
+const equal = document.querySelector('.equals')
+const calcResult = document.querySelector('.final-results')
+equal.addEventListener('click', calc)
+equal.addEventListener('click', saveResults)
+
+
+let storageForResults = 0
+
+function calc() {
+	const firstNum = +document.querySelector('.input-one > input').value;
+	const secondNum = +document.querySelector('.input-two > input').value;
+	const operator = document.querySelector('.list').value;
+	const result = document.querySelector('.result')
+	if (firstNum == ' ' || secondNum == ' ') {
+		return result.innerHTML = 'Заполните все поля'
+	}
+	let finalResult = 0
+	switch (operator) {
+		case '+': finalResult = firstNum + secondNum
+			break
+		case '*': finalResult = firstNum * secondNum
+			break
+		case '-': finalResult = firstNum - secondNum
+			break
+		case '/': finalResult = firstNum / secondNum
+			break
+	}
+
+	storageForResults = +((finalResult).toFixed(10))
+	result.innerHTML = storageForResults
+}
+
+/*.............................................*/
+
+
+
+function saveResults() {
+	calcResult.insertAdjacentHTML('afterbegin', '<div class = "final-result"></div>')
+	let save = document.querySelector('.final-result')
+	save.innerHTML = storageForResults
+}
+
+
+
+/*......................................*/
+
+
+
+calcResult.addEventListener('click', deletion)
+

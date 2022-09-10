cfe67667dc9ca51048b1dbe293e1cee90aea4f6b
Burito-san <94059151+Burito-san@users.noreply.github.com>
2022-09-10 16:24:34 +0200

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,31 @@
+button.onclick = showResult();
+
+function calc() {
+    let a = +document.getElementById("firstNumber").value;
+    let b = +document.getElementById("secondNumber").value;
+    let operate = String(document.getElementById('choiceOperate').selectedIndex);
+    switch (operate) {
+        case '0':
+            return a + b;
+        case '1':
+            return a * b;
+        case '2':
+            return a - b;
+        case '3':
+            return a / b;
+    }  
+}
+
+function conclusion() {
+    if(outputWindow.offsetHeight >= 462) {
+        deleteResult();
+    }
+    outputWindow.innerHTML += `<div id="result"> ${calc()}</div>`;
+};
+
+function showResult() {
+    button.addEventListener("click", conclusion);
+}
+function deleteResult() {
+    outputWindow.innerHTML = " ";
+}

14bda9a8f85dd997f6e55f5ba8b955e64f664a6b
Leonid Niselovsky <leonisel@outlook.com>
2022-09-11 06:06:34 +0300

    feat: make layout and add logic calculate

--- leonisel/calc.js
+++ leonisel/calc.js
@@ -1,7 +1,24 @@
+//DOM
+let getOperand1 = document.getElementById('operand-1');
+let getOperand2 = document.getElementById('operand-2');
+let selectOperator = document.getElementById('operator');
+let equal = document.querySelector('.equal-wrapper');
+let res = document.querySelector('.result');
+
+equal.addEventListener('click', function() {
+  const op1 = Number(getOperand1.value);
+  const op2 = Number(getOperand2.value);
+  let oper = selectOperator.value;
+  res.textContent = calc(oper, op1, op2);
+})
+
+//Logic
+
 let operations = {
   add: 'add',
   multi: 'multi',
   sub: 'subtract',
+  div: 'division'
 }
 
 function calc(operator, operand1, operand2) {
@@ -18,6 +35,9 @@ function calc(operator, operand1, operand2) {
         case(operations.sub):
           return operand1 - operand2;
           break;
+        case(operations.div):
+          return operand1 / operand2;
+          break;  
         default:
           return 'Error';  
       }
@@ -34,8 +54,8 @@ function testOfType(value) {
   return typeof value === 'number';
 }
 
-console.log(calc('add', 1, 2));  // возвращает 3
-console.log(calc('multi', 1, 2));  // возвращает 2
-console.log(calc('subtract', 3, 2));  // возвращает 1
-
+// console.log(calc('add', 1, 2));  // возвращает 3
+// console.log(calc('multi', 1, 2));  // возвращает 2
+// console.log(calc('subtract', 3, 2));  // возвращает 1
+// console.log(calc('division', 3, 2);  // возвращает 1.5
 

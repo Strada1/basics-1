884e42176914918fb1cb17d896362b9f78d880b5
Dariannyko <dariannyko@gmail.com>
2022-09-11 17:15:06 +0300

    changes in main and result

--- main.js
+++ main.js
@@ -1,30 +1,36 @@
 export function calculate (a, b, operation) {
 
-let operationResult;
-
- switch (operation) {
-   case 'add' :
-    operationResult = +a + +b; 
-    break;
-
-   case 'multiply' :
-     operationResult = a * b;
-     break;
-
-   case 'subtract' :
-     operationResult = a - b;
-     break;
-
-   case 'divide' :
-     operationResult = a / b;
-     break;
- }
-
- if(!isFinite(operationResult)) {
-   return operationResult = 'error'
- } else {
-    return operationResult = Number(operationResult.toFixed(10));
- }
-
-};
-
+  const OPERATION = {
+    add : 'add',
+    multiply : 'multiply',
+    subtract : 'subtract',
+    divide : 'divide',
+  };
+  
+  let operationResult;
+  
+   switch (operation) {
+     case OPERATION.add :
+      operationResult = +a + +b; 
+      break;
+  
+     case OPERATION.multiply :
+       operationResult = a * b;
+       break;
+  
+     case OPERATION.subtract :
+       operationResult = a - b;
+       break;
+  
+     case OPERATION.divide :
+       operationResult = a / b;
+       break;
+   }
+  
+   if(!isFinite(operationResult)) {
+     return operationResult = 'error'
+   } else {
+      return operationResult = Number(operationResult.toFixed(10));
+   }
+  
+  };
--- result.js
+++ result.js
@@ -1,27 +1,34 @@
 import {calculate} from './main.js';
 
-let buttonResult = document.querySelector('.button');
-
+const ELEMENTS = {
+    firstNumber : document.querySelector('.number1'),
+    secondNumber : document.querySelector('.number2'),
+    operation : document.querySelector('.operation'),
+    totalResult : document.querySelector('.result'),
+    buttonResult : document.querySelector('.button'),
+    zero : document.body.querySelector('.zero'),
+  };
 
 function addResult () {
-    let a = document.querySelector('.number1').value;
-    let b = document.querySelector('.number2').value;
-    let operation = document.querySelector('.operation').value;
+    let a = ELEMENTS.firstNumber.value;
+    let b = ELEMENTS.secondNumber.value;
+    let operation = ELEMENTS.operation.value;
+
     let result = calculate(a, b, operation);
 
     let div = document.createElement('div');
     div.className = 'operation-result';
     div.innerHTML = result;
-    document.body.querySelector('.result').append(div);
+    ELEMENTS.totalResult.append(div);
     div.addEventListener('click', () => div.remove());
 };
 
 function deleteZero () {
-    document.body.querySelector('.zero').remove();
-    buttonResult.removeEventListener('click', deleteZero);
+    ELEMENTS.zero.remove();
+    ELEMENTS.buttonResult.removeEventListener('click', deleteZero);
    };
    
-   buttonResult.addEventListener('click', deleteZero);
+ELEMENTS.buttonResult.addEventListener('click', deleteZero);
 
 
-buttonResult.addEventListener('click', addResult);
+ELEMENTS.buttonResult.addEventListener('click', addResult);

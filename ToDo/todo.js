c1da449b4d60871a07dce41d720417279fd76924
Shirobokov52 <shirobokov52@bk.ru>
2022-09-12 09:36:50 +0300

    todo последний вариант

--- ToDo/todo.js
+++ ToDo/todo.js
@@ -1,72 +1,78 @@
 const STATUSES = {
-    IN_PROGRESS : "In Progress",
-    DONE : "Done",
-    TO_DO : "To Do",
+  IN_PROGRESS: "In Progress",
+  DONE: "Done",
+  TO_DO: "To Do",
+};
+
+const list = [
+  { name: "write a post", status: STATUSES.TO_DO, priority: "high" },
+  { name: "make a bed", status: STATUSES.IN_PROGRESS, priority: "low" },
+  { name: "new task", status: STATUSES.DONE, priority: "high" },
+];
+
+// добавление задачи===============================================================================
+
+function addTask(newTask, newPriority) {
+  list.push({
+    name: newTask,
+    status: STATUSES.TO_DO,
+    priority: newPriority,
+  });
+}
+
+// удаление задачи===================================================================================
+
+function deleteTask(task) {
+  let a = list.findIndex((item) => {
+    if (item.name == task) return true;
+  });
+  list.splice(a, 1);
 }
 
 
-const LIST = { 
-    "create a new practice task": STATUSES.IN_PROGRESS,
-     "make a bed": STATUSES.DONE, 
-     "write a post": STATUSES.TO_DO, 
+// изменение статуса задачи===========================================================================
+
+function changeTask(task, newStatus) {
+  let b = list.findIndex((item) => {
+    if (item.name == task) {
+      return true;
+    } else {
+      console.log("такой задачи не существует");
     }
+  });
 
-
-function changeStatus (task, status) {
-    LIST[task] = status;
+  list[b].status = newStatus;
 }
 
 
-function addTask(task) {
-    LIST[task] = STATUSES.TO_DO;
+// вывод задач по статусу===============================================================================
+function showList () {
+
+console.log ('Done:    ')
+for (i=0; i < list.length; i++) {
+  if(list[i].status == STATUSES.DONE){
+    console.log (list[i].name)
+  } 
 }
 
-
-function deleteTask (task) {
-    delete LIST[task];
+console.log ('In Progress:    ')
+for (i=0; i < list.length; i++) {
+  if(list[i].status == STATUSES.IN_PROGRESS){
+    console.log (list[i].name)
+  } 
 }
 
-function ShowList() {
-
-    console.log("In Progress:  ");
-
-        for (let key in LIST) {
-
-            if(LIST[key] == STATUSES.IN_PROGRESS) {
-                console.log(key);  
-            } 
-        }
-            
-     
-
-
-    console.log("Done:  ");
-
-        for (let key in LIST) {
-
-            if (LIST[key] == STATUSES.DONE) {
-                console.log(key);
-            }  
-        }
-        // console.log('-');
-
-
-    console.log("ToDo:  ");
-
-        for (let key in LIST) {
-
-            if(LIST[key] == STATUSES.TO_DO) {
-                console.log(key);
-            }   
-        }
-        // console.log('-');
+console.log ('Io Do:    ')
+for (i=0; i < list.length; i++) {
+  if(list[i].status == STATUSES.TO_DO){
+    console.log (list[i].name)
+  } 
+}
 }
 
-
-
-deleteTask('have a walk');
-changeStatus('write a post', STATUSES.DONE);
-addTask('have a walk')
-
-
-ShowList();
+changeTask("write a post", "Done");
+addTask("fishing", "low");
+addTask("watch film", "low");
+deleteTask("make a bed")
+addTask("run", "low");
+showList();

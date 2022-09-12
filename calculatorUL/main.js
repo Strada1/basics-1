705b2cec713ff18ae7871936a6d0d6f431e6f97a
Shirobokov52 <shirobokov52@bk.ru>
2022-09-12 15:49:25 +0300

    калькулятор с модулем

--- calculatorUL/main.js
+++ calculatorUL/main.js
@@ -1,44 +1,23 @@
+import { calc } from "./calc.js";
 
-    elem.addEventListener("click", function () {
-       
-        let a = document.querySelector(".number1").value;
-        let b = document.querySelector(".number2").value;
-        let operator = document.querySelector("select").value;
-        let result = document.querySelector(".result");
-        calc (a,b,operator);
+elem.addEventListener("click", function () {
+  let a = document.querySelector(".number1").value;
+  let b = document.querySelector(".number2").value;
+  let operator = document.querySelector("select").value;
+  let result = document.querySelector(".result");
+  result.textContent = calc(a, b, operator);
 
-       result.textContent = calc (a,b,operator);
+  let div = document.createElement("div");
+  div.className = "del";
+  div.textContent = calc(a, b, operator);
+  let saveDiv = document.querySelector(".save_container");
+  saveDiv.append(div);
 
-       let div = document.createElement('div');
-       div.className = "del";
-       div.textContent = calc (a,b,operator);
-       let saveDiv = document.querySelector('.save_container')
-       saveDiv.append(div);
-
-       div.addEventListener("click", function() {
-        div.remove();
-    });
-
-    });
+  div.addEventListener("click", function () {
+    div.remove();
+  });
+});
 
 
-    function calc (a,b,operator) {
 
-        switch(operator) {
-    
-               case "+":
-               return +(+a + +b);
-    
-               case "-":
-               return +(+a - +b);
-    
-               case "*":
-               return  +(+a * +b);
 
-               case "/":
-               return  +(+a / +b);
-        }
-    }
-   
-
-   

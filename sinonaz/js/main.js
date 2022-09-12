3b1975c152e2e3880c36d0df077307f3c915f2f3
Isaac Shir <aziksimpson@gmail.com>
2022-09-12 15:46:13 +0300

    initialize a module consts.js

--- sinonaz/js/main.js
+++ sinonaz/js/main.js
@@ -1,46 +1,47 @@
-const calculator = document.querySelector(`.js-calculator`);
-
-const calcButton = calculator.querySelector(`.js-button`);
-const firstInput = calculator.querySelector(`.js-first-operand`);
-const secondInput = calculator.querySelector(`.js-second-operand`);
-const operator = calculator.querySelector(`.js-operator`);
-const value = calculator.querySelector(`.js-value`);
-const list = calculator.querySelector(`.js-list`);
+import {
+  calculator,
+  calcButton,
+  firstInput,
+  secondInput,
+  operator,
+  value,
+  list,
+} from "./consts.js";
 
 function calc() {
-    let firstOperand = +firstInput.value;
-    let secondOperand = +secondInput.value;
-    switch(operator.value) {
-        case `sum`:
-            value.innerHTML = firstOperand + secondOperand
-            break;
-        case `sub`:
-            value.innerHTML = firstOperand - secondOperand;
-            break;
-        case `multi`:
-            value.innerHTML = firstOperand * secondOperand;
-            break;
-        case `div`:
-            value.innerHTML = firstOperand / secondOperand;
-            break;
-        default:
-            console.log(`Error`)
+  let firstOperand = +firstInput.value;
+  let secondOperand = +secondInput.value;
+  switch (operator.value) {
+    case `sum`:
+      value.innerHTML = firstOperand + secondOperand;
+      break;
+    case `sub`:
+      value.innerHTML = firstOperand - secondOperand;
+      break;
+    case `multi`:
+      value.innerHTML = firstOperand * secondOperand;
+      break;
+    case `div`:
+      value.innerHTML = firstOperand / secondOperand;
+      break;
+    default:
+      console.log(`Error`);
+  }
+
+  list.insertAdjacentHTML(
+    `beforeend`,
+    `<li class="js-item">${value.innerHTML}</li>`
+  );
+
+  const items = calculator.querySelectorAll(`.js-item`);
+
+  items.forEach((item) => {
+    function deleteItem() {
+      item.remove();
     }
 
-    list.insertAdjacentHTML(`beforeend`, `<li class="js-item">${value.innerHTML}</li>`);
-
-    const items = calculator.querySelectorAll(`.js-item`);
-
-    items.forEach((item) => {
-
-        function deleteItem() {
-            item.remove();
-        }
-
-        item.addEventListener(`click`, deleteItem);
-    });
+    item.addEventListener(`click`, deleteItem);
+  });
 }
-
-
 
 calcButton.addEventListener(`click`, calc);

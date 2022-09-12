dd3f4fff5e7a989a5ed2c21bbd1543285411ee5e
hollywood787 <hollywood7878@yandex.ru>
2022-09-12 19:58:56 +0300

    To do Add Task

--- danis/main.js
+++ danis/main.js
@@ -1,40 +1,35 @@
 const addTaskButton = document.querySelectorAll(".addTask");
 const list = [];
+const highElement = document.querySelector('.todo__list__high-task');
 
-const STATUS = {
-	inProgress: "In Progress",
-	toDo: "To do",
-	Done: "Done",
-};
-
-const PRIORITY = {
-	low: "Low",
-	high: "High",
-};
 
 for (let i = 0; i < addTaskButton.length; i++) {
 	addTaskButton[i].addEventListener("click", function () {
-    let presentValue = addTaskButton[i].parentElement.querySelector('input').value;
-    addTask(presentValue, PRIORITY.high);
-
-    let myDiv = document.createElement('div'); 
-    myDiv.textContent = presentValue;
-    
-    });
+		let presentValue = addTaskButton[i].parentElement.querySelector('input').value;
+		addTask(presentValue, addTaskButton[i]);
+	});
 }
 
+function createTaskElement(button) {
+	let presentValue = button.parentElement.querySelector('input').value;
+	let myDiv = highElement.cloneNode(true);
+	let nameTask = myDiv.querySelector('.todo__list__high-task-item-text');
+	nameTask.textContent = presentValue;
+	let highParent = button.parentElement.parentElement;
+	highParent.append(myDiv);
+}
 
-
-	function addTask(nameTask, priority) {
-		let result = list.findIndex(function (item) {
-			return item.name === nameTask;
-		});
-		if (result === -1) {
-			list.push({name: nameTask, status: STATUS.toDo, priority: priority,});
-		} else {
-			console.log("Такая задача уже существует");
-		}
+function addTask(nameTask, button) {
+	let result = list.findIndex(function (item) {
+		return item.name === nameTask;
+	});
+	if (result === -1) {
+		list.push({ name: nameTask});
+		createTaskElement(button);
+	} else {
+		console.log("Такая задача уже существует");
 	}
+}
 
 // function deleteTask(nameTask) {
 // 	let result = list.findIndex(function (item) {

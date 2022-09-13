ab08d8c86f9091e9bd19c8d815f0a16a993b2cb8
nya2000 <tighineanu00@mail.ru>
2022-09-14 00:34:12 +0300

    Доделал вёрстку, также js , добавление и удаление

--- ToDo_UI/script.js
+++ ToDo_UI/script.js
@@ -1,12 +1,26 @@
-const addBtn = document.querySelector('.btn');
-const addList = document.querySelector('.addlist');
-const inputField = document.getElementById('todo-input');
+const addBtnHight = document.querySelector('.btnHight');
+const addBtnLow = document.querySelector('.btnLow')
+const addListHight = document.querySelector('.addlistHight');
+const addListLow = document.querySelector('.addlistLow');
+const inputFieldHight = document.getElementById('todo-input-high');
+const inputFieldLow = document.getElementById('todo-input-low');
 
-addBtn.addEventListener ('click', addTask)
+addBtnHight.addEventListener ('click', checkHigh)
+addBtnLow.addEventListener ('click', checkLow)
+function checkHigh(event) {
+    event.preventDefault()
 
-function addTask () {
-    
-    addlist.insertAdjacentHTML("afterbegin", `<div class="add-list">
+    if (inputFieldHight.value.trim() == '') {
+        alert('Введите текст!')
+    } else {
+        addTaskHigh(event)
+    }
+}
+
+function addTaskHigh (event) {
+    event.preventDefault()
+
+    addListHight.insertAdjacentHTML("afterbegin", `<div id="removelistHigh" class="addlist">
     <li class="item" id="item">
         <a href="#">
             <label class="container">
@@ -14,11 +28,46 @@ function addTask () {
                 <span class="checkmark"></span>
             </label>
         </a>
-        <span class="todo-text">${inputField.value}</span>
-        <a href="#" class="todo-remove"><img id="todo-remove"src="img/delete-task.svg" alt=""></i></a>
+        <span class="todo-text">${inputFieldHight.value}</span>
+        <a href="#" id="itemToDo"class="todo-remove-high"><img id="todo-remove" src="img/delete-task.svg" alt=""></i></a>
     </li>
-</div>`);
+</div>`) ;
+let removeHighTasks = document.querySelector('.todo-remove-high')
+let deleteHighTarget = document.querySelector('#removelistHigh')
+removeHighTasks.addEventListener( 'click', ()=> {
+    deleteHighTarget.remove(this)
+})
+
 }
+function checkLow(event) {
+    event.preventDefault()
+
+    if (inputFieldLow.value.trim() == '') {
+        alert('Введите текст!')
+    } else {
+        addTaskLow(event)
+    }
+}
+function addTaskLow (event) {
+    event.preventDefault()
+
+    addListLow.insertAdjacentHTML("afterbegin", `<div id="removelistLow" class="addlistLow">
+    <li class="item" id="item">
+        <a href="#">
+            <label class="container">
+                <input type="checkbox" name="radio">
+                <span class="checkmark"></span>
+            </label>
+        </a>
+        <span class="todo-text">${inputFieldLow.value}</span>
+        <a href="#" id="itemToDo"class="todo-remove-low"><img id="todo-remove" src="img/delete-task.svg" alt=""></i></a>
+    </li>
+</div>`) ;
+let removeLowTasks = document.querySelector('.todo-remove-low')
+let deleteLowTarget = document.querySelector('#removelistLow')
+removeLowTasks.addEventListener( 'click', ()=> {
+    deleteLowTarget.remove(this)
+})
 
 
-
+}

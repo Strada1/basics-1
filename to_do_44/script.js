fd19aeb64eb639dd60103a4d14c72a1553406557
Inal Lukyaev <lukyaevinal@gmail.com>
2022-09-14 00:14:02 +0300

    add new update task # 44 TO_DO

--- to_do_44/script.js
+++ to_do_44/script.js
@@ -28,26 +28,9 @@ function createTaskElem(task, priority) {
   return result;
 }
 
-inputs.forEach(item => {
-  item.addEventListener('keydown', function (e) {
-    if (
-      e.key === 'Enter' &&
-      e.target.classList.contains('high-input') &&
-      inputHighTask.value !== ''
-    ) {
-      highBlock.insertAdjacentHTML('afterBegin', createTaskElem(inputHighTask));
-    } else if (
-      e.key === 'Enter' &&
-      e.target.classList.contains('low-input') &&
-      inputHighTask.value !== ''
-    ) {
-      lowBlock.insertAdjacentHTML('afterBegin', createTaskElem(inputHighTask));
-    }
-  });
-});
-
 addBtns.forEach(item => {
   item.addEventListener('click', e => {
+    console.log(e.target);
     if (e.target.classList.contains('high') && inputHighTask.value !== '') {
       highBlock.insertAdjacentHTML('afterBegin', createTaskElem(inputHighTask));
     } else if (

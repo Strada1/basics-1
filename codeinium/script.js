a990105d45ba1bce26231694782afb796c148c5e
codeinium <timofiei.tarasov@gmail.com>
2022-09-14 00:44:02 +0500

    some changes

--- codeinium/script.js
+++ codeinium/script.js
@@ -3,7 +3,9 @@ const LOWPRIORITYINPUT = document.getElementById('low-priority-input');
 const HIGHINPUTCONTAINER = document.getElementsByClassName('input-container')[0];
 const LOWINPUTCONTAINER = document.getElementsByClassName('input-container')[1];
 
+
 function createHighTask() {
+    event.preventDefault();
     HIGHINPUTCONTAINER.insertAdjacentHTML('afterend', `
         <div class="task-outer">
                     <div class="task-inner">
@@ -19,7 +21,27 @@ function createHighTask() {
                     </div>
                 </div>
         `);
+    HIGHPRIORITYINPUT.value = '';
 }
 
-// HIGHPRIORITYINPUT.addEventListener('submit', createHighTask)
+function createLowTask() {
+    event.preventDefault();
+    LOWINPUTCONTAINER.insertAdjacentHTML('afterend', `
+        <div class="task-outer">
+                    <div class="task-inner">
+                        <div class="text-container">
+                            <label class="text"> 
+                                <input type="checkbox" id="task" name="task"> 
+                                <span>${LOWPRIORITYINPUT.value}</span>       
+                            </label>
+                        </div>
+                        <div class="close">
+                            <img src="./close-icon-2.svg">
+                        </div>
+                    </div>
+                </div>
+        `);
+    LOWPRIORITYINPUT.value = '';
+}
+
 

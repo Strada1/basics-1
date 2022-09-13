878819a68d5ebd726ddf7dd44126e3d46955ede9
Kirill108 <kirill108bol@gmail.com>
2022-09-13 18:41:13 +0200

    не выходит, помогите

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
+
+
 const input_addtask = document.getElementById("input_addtask")
 const input_checkbox = document.getElementById("input_checkbox")
 
+input_addtask.addEventListener("click", event)
+
+ function event (event) {
+	event.preventDefault()
+}
+
 input_addtask.addEventListener("click", addtask)
 
-function addtask () {
 
+function addtask () {
 	const value_input = input_addtask.value;
-	input_checkbox.insertAdjacentElement("beforeend", 
+	input_checkbox.insertAdjacentHTML("afterend", 
 	`
+	<div> 
 		<label class="input_checkbox" id="input_checkbox">
 			<input type="checkbox" name="value" value="option">
 			${value_input}
 		</label>
+	</div>
 	`
 	)
 }

0f958d5369f3b521360f931f21fc0f86e94b61c7
kamilsalikhov75 <kamilsalikhov75@gmail.com>
2022-09-13 22:10:05 +0300

    Added task check

--- main.js
+++ main.js
@@ -9,18 +9,36 @@ function createTask (event) {
   event.preventDefault();
   const todoInput = this.todoForm.querySelector('input');
   const todoUl = this.todoForm.nextElementSibling;
-
+  const tasks = document.querySelectorAll('.todo__task-text');
+  const newTask = todoInput.value;
+  if (tasks[0]!= undefined) {
+    if (checkTask(newTask)) {
+      alert('Такая задача уже есть!');
+      return;
+    }
+  }
+  
   todoUl.insertAdjacentHTML('beforeend', `
             <li class="todo__list-item">
               <input class="todo__checkbox" type="checkbox" id="todo__checkbox-${checkboxCounter}">
               <label for="todo__checkbox-${checkboxCounter}" class="todo__task">
                 <div class="todo__text">
                   <p class="todo__task-text">
-                    ${todoInput.value}
+                    ${newTask}
                   </p>
                 </div>
                 <button class="todo__close-btn"></button>
               </label>
             </li>`);
   checkboxCounter++;
+}
+
+function checkTask (newTask) {
+  const tasks = document.querySelectorAll('.todo__task-text');
+  
+  for (let i = 0; i<tasks.length; i++) {
+    if (newTask === tasks[i].innerHTML.trim()) {
+      return true;
+    }
+  }
 }

9de787e1c8f3cf6da0128249a8bbd916822c80d3
Rob <everton199643@gmail.com>
2022-09-13 13:13:29 +0300

    func delete

--- practice/calc.js
+++ practice/calc.js
@@ -20,9 +20,21 @@ button.addEventListener('click', () => {
   let select = document.getElementById('select')
   let value1 = document.getElementById('value1').value
   let value2 = document.getElementById('value2').value
-  let res = document.getElementById('result')
+
+  let button = document.createElement('button')
+  button.innerHTML = 'Удалить'
+
+  let foo = document.body.appendChild(button)
+  console.log('button', button)
+
+
+  let res = document.getElementById('ul')
+  res.className = 'element'
 
   let result = calcSwitch(select.value, Number(value1), Number(value2))
 
-  return res.innerHTML = `Result: ${result}`
+  let li = document.createElement('li')
+  li.innerHTML = `Result: ${result} ${foo}`
+
+  res.append(li);
 })

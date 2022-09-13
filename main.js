dabdf78256024d175654c1ebee0d8a82bdb34ff7
Kirill108 <kirill108bol@gmail.com>
2022-09-13 17:40:19 +0200

    ошибка в консоли

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 const input_addtask = document.getElementById("input_addtask")
+const input_checkbox = document.getElementById("input_checkbox")
 
 input_addtask.addEventListener("click", addtask)
 
 function addtask () {
+
 	const value_input = input_addtask.value;
+	input_checkbox.insertAdjacentElement("beforeend", 
+	`
+		<label class="input_checkbox" id="input_checkbox">
+			<input type="checkbox" name="value" value="option">
+			${value_input}
+		</label>
+	`
+	)
+
 	alert(value_input)
 }

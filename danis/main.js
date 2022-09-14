3d8eae778e1dede416b1518e41c2a7a5d481a9e5
hollywood787 <hollywood7878@yandex.ru>
2022-09-14 14:43:15 +0300

    it work

--- danis/main.js
+++ danis/main.js
@@ -9,53 +9,52 @@ const PRIORITET = {
 };
 
 const list = [
-	{
-		name: "пойти бегать",
-		status: 'to do'
-	},
 ];
 
-const form = document.querySelector(".container");
-const addFroms = document.querySelectorAll(".todo__add_new-task-container");
+const hightParent = document.querySelector(".todo_container-high .container");
+const lowParent = document.querySelector(".todo_container-low .container");
+const taskFormHigh = document.querySelector(".todo_container-high");
+const taskFormLow = document.querySelector(".todo_container-low");
 
-for (let i = 0; i < addFroms.length; i++) {	
-	addFroms[i].addEventListener("submit", function (event) {
-		event.preventDefault();
-		const form = event.currentTarget;
-		const input = form.querySelector('input[type=text]')
-		addTask(input.value)
-	});
-}
+taskFormHigh.addEventListener("submit", function (event) {
+	event.preventDefault();
+	const form = event.currentTarget;
+	const input = form.querySelector("input[type=text]");
+	addTask(input.value, PRIORITET.high);
+});
 
-function addTask(nameTask) {
+taskFormLow.addEventListener("submit", function (event) {
+	event.preventDefault();
+	const form = event.currentTarget;
+	const input = form.querySelector("input[type=text]");
+	addTask(input.value, PRIORITET.low);
+});
+
+function addTask(nameTask, prioritet) {
 	let result = list.findIndex(function (item) {
 		return item.name === nameTask;
 	});
 	if ((result === -1) && (nameTask !== "")) {
-		list.push({ name: nameTask, status: STATUS.toDO });
+		list.push({
+			name: nameTask,
+			status: STATUS.toDO,
+			prioritet: prioritet,
+		});
 		render();
 	} else {
 		errorMessage();
 	}
+	render();
 }
 
 function deleteTask(nameTask) {
-		list.splice(nameTask, 1);
-		render();
+	list.splice(list.indexOf(nameTask), 1);
+	render();
 }
 
 function changeStatus(nameTask, status) {
-	let result = list.findIndex(function (item) {
-		return item.name === nameTask;
-	});
-
-	if (result === -1) {
-		console.log("Такой задачи нет ");
-	} else {
-		list[result].status = status;
-	}
+	nameTask.status = status;
 }
-
 
 function errorMessage() {
 	let myDiv = document.createElement("div");
@@ -67,55 +66,44 @@ function errorMessage() {
 	}, 1500);
 }
 
-function render() {
-	form.innerHTML = "";
-	list.forEach(function (item) {
-		form.insertAdjacentHTML(
-			"beforeend",
-			`<div class="todo__list__high-task">
-				<div class="todo__list__high-task-item">
-						<input type="checkbox">
-						<div class="todo__list__high-task-item-text"> ${item.name} </div>
-				</div>
-				<button>x</button>
-			</div>`
-		);
-
-		let buttonDeleteTask = document.querySelectorAll("button");
-		for (let i = 0; i < buttonDeleteTask.length; i++) {
-			buttonDeleteTask[i].addEventListener("click", function () {
-				deleteTask(item.name);
-			});
+function createElement(parent, item) {
+	let todoListHighTask = document.createElement("div");
+	todoListHighTask.className = "todo__list__high-task";
+	parent.append(todoListHighTask);
+	let checkbox = document.createElement("input");
+	checkbox.setAttribute("type", "checkbox");
+	if (item.status === STATUS.done) {
+		todoListHighTask.classList.add('done');
+		checkbox.checked = true;
+	};
+	checkbox.addEventListener("change", function () {
+		todoListHighTask.classList.toggle("done");
+		if (checkbox.checked) {
+			changeStatus(item, STATUS.done);
+		} else {
+			changeStatus(item, STATUS.toDO);
 		}
-
-		let buttonCheckBox = document.querySelectorAll("input[type=checkbox]");
-		let inputTaskElement = document.querySelectorAll(".todo__list__high-task");
-
-		for (let i = 0; i < buttonCheckBox.length; i++) {
-			buttonCheckBox[i].addEventListener("change", function () {
-				
-				inputTaskElement[i].classList.toggle("done");
-
-				if (buttonCheckBox[i].checked) {
-					changeStatus(item.name, STATUS.done);
-				} else {
-					changeStatus(item.name, STATUS.toDO);
-				}
-			});
-		}
-		console.log(list);
 	});
-
-	// const addTaskButton = document.querySelectorAll(".addTask");
-	// 	for (let i = 0; i < addTaskButton.length; i++) {
-	// 		addTaskButton[i].addEventListener("click", function () {
-	// 			let presentValue = addTaskButton[i].parentElement.querySelector("input").value;
-	// 			addTask(presentValue, addTaskButton[i]);
-	// 		});
-	// 	}
-	
+	todoListHighTask.append(checkbox);
+	let todoListHighTaskItemText = document.createElement("div");
+	todoListHighTaskItemText.textContent = item.name;
+	todoListHighTask.append(todoListHighTaskItemText);
+	let button = document.createElement("button");
+	button.textContent = "x";
+	button.addEventListener("click", function () {
+		deleteTask(item);
+	});
+	todoListHighTask.append(button);
 }
 
-
-
-render();
+function render() {
+	hightParent.innerHTML = "";
+	lowParent.innerHTML = "";
+	list.forEach(function (item) {
+		if (item.prioritet === "high") {
+			createElement(hightParent, item);
+		} else {
+			createElement(lowParent, item);
+		}
+	});
+}

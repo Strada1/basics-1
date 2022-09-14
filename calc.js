a7057849c1e49433ca148143d4323f9b3a202e4a
Lunar_blessing <syrych71@gmail.com>
2022-09-14 23:46:11 +0300

    add modules

--- calc.js
+++ calc.js
@@ -1,24 +1,4 @@
-let button = document.getElementById("button")
-let result = document.getElementById("result")
-let list = document.getElementById("list")
-
-button.addEventListener('click', calc)
-
-
-function calc() {
-    const x = getResult();
-    
-    result.innerHTML = x;
-
-    let divItem = document.createElement('div');
-    divItem.innerHTML = x;
-    divItem.addEventListener('click', function(){
-        divItem.remove()
-    })
-    list.prepend(divItem);
-}
-
-function getResult() {
+export function getResult() {
     const a = +document.getElementById('a').value;
     const b = +document.getElementById('b').value;
     const operation = document.getElementById('operation').value;

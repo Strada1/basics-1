1e439526534f75f16e50e9696ac55a7baa4bbb9b
parcanec <igarek200706@gmail.com>
2022-09-14 16:55:48 +0300

    refactoring

--- parcanec/todo_ui/main.js
+++ parcanec/todo_ui/main.js
@@ -1,24 +1,20 @@
-const formHigh = document.querySelector('.add_task')
-function addTaskHigh(evt){
-    let taskName = document.getElementById('addHigh').value
-    document.querySelector('#highList').insertAdjacentHTML('afterbegin', `
-    <div class='task_list'>
-    <label><input type="radio">${taskName}<span></span></label>
-    <button id="delButton">×</button>
-    </div>`)
-    evt.preventDefault()
-}
-formHigh.addEventListener('submit', addTaskHigh)
+	const HighDiv = document.querySelector('#high') 
+	const LowDiv = document.querySelector('#low') 
+	todo(HighDiv)
+	todo(LowDiv)
 
-
-const formLow = document.querySelector('.add_task_low')
-function addTaskLow(evt){
-    let taskName = document.getElementById('addLow').value
-    document.querySelector('#lowList').insertAdjacentHTML('afterbegin', `
-    <div class='task_list'>
-    <label><input type="radio">${taskName}<span></span></label>
-    <button id="delButton">×</button>
-    </div>`)
-    evt.preventDefault()
-}
-formLow.addEventListener('submit', addTaskLow)
+function todo (anyDiv) {
+    const Form =  anyDiv.querySelector('.add_task')
+    Form.addEventListener('submit', addTask)
+    
+    function addTask(evt){
+        let taskName = Form.querySelector('input').value
+        anyDiv.querySelector('.list').insertAdjacentHTML('afterbegin', `
+			<div class='task_list'>
+			<label><input type="radio">${taskName}<span></span></label>
+			<button id="delButton">×</button>
+			</div>`)
+        evt.preventDefault()
+        Form.querySelector('input').value = ''
+    }
+}  

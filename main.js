3d770c1f1cbb069b979ff130732fdeb0ec610854
Kirill108 <kirill108bol@gmail.com>
2022-09-14 17:24:32 +0200

    Не получаеться! Не знаю что делать ):

---
+++ main.js
@@ -0,0 +1,142 @@
+const STATUS  = {
+	TO_DO: "To Do",
+	In_Progress: "In Progress",
+	Done: "Done",
+  }
+
+const PRIORITY = {
+	High: "high",
+	Low: "low",
+  }
+
+const input_addtask = document.getElementById("input_addtask")
+const input_checkbox = document.getElementById("input_checkbox")
+const form_addtask = document.getElementById("form_addtask")
+
+form_addtask.addEventListener("submit", addtask)
+
+const list = [ ];
+
+function addtask (event) {
+	event.preventDefault();
+
+	const value_input = input_addtask.value;
+
+	const indexObj = list.findIndex(function(item){
+		return item.Name == value_input;
+	  })
+	  if (indexObj == -1) {
+		list.push({Name: value_input,
+		  status: STATUS.TO_DO,
+		  priority: PRIORITY.High,}) 
+	 
+		form_addtask.insertAdjacentHTML("afterend", 
+		`
+		<div> 
+			<label class="input_checkbox" id="input_checkbox">
+				<input type="checkbox" name="value" value="option">
+				${value_input} (${STATUS.TO_DO})
+			</label>
+		</div>
+		`
+		)
+		form_addtask.reset()
+	  } else {
+		alert("Уже есть такая задача")
+	  }
+}
+
+  
+//   function changeStatus(task, newStatus) {
+// 	const indexObj = list.findIndex(function(item){
+// 	  return item.Name == task
+// 	})
+// 	if(indexObj == -1) {
+// 	  console.log(`Нет такой задачи "${task}"`)
+// 	} else {
+// 	  const MyObj = list.find(function(item) {
+// 		return item.Name == task;
+// 		})
+	
+// 		MyObj.status = newStatus;
+// 	} 
+//   }
+  
+//   function changePriority(task, newPriopity) {
+// 	const indexObj = list.findIndex(function(item){
+// 	  return item.Name == task
+// 	})
+// 	if(indexObj == -1) {
+// 	  console.log(`Нет такой задачи "${task}"`)
+// 	} else {
+// 	  const MyObj = list.find(function(item) {
+// 		return item.Name == task;
+// 	  })
+// 	  MyObj.priority = newPriopity;
+// 	}
+//   }
+  
+  
+//   function deleteTask(task) {
+// 	const IndexObj = list.findIndex(function(item){
+// 	  return item.Name == task
+// 	})
+// 	if(IndexObj == -1) {
+// 	  console.log(`Нет такой задачи "${task}"`)
+// 	} else {
+// 	  const IndexObj = list.findIndex(function(item) {
+// 		return item.Name == task;
+// 	  })
+	
+// 	  list.splice(IndexObj, 1)
+// 	}
+//   }
+  
+//   function showList() {
+  
+// 	let GroupTO_Do = ``;
+// 	let GroupIn_Progress= ``;
+// 	let Group_Done = ``;
+  
+// 	const objTodo = list.filter(function(item){
+// 	  return item.status == STATUS.TO_DO;
+// 	})
+	
+// 	objTodo.forEach(function(item){
+// 	  GroupTO_Do += `\n \t ${item.Name}, (${item.priority})`
+// 	})
+  
+// 	const objIn_Progress = list.filter(function(item){
+// 	  return item.status == STATUS.In_Progress;
+// 	})
+  
+// 	objIn_Progress.forEach(function(item){
+// 	  GroupIn_Progress += `\n \t ${item.Name}, (${item.priority})`
+// 	})
+  
+// 	const objDone = list.filter(function(item){
+// 	  return item.status == STATUS.Done;
+// 	})
+  
+// 	objDone.forEach(function(item){
+// 	  Group_Done += `\n \t ${item.Name}, (${item.priority})`
+// 	})
+	
+// 	if (GroupTO_Do == false) {
+// 	  console.log('Todo: \n \t -')
+// 	} else {
+// 	  console.log(`\n Todo:  ${GroupTO_Do}`)
+// 	}
+  
+// 	if (GroupIn_Progress == false) {
+// 	  console.log('In progress: \n \t -')
+// 	} else {
+// 	  console.log(`\n In progress:  ${GroupIn_Progress}`)
+// 	}
+   
+// 	if (Group_Done == false) {
+// 	  console.log('Done: \n \t -')
+// 	} else {
+// 	  console.log(`\n Done:  ${Group_Done}`)
+// 	}
+//   }

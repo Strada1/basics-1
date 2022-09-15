171fc1a3bcfd0b8cdbf40e176d31deeb934f83c3
codeinium <timofiei.tarasov@gmail.com>
2022-09-15 16:17:05 +0500

    added remove tasks

--- codeinium/script.js
+++ codeinium/script.js
@@ -1,11 +1,12 @@
 const list = [];                //массив задач todo листа
-const STATUS = {TO_DO: 'To Do',  
+const STATUS = {TO_DO:'To Do',  
                 DONE:'Done'};              // массив статусов
 const PRIORITY = {
     HIGH: "High",
     LOW: "Low"
 }
 const ELEMENTS = {
+    CONTAINERTODO: document.querySelector('.container'),
     HIGHPRIORITYINPUT: document.getElementById('high-priority-input'),
     LOWPRIORITYINPUT: document.getElementById('low-priority-input'),
     HIGHINPUTCONTAINER: document.getElementsByClassName('input-container')[0],
@@ -14,101 +15,98 @@ const ELEMENTS = {
     LOWTASKFORM: document.getElementsByClassName('form')[1],
     HIGHADDBUTTON: document.querySelector('#high-add-button'),
     LOWADDBUTTON: document.querySelector('#low-add-button'),
-    TASKOUTER: document.querySelectorAll('.task-outer')
+    TASKOUTER: document.querySelectorAll('.task-outer'),
+    BODY: document.body
 }
 const nullString = ''
 
 let i = 0;
-
-function createTask(inputelem, place, priority) {
+function createTaskUI(inputelem, place, priority) {
     i += 1;
     if (inputelem.value !== nullString) {
-        
         place.insertAdjacentHTML('afterend', `
-            <div class="task-outer" id="${i}">
-                <div class="task-inner">
-                    <div class="text-container">
-                        <input type="checkbox" id="task" name="task"> 
-                        <span class="texttask">${inputelem.value}</span>      
-                        <button class="button"> <img src="./img/close-icon.svg"> </button>
-                    </div>
+            <div id="task-outer${i}">
+                <div class="text-container">
+                    <input type="checkbox" class="box" id="${i}" name="task"> 
+                    <label for="${i}" class="texttask">${inputelem.value}</label>      
+                    <button id="button${i}"> <img src="./img/close-icon.svg"> </button>
                 </div>
             </div>
             `);
+        const button = ELEMENTS.CONTAINERTODO.querySelector(`#button${i}`)
+        const div = document.querySelector(`#task-outer${i}`)
+        button.addEventListener('click', () => {
+            div.remove();
+            removeTaskFromList(i);
+        });
+        // const checkbox = ELEMENTS.CONTAINERTODO.querySelector(`#${i}`)
+        // checkbox.addEventListener('change', () => {
+        //     if (this.checked) {
+        //         changeStatusFromList(i, STATUS.DONE);
+        //     } else {
+        //         changeStatusFromList(i, STATUS.TO_DO)
+        //     };
+        // });
         list.push({id: i, status: STATUS.TO_DO, priority});
         inputelem.value = nullString;
+        render();
     } else {
         alert('Введите что-нибудь');
     }
 }
-function removeTask() {
-    let taskOuter = document.querySelectorAll('.task-outer')
-    let buttonOnTasks = document.querySelectorAll('.button')
-    for (elem of taskOuter){
-        for (let i = 0; i < list.length; i++) {
-            if (list[i].id === +elem.id) {
-                for (butt of buttonOnTasks) {
-                    butt.addEventListener('click', (event) => {
-                        event.preventDefault();
-                        butt.remove();
-                    })
-                }
-            }
-        }
-    }
-};
 
-
-// function removeTask() {
-//     for (let task of tasks) {
-//         if (list.find(item => item.id === task.innerHTML)) {
-//             list.splice(list.findIndex(item => item.name === taskName, 0), 1);
-//         }
-//     }
-// }
-
-function changeStatus(taskName, taskStatus) { // функция изменения статуса todo листа
-    for (let i = 0;  i < list.length; i++) {
-        if (list[i].name === taskName && taskStatus !== undefined) {
-            list[i].status = taskStatus;
-        } else return;
+function removeTaskFromList(elem) {
+    if (list.find(item => item.id === elem)) {
+        list.splice(list.findIndex(item => item.id === elem, 0), 1);
     }
 }
 
-// function deleteTask (taskName) {
-//     if (list.find(item => item.name === taskName)) {
-//         list.splice(list.findIndex(item => item.name === taskName, 0), 1);
-//     }
-// }
 
+function changeStasusUI() {
+    let checkboxes = document.querySelectorAll(".box");
+    for (elem of checkboxes) {
+        elem.addEventListener('change', () => {
+            if (this.checked) {
+                changeStatusFromList(elem.id, STATUS.DONE);
+            } else {
+                changeStatusFromList(elem.id, STATUS.TO_DO)
+            };
+        });
+    };
+}
 
+function changeStatusFromList(id, taskStatus) { // функция изменения статуса todo листа
+    if (list.find(item => item.id == id)) {
+        list[i].status = taskStatus;
+    }
+}
+
+function render() {
+    list.forEach(() => {
+        cre
+    })
+
+}
 ELEMENTS.HIGHADDBUTTON.addEventListener('click', (event) => {
     event.preventDefault(); 
-    createTask(ELEMENTS.HIGHPRIORITYINPUT, ELEMENTS.HIGHINPUTCONTAINER, PRIORITY.HIGH);
+    createTaskUI(ELEMENTS.HIGHPRIORITYINPUT, ELEMENTS.HIGHINPUTCONTAINER, PRIORITY.HIGH);
 })
 
 ELEMENTS.LOWADDBUTTON.addEventListener('click', (event) => {
     event.preventDefault(); 
-    createTask(ELEMENTS.LOWPRIORITYINPUT, ELEMENTS.LOWINPUTCONTAINER, PRIORITY.HIGH);
+    createTaskUI(ELEMENTS.LOWPRIORITYINPUT, ELEMENTS.LOWINPUTCONTAINER, PRIORITY.LOW);
 })
 
 ELEMENTS.HIGHTASKFORM.addEventListener('submit', (event) => {
     event.preventDefault(); 
-    createTask(ELEMENTS.HIGHPRIORITYINPUT, ELEMENTS.HIGHPUTCONTAINER, PRIORITY.LOW);
+    createTaskUI(ELEMENTS.HIGHPRIORITYINPUT, ELEMENTS.HIGHPUTCONTAINER, PRIORITY.HIGH);
 })
 
 ELEMENTS.LOWTASKFORM.addEventListener('submit', (event) => {
     event.preventDefault(); 
-    createTask(ELEMENTS.LOWPRIORITYINPUT, ELEMENTS.LOWINPUTCONTAINER, PRIORITY.LOW);
+    createTaskUI(ELEMENTS.LOWPRIORITYINPUT, ELEMENTS.LOWINPUTCONTAINER, PRIORITY.LOW);
 })
-
-// function changePriority (taskName, taskPriority) {
-//     for (let i = 0;  i < list.length; i++) {
-//         if (list[i].name === taskName && taskPriority !== undefined) {
-//             list[i].priority = taskPriority;
-//         } else return;
-//     }
-// }
-
-
+ELEMENTS.BODY.onload(() => {
+    render()
+})
 

70b8bc97fd01b2c57a24396f692c585b9c60ce1a
Jarvis-DeNero <github.nikiforov94@gmail.com>
2022-09-16 02:58:28 +0300

    #45: Полноценный ToDo с UI - 1.0

--- JarvisDeNero_practice-42/js/main.js
+++ JarvisDeNero_practice-42/js/main.js
@@ -4,31 +4,121 @@ function getDomElement(selector, all = false) {
    }
    return document.querySelector(selector);
 }
+const { log } = console;
 
+// All const ------------------------------------------------------------
+const appBody = getDomElement('.app-todo__body');
 const allAddForm = getDomElement('.app-todo__add-to-list form', true)
+const highTaskList = getDomElement('.app-todo__all-task.high');
+const lowTaskList = getDomElement('.app-todo__all-task.low');
+const highInput = getDomElement('input[data-priority="high"]');
+const lowInput = getDomElement('input[data-priority="low"]');
+
 let conterIdForTask = 0;
+
+const PRIORITY = {
+   LOW: 'low',
+   HIGH: 'high',
+}
+
+const STATUSES = {
+   TO_DO: 'To Do',
+   DONE: 'Done',
+}
+
+const TASK_LIST = [
+
+];
+
+// EVENTS ------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+   render(TASK_LIST);
+});
 
 for (form of allAddForm) {
    form.addEventListener('submit', (event) => {
       event.preventDefault();
-      createNewTask(event.target);
-      event.target[0].value = '';
+      pushToArrTask(event.target);
    })
 }
 
-function createNewTask(form) {
-   const inputIndex = 0;
-   const value = form[inputIndex].value;
-   const taskList = getDomElement(`.app-todo__all-task.${form[inputIndex].dataset.priority}`);
+appBody.addEventListener('click', (event) => {
+   if (event.target.classList.contains('task__delete-btn')) {
+      deleteTaskFromArr(event.target.dataset.id);
+   }
+
+})
+
+appBody.addEventListener('input', (event) => {
+   if (event.target.classList.contains('task__checkbox')) {
+      setTaskStatus(event.target)
+   }
+})
+
+// RENDER ------------------------------------------------------------
+
+function render(arr) {
+   highTaskList.innerHTML = '';
+   lowTaskList.innerHTML = '';
+
+   arr.forEach(task => {
+      addTaskToList(task.name, task.status, task.priority, task.id);
+   });
+}
+
+function addTaskToList(value, status, priority, id) {
    const task = document.createElement('div');
    task.classList.add('task__item');
    task.innerHTML = `
-      <input type="checkbox" id="${conterIdForTask}">
-      <label for="${conterIdForTask++}">
-         ${value ? value : null}
+      <input class="task__checkbox" type="checkbox" id="${id}" ${(status === STATUSES.DONE) ? 'checked' : ''}>
+      <label for="${id}">
+         ${value ? value : 'Значение не задано!'}
       </label>
-      <button class="task__delete-btn"></button>
+      <button class="task__delete-btn" data-id="${id}"></button>
    `;
-   taskList.append(task);
+
+   if (priority === 'high') {
+      return highTaskList.append(task);
+   }
+
+   return lowTaskList.append(task);
 }
 
+// ADD TASK TO ARR ------------------------------------------------------------
+
+function pushToArrTask(currentForm) {
+   const currentInput = (currentForm.contains(highInput)) ? highInput : lowInput;
+   const currentId = conterIdForTask++;
+   const currentStatus = (currentInput.checked) ? STATUSES.DONE : STATUSES.TO_DO;
+   const currentPriority = (currentInput.dataset.priority === PRIORITY.HIGH) ? PRIORITY.HIGH : PRIORITY.LOW;
+
+   TASK_LIST.push({
+      name: currentInput.value,
+      status: currentStatus,
+      priority: currentPriority,
+      id: currentId,
+   })
+
+   render(TASK_LIST);
+
+   return currentInput.value = '';
+}
+
+// DELETE TASK ------------------------------------------------------------
+
+function deleteTaskFromArr(currentId) {
+   let deleteIndex = TASK_LIST.findIndex(task => Number(task.id) === Number(currentId));
+   TASK_LIST.splice(deleteIndex, 1);
+
+   render(TASK_LIST);
+}
+
+// LISTEN TASK STATUS ------------------------------------------------------------
+
+function setTaskStatus(input) {
+   const taskIndex = TASK_LIST.findIndex(task => Number(task.id) === Number(input.id));
+   TASK_LIST[taskIndex].status = input.checked ? STATUSES.DONE : STATUSES.TO_DO;
+
+   render(TASK_LIST);
+}

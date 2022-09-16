6dcc59922115aa1146a98d9723132b33ccfef686
Kirill108 <kirill108bol@gmail.com>
2022-09-16 09:33:31 +0200

    задачи не удаляються /:

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ const form_addtask_Low = document.getElementById("form_addtask_Low")
 form_addtask_High.addEventListener("submit", addtask_High)
 form_addtask_Low.addEventListener("submit", addtask_Low)
 
-
 const list = [ ];
 
 	function addtask_High () {
@@ -36,7 +35,6 @@ const list = [ ];
 				priority: PRIORITY.High,}) 
 				
 				render(event)
-				add_Listner_In_Task()
 			} else {
 				alert("Уже есть такая задача")
 			}
@@ -59,7 +57,6 @@ const list = [ ];
 				priority: PRIORITY.Low,}) 
 				
 				render(event)
-				add_Listner_In_Task()
 		
 			} else {
 				alert("Уже есть такая задача")
@@ -70,28 +67,9 @@ const list = [ ];
 		}
 	}
 
-	function add_Listner_In_Task () {
-		const close_Task = document.getElementById("close_Task")
-
-		close_Task.addEventListener("click", deleteTask)
-	}
-
 	function deleteTask(event) {
 		try {
-
-			const deleteTask = event.currentTarget
-			
-			let IndexObj = list.findIndex(function(item){
-				return item.Name == deleteTask
-			  })
-			  
-				 IndexObj = list.findIndex(function(item) {
-				  return item.Name == deleteTask;
-				})
-			  
-				// list.splice(IndexObj, 1)
-				list[IndexObj].status = STATUS.Done
-			  
+			alert("klik")
 
 			  render(event)
 
@@ -100,8 +78,6 @@ const list = [ ];
 		}
 		
 	  }
-
-
 
 	function render (event) {
 		try {
@@ -119,7 +95,7 @@ const list = [ ];
 						<div> 
 							<label class="input_checkbox" id="input_checkbox">
 								<input type="checkbox" name="value" value="option">
-								<input type="submit" value="&#9746" class="btn_close" id="close_Task">
+								<input type="submit" value="&#9746" class="btn_close" id="close_Task" onclick="deleteTask(event)">
 								${item.Name}
 							</label>
 						</div>
@@ -133,7 +109,7 @@ const list = [ ];
 						<div> 
 							<label class="input_checkbox" id="input_checkbox">
 								<input type="checkbox" name="value" value="option">
-								<input type="submit" value="&#9746" class="btn_close" id="close_Task">
+								<input type="submit" value="&#9746" class="btn_close" id="close_Task" onclick="deleteTask(event)">
 								${item.Name}
 							</label>
 						</div>
@@ -147,56 +123,4 @@ const list = [ ];
 			alert(err)
 		}
 	}
-
-
-
-  function showList() {
-  
-	let GroupTO_Do = ``;
-	let GroupIn_Progress= ``;
-	let Group_Done = ``;
-  
-	const objTodo = list.filter(function(item){
-	  return item.status == STATUS.TO_DO;
-	})
-	
-	objTodo.forEach(function(item){
-	  GroupTO_Do += `\n \t ${item.Name}, (${item.priority})`
-	})
-  
-	const objIn_Progress = list.filter(function(item){
-	  return item.status == STATUS.In_Progress;
-	})
-  
-	objIn_Progress.forEach(function(item){
-	  GroupIn_Progress += `\n \t ${item.Name}, (${item.priority})`
-	})
-  
-	const objDone = list.filter(function(item){
-	  return item.status == STATUS.Done;
-	})
-  
-	objDone.forEach(function(item){
-	  Group_Done += `\n \t ${item.Name}, (${item.priority})`
-	})
-	
-	if (GroupTO_Do == false) {
-	  console.log('Todo: \n \t -')
-	} else {
-	  console.log(`\n Todo:  ${GroupTO_Do}`)
-	}
-  
-	if (GroupIn_Progress == false) {
-	  console.log('In progress: \n \t -')
-	} else {
-	  console.log(`\n In progress:  ${GroupIn_Progress}`)
-	}
-   
-	if (Group_Done == false) {
-	  console.log('Done: \n \t -')
-	} else {
-	  console.log(`\n Done:  ${Group_Done}`)
-	}
-  }
-
   console.log(list)

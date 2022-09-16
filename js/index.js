554dac463a3fdf256723601e55f723f2115d9d87
Nikita <ifyoucan1337@yandex.ru>
2022-09-16 10:50:51 +0300

    Add array of list for data

--- js/index.js
+++ js/index.js
@@ -1,9 +1,66 @@
+const STATUS = {
+	DONE: "Done",
+	TO_DO: "To Do",
+};
+
+const PRIORITY = {
+	HIGH: 'high',
+	LOW: 'low',
+}
+
+const list = [];
+
 const forms = document.querySelectorAll('form');
 const priorityHigh = document.querySelector('.high-list');
 const priorityLow = document.querySelector('.low-list');
-const addButton = document.querySelector('.new-task__button')
+const addButton = document.querySelector('.new-task__button');
+const taskTextHigh = document.querySelector('.text_high');
+const taskTextLow = document.querySelector('.text_low');
 
-function createTask(priority) {
+let i = 0;
+
+function addTask(task, status = STATUS.TO_DO, priority) {
+	i += 1;
+
+	const newTask = {
+		id: i,
+		name: task,
+		status: status,
+		priority: priority,
+	}
+
+	return list.push(newTask)
+};
+
+function deleteTask(task) {
+	const findItem = list.findIndex(item => item.name === task.name);
+
+	if (findItem !== -1) {
+    list.splice([findItem], 1);
+  }
+}
+
+function changeStatus(task, status) {
+	const result = list.find(item => item.name === task.name);
+
+	return result.status = status;
+}
+
+forms.forEach((form) => {
+	form.addEventListener('submit', (evt) => {
+		evt.preventDefault();
+
+		if (evt.target.className === 'high') {
+			addTask(evt.target[0].value, STATUS.TO_DO, PRIORITY.HIGH)
+		} else {
+			addTask(evt.target[0].value, STATUS.TO_DO, PRIORITY.LOW)
+		}
+			
+		render()
+	} )
+})
+
+function createTask(priority, taskList, id) {
 	const task = document.createElement('li');
 	const label = document.createElement('label');
 	const divCheck = document.createElement('div');
@@ -13,47 +70,53 @@ function createTask(priority) {
 	task.classList.add('item');
 	label.classList.add('item-label');
 	divCheck.classList.add('check');
-	span.classList.add('item-text')
+	span.classList.add('item-text');
+	span.id = id;
 	closeBtn.classList.add('close');
 	closeBtn.src = 'css/img/close.png';
 	
 	
-	priority.prepend(task)
+	priority.prepend(task);
 	task.append(label);
 	label.append(divCheck);
-	label.append(span)
-	label.append(closeBtn)
+	label.append(span);
+	label.append(closeBtn);
 	
 	
 	divCheck.addEventListener('click', (evt) => {
 		
 		task.classList.toggle('checked');
 
-		evt.target.classList.contains('checked') ?
-		evt.target.classList.remove('checked') :
-		evt.target.classList.add('checked');
-	})
-
-	closeBtn.addEventListener('click', () => task.remove())	
-
-	return task;
-
-}
-
-function getClassName(priority, evt) {
-	let task = createTask(priority);
-	const span = task.querySelector('span');
-
-	span.textContent = evt.target[0].value;
-}
-
-forms.forEach((form) => {
-	form.addEventListener('submit', (evt) => {
-		evt.preventDefault();
-
-		evt.target.className === 'high' ? getClassName(priorityHigh, evt) : getClassName(priorityLow, evt)
+		if (evt.target.classList.contains('checked')) {
+			evt.target.classList.remove('checked');
+			changeStatus(taskList, STATUS.TO_DO)
+		} else {
+			evt.target.classList.add('checked');
+			changeStatus(taskList, STATUS.DONE)
+		}
+		 console.log(list)
 		
 	})
-})
 
+	closeBtn.addEventListener('click', () => {
+		task.remove();
+		deleteTask(taskList);
+		console.log(list)
+	})
 
+	span.textContent = taskList.name
+
+	return task;
+}
+
+function render() {
+	document.querySelectorAll('.item').forEach(task => task.remove());
+
+	list.map((task, idx) => {
+		if (task.priority === PRIORITY.HIGH) {
+			createTask(priorityHigh, task, task.id);
+		} else {
+			createTask(priorityLow, task, task.id)
+		}
+	})
+}

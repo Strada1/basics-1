90b44729a01581ae5e6815097a57117d9b8e809e
Kitaev Boris <borkit2000@mail.ru>
2022-09-16 19:29:51 +0300

    простая обработка ошибки

--- main.js
+++ main.js
@@ -4,17 +4,26 @@ const operations = {
     subtract: '-',
 }
 function calc() {
-    let a = +document.querySelectorAll('.input')[0].value;
-    let b = +document.querySelectorAll('.input')[1].value;
-    let action = document.getElementById('action').value;
-    switch (action) {
-        case operations.add:
-            return a + b;
-        case operations.multi:
-            return a * b;
-        case operations.subtract:
-            return a - b;
-    };
+    try {
+        let a = +document.querySelectorAll('.input')[0].value;
+        let b = +document.querySelectorAll('.input')[1].value;
+        if (isNaN(a) || isNaN(b)) {
+            throw new Error('вы ввели не число')
+        }
+        let action = document.getElementById('action').value;
+        switch (action) {
+            case operations.add:
+                return a + b;
+            case operations.multi:
+                return a * b;
+            case operations.subtract:
+                return a - b;
+        };
+    }
+    catch (err) {
+        alert(err.name);
+        alert(err.message);
+    }
 }
 
 let btn = document.getElementById('btn');

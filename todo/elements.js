46a8a831f66efdaa774bcb92ebc24135bc3b9fa0
Dmitriy Bakhtin <dmitri.bakhtin@gmail.com>
2022-09-17 14:09:53 +0300

    ui todo

---
+++ todo/elements.js
@@ -0,0 +1,6 @@
+export const ELEMENTS = {
+	FORMS: document.querySelectorAll('.todo_form'),
+	TASKS_LISTS: document.querySelectorAll('.todo_list'),
+	HIGH_PRIORITY_LIST: document.querySelector('.todo_list[data-priority="high"]'),
+	LOW_PRIORITY_LIST: document.querySelector('.todo_list[data-priority="low"]')
+}

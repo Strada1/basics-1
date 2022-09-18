146c10943261c475f1f6cd18b6b52595d8af6234
maxoldgood <maxoldgood@gmail.com>
2022-09-18 22:12:37 +0700

    added main functions and styles

--- maxoldgood/main.js
+++ maxoldgood/main.js
@@ -4,7 +4,8 @@ const highPriorityBlock = document.querySelector (".task-block.priority-high");
 const lowPriorityBlock = document.querySelector(".task-block.priority-low")
 const inputHighTask = document.getElementById("add-high-task-input");
 const inputLowTask = document.getElementById("add-low-task-input");
-const deleteButton =document.querySelector(".delete-task-button");
+const deleteButton = document.querySelector(".delete-task-button");
+const checkBox = document.querySelector(".complete-task-button");
 
 addTaskHigh.addEventListener("submit", addTaskFunctionHigh);
 addTaskLow.addEventListener("submit", addTaskFunctionLow);
@@ -26,6 +27,7 @@ addTaskLow.addEventListener("submit", addTaskFunctionLow);
         let addedTask = {} 
         addedTask.name = inputHighTask.value;
         addedTask.priority = highPriority;
+        addedTask.status = toDo;
         list.push(addedTask); 
         inputHighTask.value="";
         render();    
@@ -36,6 +38,7 @@ addTaskLow.addEventListener("submit", addTaskFunctionLow);
         let addedTask = {} 
         addedTask.name = inputLowTask.value;
         addedTask.priority = "low";
+        addedTask.status = toDo;
         list.push(addedTask);
         inputLowTask.value="";
         render()
@@ -49,7 +52,16 @@ addTaskLow.addEventListener("submit", addTaskFunctionLow);
         list.splice(taskToDeleteIndex,1);
         render();
     }
-
+    
+    function changeStatus (){
+        let checkButton = event.currentTarget;
+        let taskName = checkButton.nextElementSibling.innerHTML;
+        let taskComplete = list.find(item =>item.name === taskName);
+       if (checkButton.checked) {
+        taskComplete.status = done;
+       } else taskComplete.status = toDo;
+    }
+    
 
     function render() {
         let allTasks = document.querySelectorAll(".task");
@@ -58,18 +70,19 @@ addTaskLow.addEventListener("submit", addTaskFunctionLow);
         }
         for (let tasks of list) {
             if (tasks.priority === highPriority){
-                
+                let isDone = (tasks.status === done) ? `checked` : "";
                 highPriorityBlock.insertAdjacentHTML("beforeend", `
                 <div class="task">
-                    <input class="complete-task-button" type="checkbox">
+                    <input class="complete-task-button" type="checkbox" ${isDone} onclick="changeStatus()">
                     <div class="task-name">${tasks.name}</div>
                     <input class="delete-task-button" onclick="deleteTask()" type="image" src="./img/close-icon.svg">
                 </div>`);
             }
             else if (tasks.priority === lowPriority) {
+                let isDone = (tasks.status === done) ? "checked" : "";
                 lowPriorityBlock.insertAdjacentHTML("beforeend", `
                 <div class="task">
-                    <input class="complete-task-button" type="checkbox">
+                    <input class="complete-task-button" type="checkbox" ${isDone} onclick ="changeStatus()">
                     <div class="task-name">${tasks.name}</div>
                     <input class="delete-task-button" onclick="deleteTask()" type="image" src="./img/close-icon.svg">
                 </div>`);

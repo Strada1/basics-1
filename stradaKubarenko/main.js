f0f592f2d2705b0fe89574000d8fe19e53c5635d
AndreyKubarenko <akubarenko1986@gmail.com>
2022-09-19 16:25:19 +0300

    не работает addTask

--- stradaKubarenko/main.js
+++ stradaKubarenko/main.js
@@ -3,26 +3,24 @@ const tasks = {
    LOW_PRIORITY: document.getElementById('add_task_low'),
 }
 const elements = {
-   UL : document.querySelectorAll('ul.flex')
+   UL : document.querySelectorAll('ul.flex'),
 }
 const addTaskHighPriority = tasks.HIGH_PRIORITY;
 const addTaskLowPriority = tasks.LOW_PRIORITY;
 
-addTaskHighPriority.addEventLister('submit', addTask );
-addTaskLowPriority.addEventLister('submit', addTask );
-
+addTaskHighPriority.addEventListener('submit', addTask );
+addTaskLowPriority.addEventListener('submit', addTask );
 function addTask() {
-//   let textHighPriority = tasks.HIGH_PRIORITY.textContent;
+  let textHighPriority = tasks.HIGH_PRIORITY.textContent;
 //   let textLowPriority = tasks.LOW_PRIORITY.textContent;
   let li = document.createElement('li');
   li.classList.add("box","delete");
   elements.UL.append(li);
   li.innerHTML = `
-   <label>
-      <input type="checkbox">
-      <button type="submit" class="delButton">del</button>
-   </label>`;
-   // li = textHighPriority;
-
+  <label>
+  <input type="checkbox">
+  <button type="submit" class="delButton">del</button>
+  </label>`;
+   li = textHighPriority;
 }
 

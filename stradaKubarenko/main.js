649c18527f94f43ae9f85298bad7f5b340b8f419
AndreyKubarenko <akubarenko1986@gmail.com>
2022-09-19 19:21:59 +0300

    не вставляется текс из инпута в новый элемент

--- stradaKubarenko/main.js
+++ stradaKubarenko/main.js
@@ -3,24 +3,31 @@ const tasks = {
    LOW_PRIORITY: document.getElementById('add_task_low'),
 }
 const elements = {
-   UL : document.querySelectorAll('ul.flex'),
+   // UL : document.querySelectorAll('ul.flex'),
+   UL_HIGH : document.getElementById('listHigh'),
+   UL_LOW : document.getElementById('listLow'),
+   TEXT_INPUT_HIGH : document.getElementById('textInputHigh'),
+   TEXT_INPUT_LOW : document.getElementById('textInputLow'),
 }
 const addTaskHighPriority = tasks.HIGH_PRIORITY;
 const addTaskLowPriority = tasks.LOW_PRIORITY;
 
 addTaskHighPriority.addEventListener('submit', addTask );
 addTaskLowPriority.addEventListener('submit', addTask );
-function addTask() {
-  let textHighPriority = tasks.HIGH_PRIORITY.textContent;
-//   let textLowPriority = tasks.LOW_PRIORITY.textContent;
-  let li = document.createElement('li');
-  li.classList.add("box","delete");
-  elements.UL.append(li);
-  li.innerHTML = `
-  <label>
-  <input type="checkbox">
-  <button type="submit" class="delButton">del</button>
-  </label>`;
-   li = textHighPriority;
+
+function addTask(event) {
+   event.preventDefault()
+   let textHighPriority = elements.TEXT_INPUT_HIGH.textContent;
+ 
+   alert(textHighPriority)
+   // let textLowPriority = elements.TEXT_INPUT_LOW.textContent;
+   elements.UL_HIGH.insertAdjacentHTML("afterbegin", `
+   <li class="box delete"> 
+      <label>
+      <input type="checkbox">
+      <button type="submit" class="delButton">del</button>
+      </label>
+   </li>`);
+   
 }
 

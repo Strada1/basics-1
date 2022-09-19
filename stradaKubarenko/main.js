934d7ff630e48faf0b822e24980f36b7be55aaa7
AndreyKubarenko <akubarenko1986@gmail.com>
2022-09-19 08:08:09 +0300

    выдаёт ошибку при навешивании обработчика событий

---
+++ stradaKubarenko/main.js
@@ -0,0 +1,28 @@
+const tasks = {
+   HIGH_PRIORITY: document.getElementById('add_task_high'),
+   LOW_PRIORITY: document.getElementById('add_task_low'),
+}
+const elements = {
+   UL : document.querySelectorAll('ul.flex')
+}
+const addTaskHighPriority = tasks.HIGH_PRIORITY;
+const addTaskLowPriority = tasks.LOW_PRIORITY;
+
+addTaskHighPriority.addEventLister('submit', addTask );
+addTaskLowPriority.addEventLister('submit', addTask );
+
+function addTask() {
+//   let textHighPriority = tasks.HIGH_PRIORITY.textContent;
+//   let textLowPriority = tasks.LOW_PRIORITY.textContent;
+  let li = document.createElement('li');
+  li.classList.add("box","delete");
+  elements.UL.append(li);
+  li.innerHTML = `
+   <label>
+      <input type="checkbox">
+      <button type="submit" class="delButton">del</button>
+   </label>`;
+   // li = textHighPriority;
+
+}
+

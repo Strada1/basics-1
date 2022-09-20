84265662c0c7574d5c86b7ead7ff8dfcec7f0cb9
pipupopi <sonalavrushin@gmail.com>
2022-09-20 12:43:11 +0300

    о!

--- todo/todo.js
+++ todo/todo.js
@@ -1,118 +1,103 @@
-const AddTaskFormHigh = document.querySelector(".add-task-form-high");
-const InputAddTaskHigh = document.querySelector(".add-task-input-high");
-const EmptyDivHigh = document.querySelector(".EmptyDivHigh");
-const AddTaskFormLow = document.querySelector(".add-task-form-low");
-const InputAddTasklow = document.querySelector(".add-task-input-low");
-const EmptyDivLow = document.querySelector(".EmptyDivLow");
-//const deleteTask = document.querySelectorAll('.delete')
-
 const STATUS = {
-  TO_DO: "To Do",
+  TO_DO: "To do",
   DONE: "Done",
 };
 
 const PRIORITY = {
-  LOW: "Low",
   HIGH: "High",
+  LOW: "Low",
 };
 
 const list = [
-  { name: "Поесть", status: STATUS.TO_DO, priority: PRIORITY.HIGH },
-  {
-    name: "Сверстать этот TODO list",
-    status: STATUS.TO_DO,
-    priority: PRIORITY.HIGH,
-  },
-  {
-    name: "Начать делать задачу",
-    status: STATUS.TO_DO,
-    priority: PRIORITY.HIGH,
-  },
-  {
-    name: "Начать делать задачу",
-    status: STATUS.TO_DO,
-    priority: PRIORITY.LOW,
-  },
+
 ];
 
-AddTaskFormHigh.addEventListener("submit", addTaskHigh);
-AddTaskFormLow.addEventListener("submit", addTaskLow);
+const formHigh = document.querySelector(".add-task-form-high");
+const formLow = document.querySelector(".add-task-form-low");
+const inputHigh = document.querySelector(".add-task-input-high");
+const inputLow = document.querySelector(".add-task-input-low");
 
-function addTaskHigh(event) {
+//добавление верстки в массив (LOW, HIGH)
+formHigh.addEventListener("submit", function (event) {
   event.preventDefault();
-  task = document.querySelector(".add-task-input-high");
-  if (task.value.trim() == "") {
-    alert("Введите имя задачи");
-  
-} else if (!(isNaN(task.value))) {
-    alert("Имя задачи не может быть числом");
-  
-} else {
-    list.push({
-      name: task.value,
-      status: STATUS.TO_DO,
-      priority: PRIORITY.HIGH,
-    });
-    renderHigh();
-  }
-}
-
-function addTaskLow(event) {
-  event.preventDefault();
-  task = document.querySelector(".add-task-input-low");
-  if (task.value.trim() == "") {
-    alert("Введите имя задачи");
-  } else if (!(isNaN(task.value))) {
-    alert("Имя задачи не может быть числом");
-  } else {
-    list.push({
-      name: task.value,
-      status: STATUS.TO_DO,
-      priority: PRIORITY.LOW,
-    });
-    renderLow();
-  }
-}
-
-function renderHigh() {
-  if (InputAddTaskHigh.value == "") {
-    console.log("пустая");
-  } else {
-    EmptyDivHigh.insertAdjacentHTML(
-      "afterbegin",
-      `<form class="high-task-form">
-      <input type="radio" class="TasksBox" id="HighTasks">
-      <span class="tasks-span">${InputAddTaskHigh.value}</span>
-      <button class="delete">х</button>
-  </form>`
-    );
-  }
-}
-
-function renderLow() {
-    if (InputAddTasklow.value == "") {
-        console.log("пустая");
-      } else {
-EmptyDivLow.insertAdjacentHTML(
-      "afterbegin",
-      `<form class="low-task-form">
-    <input type="radio" class="TasksBox" id="LowTasks">
-    <span class="tasks-span">${InputAddTasklow.value}</span>
-    <button class="delete">х</button>
-</form>`
-    );
-  }
-}
-
-const deleteTask = document.querySelectorAll('.delete')
-deleteTask.forEach( (task) => {
-  task.addEventListener('click', deleteTasklist)
+  nameTask = inputHigh.value;
+  addTask(nameTask, PRIORITY.HIGH);
 });
 
-function deleteTasklist() {
-  let newlist = list.filter((task) => {
-  (!(task.name === deleteTask))
+formLow.addEventListener("submit", function (event) {
+  event.preventDefault();
+  nameTask = inputLow.value;
+  addTask(nameTask, PRIORITY.LOW);
+});
+
+//добавление
+
+function addTask(nameTask, taskPriority) {
+  list.push({
+    name: nameTask,
+    status: STATUS.TO_DO,
+    priority: taskPriority,
+  });
+  render();
+}
+
+
+//удаление
+
+function deleteTask(nameTask) {
+  const indexTask = list.findIndex(function (element) {
+    return element.name === nameTask;
+  });
+  list.splice(indexTask, 1);
+  render();
+}
+
+
+//поменять статус
+
+function chengeStatus(nameTask) {
+  const statusTask = list.find(function (item) {
+    item.name === nameTask;
+  });
+  nameTask.status = STATUS.DONE
+  render();
+};
+
+
+
+//добавление в верстку
+
+function render() {
+  const highForm = document.querySelector(".high-task");
+  const lowForm = document.querySelector(".low-task");
+  inputHigh.value = ''
+  inputLow.value = ''
+  highForm.innerHTML = ''
+  lowForm.innerHTML = ''
+  list.forEach(function(item) {
+   if (item.priority === PRIORITY.HIGH) {
+    highForm.insertAdjacentHTML('afterbegin', `<div class="high-task-form"> 
+    <input type="radio" class="TasksBoxHigh" id="HighTasks">
+    <span class="tasks-span">${item.name}</span>
+    <button class="delete-high">х</button>
+</div>`)
+const deleteHigh = document.querySelector('.delete-high')
+deleteHigh.addEventListener('click', function(){
+deleteTask(item.name);
+})
+
+   } else {
+    lowForm.insertAdjacentHTML('afterbegin', `<div class="low-task-form"> 
+    <input type="radio" class="TasksBoxLow" id="HighTasks">
+    <span class="tasks-span">${item.name}</span>
+    <button class="delete-low">х</button>
+</div>`)
+const deleteLow = document.querySelector('.delete-low')
+deleteLow.addEventListener('click', function(){
+deleteTask(item.name);
+})
+   }
   })
 }
 
-console.log(list)
+

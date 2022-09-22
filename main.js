81de5f9bae493348235b1c99745c82a071197de4
Kirill108 <kirill108bol@gmail.com>
2022-09-22 16:56:15 +0200

    как вывести пол из объекта проимса?

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function getGender(event) {
 		const serverUrl = 'https://api.genderize.io';
 		const url = `${serverUrl}?name=${firstName}`;
 
-		let response = fetch(url);
-		let json = response.json();
-
-		console.log(json)
+		let response =fetch(url);
 		
-		// console.log (`response:\n ${response}`)
+		console.log (`response:\n ${response}`)
+		console.log (typeof response)
+		console.log (response.gender)
+
 		formSubmit.reset()
 
 	} catch(error) {

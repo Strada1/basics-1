1c330e8bff349a39b9ae302a04f1111b1196f5b7
Jarvis-DeNero <github.nikiforov94@gmail.com>
2022-09-22 20:31:07 +0300

    #53: Genderize 1.0 - by Jarvis De-Nero

---
+++ practicle-53/js/script.js
@@ -0,0 +1,43 @@
+const input = document.querySelector('.app__input');
+const form = document.querySelector('.app__form');
+const resultList = document.querySelector('.show-result');
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   getGender(input.value)
+   input.value = '';
+})
+
+function getGender(name) {
+   try {
+      if (!name.trim()) {
+         throw new Error('Укажите имя!');
+      }
+      const url = finalUrl(name);
+      const resultGender = fetch(url);
+      resultGender.then(result => result.json())
+         .then(result => {
+            createGenderAnswer(result.name, result.gender);
+         });
+   } catch (error) {
+      alert(`${error.name}: ${error.message}`);
+   }
+}
+
+function finalUrl(firstName) {
+   const serverUrl = 'https://api.genderize.io';
+   return `${serverUrl}?name=${firstName}`;
+}
+
+function createGenderAnswer(name, gender) {
+   try {
+      if (!gender) {
+         throw new Error(`Имя "${name}" не определно, попробуйте имя указать на Английском!`);
+      }
+      const p = document.createElement('p');
+      p.innerText = `${name} is ${gender}!`;
+      resultList.append(p);
+   } catch (error) {
+      alert(`${error.name}: ${error.message}`);
+   }
+}

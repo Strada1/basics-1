85ec876bc2df24f31a2e0175fd99b83ce8100f18
terlyga78 <telyga78@gmail.com>
2022-09-22 17:02:32 +0300

    on bootstrap

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ async function takeGender() {
 
     const serverUrl = 'https://api.genderize.io';
 
-    firstName = document.mainForm.name.value;
+    firstName = document.mainForm.inputName.value;
     let url = `${serverUrl}?name=${firstName}`;
 
     let response = await fetch(url);
@@ -12,7 +12,7 @@ async function takeGender() {
     if (response.ok) { // если HTTP-статус в диапазоне 200-299
         // получаем тело ответа (см. про этот метод ниже)       
         let json = await response.json();
-        document.mainForm.res.value = json.gender;
+        document.mainForm.giveGender.value = json.gender;
     } else {
         alert("Ошибка HTTP: " + response.status);
     }

8ed34f3f1a22f26787bff74c5db795a6236496ca
YaRolly <Ea4GoodRu@gmail.com>
2022-09-22 16:48:15 +0300

    Genderize v1.1

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector('#btn');
-const FIRST_NAME = document.querySelector('#input').value;
+const FIRST_NAME = document.querySelector('#input');
 const SERVER_URL = 'https://api.genderize.io';
-const URL = `${SERVER_URL}?name=${FIRST_NAME}`;
-btn.addEventListener('click', function () {
-  fetch(URL)
+const RESULT = document.querySelector('#result');
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+  let name = FIRST_NAME.value;
+  let url = `${SERVER_URL}?name=${name}`;
+  fetch(url)
     .then((response) => response.json())
-    .then((commits) => alert(commits.gender));
+    .then((commits) => (RESULT.textContent = commits.gender));
 });

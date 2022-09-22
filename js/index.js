2937c2a1b9887088616a67516c917ed6820a362b
Nikita <ifyoucan1337@yandex.ru>
2022-09-22 21:49:57 +0300

    Create a form, which get response(fetch)

--- js/index.js
+++ js/index.js
@@ -1,36 +1,26 @@
-import { createTask, addTask } from './more.js';
-import { STATUS, PRIORITY, list, forms, priorityHigh, priorityLow  } from './const.js';
+const firstName = 'artem';
+const serverUrl = 'https://api.genderize.io';
 
 
+const form = document.querySelector('form');
+const inputName = form.querySelector('.name');
 
-forms.forEach((form) => {
-	form.addEventListener('submit', (evt) => {
-		evt.preventDefault();
-		
-		try {
-			if (evt.target.className === 'high') {
-				addTask(evt.target[0].value, STATUS.TO_DO, PRIORITY.HIGH)
-				evt.target[0].value = '';
-			} else {
-				addTask(evt.target[0].value, STATUS.TO_DO, PRIORITY.LOW);
-				evt.target[0].value = ''
-			}
-		} catch (error) {
-			console.log(error.message)
-		}
-			
-		render()
-	} )
+function getNameAndGender(url) {
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			alert(`${data.name[0].toUpperCase() + data.name.slice(1)} is ${data.gender}`)
+		});	
+}	
+
+
+form.addEventListener('submit', evt => {
+	evt.preventDefault();
+	
+	const name = evt.target.inputName.value;
+	const url = `${serverUrl}?name=${name}`;
+
+	getNameAndGender(url);
+	
 })
 
-function render() {
-	document.querySelectorAll('.item').forEach(task => task.remove());
-
-	list.map((task) => {
-		if (task.priority === PRIORITY.HIGH) {
-			createTask(priorityHigh, task, task.id);
-		} else {
-			createTask(priorityLow, task, task.id)
-		}
-	})
-}

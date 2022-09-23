04514a2cd85a5cc40f90c289ab3beeb081296529
Korolev Michael <math.korolev@yandex.ru>
2022-09-23 15:14:30 +0300

    Refactoring proposal

--- practicle-53/js/script.js
+++ practicle-53/js/script.js
@@ -3,43 +3,40 @@ import { popupMessage } from '../modules/showMessage/showMessage.js';
 const input = document.querySelector('.app__input');
 const form = document.querySelector('.app__form');
 const resultList = document.querySelector('.show-result');
+const serverUrl = 'https://api.genderize.io';
 
-form.addEventListener('submit', (e) => {
+const generateUrl = (firstName) => `${serverUrl}?name=${firstName}`;
+const isInputValid = (name) => !!name && !!name.trim && !!name.trim();
+const getGender = (name) => {
+   const url = generateUrl(name.trim());
+   return fetch(url).then(result => result.json());
+}
+
+const showAnswer = (name, gender) => {
+   if (!gender) {
+      throw new Error(`Имя "${name}" не определно, попробуйте имя указать на Английском!`);
+   }
+   
+   const p = document.createElement('p');
+   p.innerText = `${name} is ${gender}!`;
+   resultList.append(p);
+}
+
+const hendleSubmit = async (e) => {
    e.preventDefault();
-   getGender(input.value)
-   input.value = '';
-})
-
-function getGender(name) {
-   try {
-      if (!name.trim()) {
+   const inputValue = input.value;
+   
+   try {            
+      if (!isInputValid(inputValue)) {
          throw new Error('Укажите имя!');
       }
-      const url = finalUrl(name.trim());
-      const resultGender = fetch(url);
-      resultGender.then(result => result.json())
-         .then(result => {
-            createGenderAnswer(result.name, result.gender);
-         });
+      const { name, gender } = await getGender(inputValue);
+      showAnswer(name, gender);
    } catch (error) {
       popupMessage(error.message, error.name);
    }
+
+   input.value = '';
 }
 
-function finalUrl(firstName) {
-   const serverUrl = 'https://api.genderize.io';
-   return `${serverUrl}?name=${firstName}`;
-}
-
-function createGenderAnswer(name, gender) {
-   try {
-      if (!gender) {
-         throw new Error(`Имя "${name}" не определно, попробуйте имя указать на Английском!`);
-      }
-      const p = document.createElement('p');
-      p.innerText = `${name} is ${gender}!`;
-      resultList.append(p);
-   } catch (error) {
-      popupMessage(error.message, error.name);
-   }
-}
+form.addEventListener('submit', hendleSubmit);

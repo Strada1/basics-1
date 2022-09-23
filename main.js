e8c885062199bd1b0e358c48a5b142cf6e65d3d3
Kitaev Boris <borkit2000@mail.ru>
2022-09-23 20:32:01 +0300

    v1.0

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const form = document.querySelector('.form');
 const input = document.querySelector('.name__input');
 const output = document.querySelector('.answer__inner');
@@ -11,17 +10,14 @@ form.addEventListener('submit', getGender);
 function getGender(event) {
     event.preventDefault();
     const url = `${serverUrl}?name=${firstName.value}`;
-    fetch(url).then(
-        function (response) {
-            const json = response.json().then(
-                function (obj) {
-                    output.innerHTML = obj.gender;
-                }
-            );
-        }
-    );
+    fetch(url)
+        .then(response => response.json())
+        .then(obj => correct(obj))
 }
 
+function correct(obj) {
+    return (obj.gender === null) ? output.innerHTML = `Probably this name doesn't exist` : output.innerHTML = obj.gender;
+}
 
 
 

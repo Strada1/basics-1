a776acd206b17fc056ca1638f8f7d657719ebb1b
ZefirKefir <mr.yunusov.00@gmail.com>
2022-09-23 05:30:17 +0200

    Finished TODO

--- js/main.js
+++ js/main.js
@@ -1,33 +1,119 @@
-//calc basic
-const calc = document.querySelector('.calc__row');
-const operator = calc.querySelector('.operator-select');
-const btn = calc.querySelector('.equal-btn');
-const a = calc.querySelector('#a');
-const b = calc.querySelector('#b');
-const ans = calc.querySelector('.answer');
-//history
-const history = document.querySelector('.history')
-const historyList = document.querySelector('.history .list');
-const historyItems = historyList.querySelectorAll('.list__item');
-const historyTitle = document.querySelector('.history .title')
-
+//SOURCE
+const list = [
+    { name: 'create a post', status: 'Not Done', priority: 'high'  },
+    { name: 'Take a walk', status: 'Not Done', priority: 'medium'  },
+    { name: 'pososat\'', status: 'Not Done', priority: 'low'  },
+    { name: 'test', status: 'Done', priority: 'high'  },
+]
+//ELEMENTS
+const sections = document.querySelectorAll('.section');
+const addTaskDivs = document.querySelectorAll('.add-task-div');
+let taskDivs = document.querySelectorAll('.task-div');
 //FUNCTIONS
-const historyPusher = (answer) => {
-    let historyItem = document.createElement('li');
-    historyItem.classList.add('list__item');
-    historyItem.innerHTML = answer;
-    historyList.append(historyItem);
+//add task
+const addTask = (arr, name, status = 'Not Done', priority = 'low') => {
+    const obj = {};
+    obj.name = name;
+    obj.status = status;
+    obj.priority = priority;
+    arr.push(obj);
+    console.log(`>> Task '${name}' with status '${status}' and priority '${priority}' has been added!`)
 }
+//change status
+const changeStatus = (arr, name, status = 'Done') => {
+    let target = arr.find((item) => item.name == name);
+    if (target != null) {
+        target.status = status;
+    } else console.log('No task with such name!')
+}
+//delete task
+const deleteTask = (arr, name) => {
+    let targetIndex = arr.findIndex((item) => item.name == name);
+    arr.splice(targetIndex,1);
+    console.log(`>> Task '${name}' has been deleted!`)
+}
+//render
+const render = (list) => {
+    document.querySelectorAll('.task-div').forEach(taskDiv => taskDiv.remove());
+    const ways = (priority) => {
+        switch(priority) {
+            case 'high':
+                return sections[0];
+                break;
+            case 'medium':
+                return sections[1];
+                break
+            case 'low':
+                return sections[2];
+                break;
+        }
+    }
+    let count = 1;
+    for (let obj of list) {
+        let nest = ways(obj.priority);
+        // div
+        let div = document.createElement('div');
+        div.classList.add('task-div');
+        nest.append(div)
+        // checkbox
+        let checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox');
+        checkbox.classList.add('task-checkbox');
+        checkbox.setAttribute('id', `task${count}`)
+        if (obj.status == 'Done') checkbox.setAttribute('checked', '')
+        div.append(checkbox)
+        // label
+        let label = document.createElement('label');
+        label.classList.add('task-label');
+        label.setAttribute('for', `task${count}`);
+        div.append(label);
+        let labelText = document.createElement('p');
+        labelText.classList.add('task__name');
+        labelText.innerHTML = obj.name;
+        label.append(labelText);
+        // button
+        let button = document.createElement('button');
+        button.classList.add('finish-task-button', 'button')
+        div.append(button)
+        let img = document.createElement('img');
+        img.classList.add('finish-task-img');
+        img.setAttribute('alt', 'x');
+        img.setAttribute('src', './img/cross-icon.svg')
+        button.append(img);
+        // +count
+        count++;
+    }
+}
+render(list);
 
-//EVENT LISTENERS
-btn.addEventListener('click', () => {
-    let answer = eval(`${a.value} ${operator.value} ${b.value}`);
-    // answer = answer.toFixed(2);
-    ans.innerHTML = answer;
-    historyPusher(answer);
+addTaskDivs.forEach(div => {
+    let input = div.querySelector('.task-add-input');
+    let button = div.querySelector('.add-task-button');
+    button.addEventListener('click', () => {
+        const name = input.value;
+        const priority = button.closest('section').classList[0];
+        addTask(list, name,'Not Done', priority);
+        input.value = '';
+        render(list)
+    })
 })
+setInterval(() => {
+    document.querySelectorAll('.task-div').forEach(task => {
+        let input = task.querySelector('.task-checkbox');
+        input.addEventListener('change', () => {
+            const name = task.querySelector('.task__name').innerHTML;
+            changeStatus(list,name,(input.checked) ? 'Done' : 'Not Done');
+            render(list)
+        })
+    })
+}, 1000);
 
-history.addEventListener('click', (e) => {
-    if (e.target.classList.contains('list__item')) e.target.remove();
-    if (historyList.hasChildNodes) console.log('a')
+sections.forEach(section => {
+    section.addEventListener('click', (e) => {
+        if (e.target.classList.contains('finish-task-img')) {
+            const name = e.target.closest('div').querySelector('.task__name').innerHTML;
+            deleteTask(list, name)
+            render(list)
+        }
+    })
 })

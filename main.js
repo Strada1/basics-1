8858d4687b840ef8ee851dc810cc33bb9a27b951
Stanovov Mikhail <98664376+CTEPXMGN@users.noreply.github.com>
2022-09-24 17:37:50 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import { UI_ELEMENTS } from "./view.js";
+
+UI_ELEMENTS.FIND_CITY.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    console.log('!!!');
+});
---
+++ view.js
@@ -0,0 +1,3 @@
+export const UI_ELEMENTS = {
+    FIND_CITY: document.querySelector('.find__button'),
+};

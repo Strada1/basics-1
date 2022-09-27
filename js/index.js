67bd041b396d965983bee78d4fb10576a14c8eae
Nikita <ifyoucan1337@yandex.ru>
2022-09-27 23:17:38 +0300

    Add catch method at Promise

--- js/index.js
+++ js/index.js
@@ -9,19 +9,25 @@ import {
 import { getData } from './fetch.js';
 
 
+const render = (data) => {
+	return data.then(obj => {
+		const temper = obj.main.temp;
+		const icon = obj.weather[0].icon;
+		const name = obj.name;
+
+		temperature.textContent = temper + '°';
+		img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+		nameCity.textContent = name;
+	})
+}
+
 form.addEventListener('submit', evt => {
 	evt.preventDefault();
 	const cityName = inputSearch.value;
 
 	const data = getData(cityName)
 	
-	data.then(obj => {
-		const temper = obj.main.temp;
-		const icon = obj.weather[0].icon;
-		const name = obj.name;
-
-		temperature.textContent = temper + '°';
-		img.src = icon;
-		nameCity.textContent = name;
-	})
+	render(data);
+	
+	inputSearch.value = '';
 })

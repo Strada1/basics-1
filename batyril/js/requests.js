e2fe9335158ab55d81104592e57e5d6813413547
batyril <237x237@gmail.ru>
2022-09-28 02:58:30 +0300

    added request error handling

--- batyril/js/requests.js
+++ batyril/js/requests.js
@@ -5,7 +5,18 @@ export function getCityWeather(cityName) {
   const API_KEY = '358eaa62b262b36cac42f77b107308e8';
   const url = `${SERVER_URL}?q=${cityName}&appid=${API_KEY}`;
   fetch(url)
-    .then((response) => response.json())
+    .then((response) => {
+      switch (response.status) {
+        case 200:
+          return response.json();
+        case 401:
+          throw new Error('token error');
+        case 404:
+          throw new Error('Not Found');
+        default:
+          throw new Error('error');
+      }
+    })
     .then((weather) => renderNowTab(weather, UI_NOW))
-    .catch((error) => alert(error.message));
+    .catch((e) => console.log(e.message));
 }

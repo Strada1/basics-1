ddb0c608fe3232f534c61c2be8b29158946a5870
terlyga78 <telyga78@gmail.com>
2022-09-28 22:51:11 +0300

    #56-Погода: Запросы

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function openCity(evt, cityName) {
+const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
+
+const inputCity = document.getElementById("inputCity");
+const nowCity = document.getElementById("nowCity");
+const nowTemperature = document.getElementById("nowTemperature");
+
+function openTab(evt, tabName) {
     var i, tabcontent, tablinks;
 
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -11,6 +18,28 @@ function openCity(evt, cityName) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    document.getElementById(cityName).style.display = "flex";
+    document.getElementById(tabName).style.display = "flex";
     evt.currentTarget.className += " active";
 }
+
+async function searchClick() {
+
+    let url = `${serverUrl}?q=${inputCity.value.trim()}&appid=${apiKey}`;
+    let response = await fetch(url);
+    let json;
+
+    if (!response.ok) { // если НЕ HTTP-статус в диапазоне 200-299
+        inputCity.value = "Ошибка HTTP: " + response.status;
+    } else {
+        json = await response.json();
+        if (json.cod === "404") {
+            inputCity.value = "City not found ";
+        } else {
+            nowCity.innerHTML = json.name;
+            nowTemperature.innerHTML = Math.floor(Number(json.main.temp) - 273.15);
+        }
+    }
+}
+
+
+

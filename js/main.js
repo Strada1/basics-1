71c70b19470f7390c78b47061d5356eb9cdf3583
Evgeniy <106548359+evvgenchik@users.noreply.github.com>
2022-09-28 19:09:30 +0300

    add: delete and adding to list

--- js/main.js
+++ js/main.js
@@ -8,11 +8,16 @@ const img = document.querySelector('.icon__cloud');
 const cityPlace = document.querySelector('.now__city');
 const degreePlace = document.querySelector('.now__degrees');
 const form = document.querySelector('form')
+const locationsList = document.querySelector('.locations__list');
+const favorite = document.querySelector('.now__favorite');
+const cityList = [];
+let cityName
 
 form.addEventListener('submit', getCity)
 
-function getCity() {
-	let cityName = input.value ? input.value : alert('введите город');
+
+function getCity(name) {
+	cityName = input.value ? input.value : name;
 	fetch(`${SERVER_URL}?q=${cityName}&appid=${API_KEY}&units=metric`)
 		.then(response => response.json())
 		.then(json => {
@@ -30,10 +35,51 @@ function changeIcon(arr) {
 	let icon = (arr.map(element => element.icon)).join('');
 	img.src = `http://openweathermap.org/img/wn/${icon}@4x.png`
 }
-function changeCity(cityName) {
-	cityPlace.textContent = cityName
+function changeCity(cityNameForChange) {
+	cityPlace.textContent = cityNameForChange;
+	cityName = cityPlace.textContent
 }
 
 
+favorite.addEventListener('click', addCityToList)
 
+
+
+/*.......................................*/
+
+function addCityToList() {
+	cityList.push(cityName)
+	console.log(cityList);
+	updateList()
+}
+
+
+function updateList() {
+	while (locationsList.firstChild) {
+		locationsList.removeChild(locationsList.firstChild);
+	}
+	for (let nameCity of cityList) {
+		let newElementLi = document.createElement("li")
+		let newElementImg = document.createElement("img")
+		newElementLi.textContent = nameCity
+		locationsList.append(newElementLi)
+		newElementLi.classList.add('list__item')
+		newElementImg.src = '/img/close-icon.svg';
+		newElementLi.append(newElementImg)
+		newElementImg.addEventListener('click', removeCity)
+		newElementLi.addEventListener('click', showCityWeather)
+	}
+}
+
+function removeCity(e) {
+	let cityName = e.target.parentNode.textContent;
+	let indexOfCiteName = cityList.findIndex(item => item === cityName)
+	cityList.splice(indexOfCiteName, 1)
+	updateList()
+}
+
+function showCityWeather(e) {
+	let name = e.target.textContent;
+	getCity(name)
+}
 

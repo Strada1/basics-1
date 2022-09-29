f39ca49564216c1e7ada8423842dc8f8fa9c8960
Jarvis-DeNero <github.nikiforov94@gmail.com>
2022-09-29 04:00:38 +0300

    #59-60: Погода - 1.0 (первые две вкладки + локал сторедж)

--- 55-pogoda/js/script.js
+++ 55-pogoda/js/script.js
@@ -1,9 +1,18 @@
 import { popupMessage } from '../modules/showMessage/showMessage.js';
-import { render } from '../modules/render/render.js';
+import {
+   render,
+   renderCityList,
+   addCityListToStorege,
+   deleteCityFromStorege,
+   increaseCityPriority,
+   locationsList,
+} from '../modules/render/render.js';
 import { } from '../modules/tabs/tabs.js';
 
 const searchLocationForm = document.querySelector('.search__form');
 const searchInput = document.querySelector('.search__input');
+const saveCityBtn = document.querySelector('.save-city-btn');
+
 
 const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f';
@@ -18,13 +27,57 @@ const getDataWeather = (cityName) => {
          case 200:
             return result.json();
          case 401:
-            throw new Error('Bad token...');
+            throw new Error('Bad token... 401');
          case 404:
-            throw new Error('Not found...');
+            throw new Error('Not found... 404');
          default:
             throw new Error('Неизвестная ошибка, обратитесь к разработчику!')
       }
    })
+}
+
+const handlerAddInCityList = (e) => {
+   try {
+      const currentCity = document.querySelector('.tabs-block__selected-city').textContent;
+      addCityListToStorege(currentCity, localStorage.arrWeatherLocations);
+      renderCityList(localStorage.arrWeatherLocations)
+   } catch (error) {
+      popupMessage(error.message, error.name)
+   }
+}
+
+const handlerCityesList = (e) => {
+   if (e.target.classList.contains('item__delete-cyty-btn')) {
+      const deleteCity = e.target.dataset.deleteCity;
+      deleteCityFromStorege(localStorage.arrWeatherLocations, deleteCity);
+      renderCityList(localStorage.arrWeatherLocations);
+   } else if (e.target.classList.contains('item__show-cyty-btn')) {
+      try {
+         getDataWeather(e.target.textContent).then(result => {
+            const {
+               name: nameCity,
+               main: { feels_like, temp },
+               sys: { sunrise, sunset },
+               weather: [{
+                  main: weatherMain,
+                  icon,
+               }],
+            } = result;
+
+            render(nameCity, temp, feels_like, weatherMain, icon, sunrise, sunset);
+
+            const newArrCity = increaseCityPriority(localStorage.arrWeatherLocations, e.target.textContent);
+            localStorage.arrWeatherLocations = JSON.stringify(newArrCity);
+
+         }).catch(error => {
+            popupMessage(error.message, error.name)
+         })
+      } catch (error) {
+         popupMessage(error.message, error.name)
+      }
+   }
+
+   renderCityList(localStorage.arrWeatherLocations);
 }
 
 const handlerApp = (e) => {
@@ -56,4 +109,7 @@ const handlerApp = (e) => {
    }
 }
 
-searchLocationForm.addEventListener('submit', handlerApp)
+document.addEventListener('DOMContentLoaded', renderCityList(localStorage.arrWeatherLocations))
+saveCityBtn.addEventListener('click', handlerAddInCityList);
+locationsList.addEventListener('click', handlerCityesList)
+searchLocationForm.addEventListener('submit', handlerApp);
--- 55-pogoda/modules/render/render.js
+++ 55-pogoda/modules/render/render.js
@@ -1,4 +1,4 @@
-import { popupMessage } from '../showMessage/showMessage.js';
+import { createElement } from '../view/createDom.js';
 
 const allDomCityName = document.querySelectorAll('[data-current_city]');
 const allDomTemp = document.querySelectorAll('[data-current_temp]');
@@ -7,10 +7,74 @@ const feelsLikeElement = document.querySelector('[data-feels-like]');
 const weatherElement = document.querySelector('[data-weather]');
 const sunriseElement = document.querySelector('[data-sunrise]');
 const sunsetElement = document.querySelector('[data-sunset]');
+const locationsList = document.querySelector('.locations-list');
+
+const deleteCityFromStorege = (storegArr, deleteCity) => {
+   const cityArr = JSON.parse(storegArr);
+   const resultArr = cityArr.filter(item => item.name !== deleteCity)
+   localStorage.arrWeatherLocations = JSON.stringify(resultArr);
+}
+
+const increaseCityPriority = (storegArr, cityName) => {
+   const cityArr = JSON.parse(storegArr);
+   return cityArr.map(item => {
+      if (item.name === cityName) {
+         ++item.elementWeight;
+      }
+      return item;
+   })
+}
+
+const deleteElements = (container) => {
+   while (container.firstChild) {
+      container.removeChild(container.firstChild)
+   }
+}
+
+const isValideCity = (cityName, cityList) => {
+   const checkValid = cityList.find(item => item.name === cityName)
+   if (checkValid) {
+      throw new Error('Данный город уже в списке!')
+   }
+}
+
+const addCityListToStorege = (currentCity, cityArr) => {
+   if (cityArr && cityArr.length) {
+      cityArr = JSON.parse(cityArr);
+      isValideCity(currentCity, cityArr)
+   } else {
+      cityArr = []
+   }
+   cityArr.push({ name: currentCity, elementWeight: 1 })
+   localStorage.arrWeatherLocations = JSON.stringify(cityArr);
+}
+
+const addSityToDomList = (city, list) => {
+   const currentCity = city.name;
+   const deleteCytyBtn = createElement('button', 'item__delete-cyty-btn', null, 'data-delete-city', city.name);
+   const showCityBtn = createElement('button', 'item__show-cyty-btn', currentCity);
+   const element = createElement('div', ['locations-list__items', 'item']);
+
+   element.append(showCityBtn);
+   element.append(deleteCytyBtn);
+   list.append(element)
+}
+
+const renderCityList = (localStoregCity) => {
+   let cityArr = localStoregCity ? JSON.parse(localStoregCity) : [];
+
+   deleteElements(locationsList);
+
+   if (cityArr.length >= 1) {
+      cityArr = cityArr.sort((a, b) => b.elementWeight - a.elementWeight);
+      cityArr.forEach(city => {
+         addSityToDomList(city, locationsList);
+      })
+   }
+}
 
 const getTime = (timeMs) => {
    const timeFull = new Date(Number(timeMs) * 1000);
-   console.log(timeFull)
    const hours = String(timeFull.getHours());
    const minutes = (timeFull.getMinutes() >= 10) ? timeFull.getMinutes() : `0${timeFull.getMinutes()}`;
    return `${hours}:${minutes}`
@@ -27,9 +91,9 @@ const render = (city, temp, feelsLike, weather, icon, sunrise, sunset) => {
 
    feelsLikeElement.textContent = Math.round(feelsLike);
    weatherElement.textContent = weather;
-   weatherImg.src = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+   weatherImg.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
    sunriseElement.textContent = getTime(sunrise);
    sunsetElement.textContent = getTime(sunset);
 }
 
-export { render }
+export { render, renderCityList, addCityListToStorege, deleteCityFromStorege, locationsList, increaseCityPriority }

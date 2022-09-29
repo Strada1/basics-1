3c66e6258cf6c148bcb080b0c11715ed622e7af3
kamilsalikhov75 <kamilsalikhov75@gmail.com>
2022-09-29 14:43:54 +0300

    #60 Погода: Запомнить

---
+++ storage.js
@@ -0,0 +1,33 @@
+export function saveCityList(cityList) {
+  cityList.forEach((city, cityID) => {
+    localStorage.setItem(`city-${cityID}`, city.name);
+  })
+}
+
+export function getCityList() {
+  const list = []
+  for (let key in localStorage) {
+    if (!localStorage.hasOwnProperty(key) || key === 'currentCity') {
+      continue;
+    }
+    list.push({ 'name': localStorage.getItem(key) })
+  }
+  return list;
+}
+
+export function deleteCityFromStorage(cityName){
+  for (let key in localStorage) {
+    const city = localStorage.getItem(key);
+    if (city === cityName){
+      localStorage.removeItem(key);
+    }
+  }
+}
+
+export function addCurrentCity(cityName){
+  localStorage.setItem(`currentCity`, cityName );
+}
+
+export function getCurrentCity(){
+  return localStorage.getItem('currentCity');
+}

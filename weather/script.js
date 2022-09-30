17e7b7181cc548e5a8208478dc2e17a4b6778daf
nya2000 <tighineanu00@mail.ru>
2022-09-30 20:59:18 +0300

    Погода, добавил детали!

--- weather/script.js
+++ weather/script.js
@@ -7,8 +7,12 @@ const NEW_ELEMENTS = {
     weatherTemp:document.querySelector('.degrees'),
     tempCity:document.createElement('p'),
 }
-const CITY_NAME = {}
-const FAVORITE_CITY = []
+const DETAILS = {
+    textHeader:document.querySelector('.locations-header'),
+    detailList:document.querySelector('.details-items'),
+}
+const CITY_NAME = localGet2()
+const FAVORITE_CITY = localGet();
 
 cityInput.addEventListener('submit' , (event) => {
     event.preventDefault();
@@ -16,29 +20,83 @@ cityInput.addEventListener('submit' , (event) => {
     const cityName = nameInp.value;
     const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
     const url = `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
-
     fetch(url)
     .then(response => response.json())
     .then(response => {
-        console.log(response)
         if(response.cod !== 200) {
             alert(response.cod + ' ' + response.message)
         } else {
-            showCurrentCity(response.name , response.main.temp)
+            showCurrentCity(response.name , response.main.temp , response.main.feels_like, response.weather[0].main, response.sys.sunset, response.sys.sunrise)
+            renderDetails()
         }
     })
     .catch(err => alert(err.message))
 })
-// Отображение городда в NOW
-function showCurrentCity(cityName, cityTemp) {
-    CITY_NAME.name = cityName;
-    CITY_NAME.temp = cityTemp;
+// localStorage Added Locations
+function localSet() {
+    localStorage.setItem('cityNow', JSON.stringify(CITY_NAME))
+    localStorage.setItem('favoriteCity' , JSON.stringify(FAVORITE_CITY))
+}
+
+function localGet() {
+    if(localStorage.getItem('favoriteCity') !== null) {
+        return JSON.parse(localStorage.getItem('favoriteCity'))
+    }  else {
+        return []
+    }
+}
+function localGet2() {
+    if(localStorage.getItem('cityNow') !== null) {
+        return JSON.parse(localStorage.getItem('cityNow'))
+    }  else {
+        return []
+    }
+}
+// Вкладка Details
+function renderDetails() {
+    // render city name
+    const newCity = document.createElement('p')
+    newCity.className = 'text-locations'
+    DETAILS.textHeader.textContent = ''
+    newCity.textContent = CITY_NAME.name
+    DETAILS.textHeader.prepend(newCity)
+    //Render city details
+    const normalDataSunSet = new Date(CITY_NAME.sunset * 1000)
+    const normalDataSunRise = new Date(CITY_NAME.sunrise * 1000)
+    const detailsTemp = document.createElement('li')
+    const detailsFeelsLike = document.createElement('li')
+    const detailsWeather = document.createElement('li')
+    const detailsSunrise = document.createElement('li')
+    const detailsSunset = document.createElement('li')
+    DETAILS.detailList.textContent = ''
+    detailsTemp.textContent = `Temperature: ${Math.round(CITY_NAME.temp)}°`
+    DETAILS.detailList.append(detailsTemp)
+    detailsFeelsLike.textContent = `Feels like: ${Math.round(CITY_NAME.feelslike)}°`
+    DETAILS.detailList.append(detailsFeelsLike)
+    detailsWeather.textContent = `Weather: ${CITY_NAME.weather}`
+    DETAILS.detailList.append(detailsWeather)
+    detailsSunrise.textContent = `Sunrise: ${normalDataSunRise.toLocaleTimeString()}`
+    DETAILS.detailList.append(detailsSunrise)
+    detailsSunset.textContent = `Sunset: ${normalDataSunSet.toLocaleTimeString()}`
+    DETAILS.detailList.append(detailsSunset)
+}
+// Отображение города в NOW
+function showCurrentCity(cityName, cityTemp, cityFeel, cityWeather, citySunSet, citySunRise) {
+    CITY_NAME.name =cityName
+    CITY_NAME.temp = cityTemp
+    CITY_NAME.feelslike = cityFeel;
+    CITY_NAME.weather = cityWeather;
+    CITY_NAME.sunset = citySunSet;
+    CITY_NAME.sunrise = citySunRise;
     NEW_ELEMENTS.nameCity.textContent = cityName
     NEW_ELEMENTS.weatherCity.prepend(NEW_ELEMENTS.nameCity)
     NEW_ELEMENTS.tempCity.textContent = Math.round(cityTemp)
     NEW_ELEMENTS.tempCity.className = 'degrees-num'
     NEW_ELEMENTS.weatherTemp.prepend(NEW_ELEMENTS.tempCity)
+    nameInp.value = ''
+    localSet()
 }
+nameInp.textContent = ''
 // Добавление города в избранные (массив)
 addFavorite.addEventListener('click' , addFavoriteInArray)
 
@@ -47,15 +105,17 @@ function addFavoriteInArray() {
     if(FAVORITE_CITY.includes(favoriteCity)) {
         alert('Такой город уже в избранных!')
     } else {
-        FAVORITE_CITY.push({name: CITY_NAME.name,temp: CITY_NAME.temp,})
-        console.log(FAVORITE_CITY)
+        FAVORITE_CITY.push({name: CITY_NAME.name,temp: CITY_NAME.temp, feelslike: CITY_NAME.feelslike, weather: CITY_NAME.weather,sunset: CITY_NAME.sunset, sunrise:CITY_NAME.sunrise,})
+        localSet()
         render()
+        
     }
 }
 // Удаление города из массива 
 function deleteCityArray(cityName) {
     let CityFavorite = FAVORITE_CITY.findIndex(item => item.name == cityName)
     FAVORITE_CITY.splice(CityFavorite , 1)
+    localSet()
     render()
 }
 // Добавление городов из массива в вёрстку
@@ -73,8 +133,11 @@ function render() {
             deleteCityArray(newFavoriteCity.textContent)
         })
         newFavoriteCity.addEventListener('click', () => {
-            showCurrentCity(item.name, item.temp)
+            showCurrentCity(item.name, item.temp, item.feelslike, item.weather, item.sunset, item.sunrise)
+            renderDetails()
         })
     })
 }
-
+window.onload = render()
+window.onload = renderDetails()
+window.onload = showCurrentCity(CITY_NAME.name, CITY_NAME.temp, CITY_NAME.feelslike, CITY_NAME.weather,CITY_NAME.sunset , CITY_NAME.sunrise)

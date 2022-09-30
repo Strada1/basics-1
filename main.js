8d74be3f3adef3a8779517a4c9a66fa9d29bf068
Stanovov Mikhail <98664376+CTEPXMGN@users.noreply.github.com>
2022-09-30 22:07:54 +0500

    123

---
+++ main.js
@@ -0,0 +1,173 @@
+import { UI_ELEMENTS } from "./view.js";
+
+let favoritesCities = [];
+
+// Переключение табов
+document.querySelectorAll('.tab').forEach((item) => 
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        const id = e.target.getAttribute('href').replace('#', '');
+
+        document.querySelectorAll('.tab').forEach(
+            (child) => child.classList.remove('active-tab')
+        );
+        document.querySelectorAll('.tab__item').forEach(
+            (child) => child.classList.remove('active')
+        )
+        
+        item.classList.add('active-tab');
+        document.getElementById(id).classList.add('active');
+    })
+);
+
+document.querySelector('.tab').click();
+
+// Получение погоды
+UI_ELEMENTS.FIND_FORM.addEventListener('submit', getWeather);
+UI_ELEMENTS.FIND_CITY.addEventListener('click', getWeather);
+
+async function getWeather(event) {
+    event.preventDefault();
+
+    const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather';
+    const API_KEY = '0a8c506a0f09e19f0f5a48594460c570';
+    const URL = `${SERVER_URL}?q=${UI_ELEMENTS.FIND_INPUT.value}&appid=${API_KEY}&units=metric&lang=ru`;
+    
+    try {
+        let response = await fetch(URL);
+
+        if (response.ok) {
+            let dataWeather = await response.json();
+    
+            const SRC_IMG = `
+            https://openweathermap.org/img/wn/${dataWeather.weather[0].icon}@4x.png
+            `;
+
+            renderNow(Math.round(dataWeather.main.temp) ,dataWeather.name, SRC_IMG);
+            renderDetails(dataWeather);
+            renderForecast(dataWeather);
+            localStorage.setItem('currentCity', dataWeather.name);
+        } else {
+            alert('Ошибочка вышла: ' + response.status);
+        }
+    } catch (error) {
+        alert(error.stack);
+    }
+    
+    UI_ELEMENTS.FIND_INPUT.value = '';
+}
+// Отрисовка вкладки NOW
+function renderNow(temp, city, icon) {
+
+    while(UI_ELEMENTS.TAB_NOW.firstChild){
+        UI_ELEMENTS.TAB_NOW.removeChild(UI_ELEMENTS.TAB_NOW.firstChild);
+    };
+
+    const p1 = document.createElement('p');
+    p1.classList.add('tab-now__temperature');
+    p1.textContent = temp + '°';
+    UI_ELEMENTS.TAB_NOW.appendChild(p1);
+
+    const p2 = document.createElement('p');
+    p2.classList.add('tab-now__city');
+    p2.textContent = city;
+    UI_ELEMENTS.TAB_NOW.appendChild(p2);
+
+    const input = document.createElement('input');
+    input.classList.add('tab-now__add');
+    input.type = 'button';
+    input.addEventListener('click', () => addToFavorites(city));
+    UI_ELEMENTS.TAB_NOW.appendChild(input);
+
+    const img = document.createElement('img');
+    img.classList.add('tab-now__img');
+    img.src = icon;
+    img.alt = 'weather icon';
+
+    UI_ELEMENTS.TAB_NOW.appendChild(img);
+};
+
+// Добавление в избранное
+function addToFavorites(city) {
+
+    favoritesCities = favoritesCities.concat([city]);
+    localStorage.setItem('cities', JSON.stringify(favoritesCities));
+    favoritesCities = JSON.parse(localStorage.getItem('cities'));
+    console.log(favoritesCities);
+
+    renderFavorites(favoritesCities);
+};
+
+// Отрисовка избранного
+function renderFavorites(cities) {
+
+    while(UI_ELEMENTS.ADDED_CITIES_LIST.firstChild){
+        UI_ELEMENTS.ADDED_CITIES_LIST.removeChild(UI_ELEMENTS.ADDED_CITIES_LIST.firstChild);
+    };
+
+    for (const elem of cities) {
+        const li = document.createElement('li');
+        li.classList.add('added-cities__item');
+        li.textContent = elem + '   ';
+        li.addEventListener('click', function() {
+            UI_ELEMENTS.FIND_INPUT.value = elem;
+            getWeather(event);
+        })
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-city');
+        deleteButton.addEventListener('click', function(e) {
+            e.stopPropagation();
+
+            cities = JSON.parse(localStorage.getItem('cities'));
+            favoritesCities = cities.filter(item => item !== elem);
+            localStorage.cities = JSON.stringify(favoritesCities);
+            console.log(favoritesCities);
+            renderFavorites(favoritesCities);
+        });
+        li.appendChild(deleteButton);
+        UI_ELEMENTS.ADDED_CITIES_LIST.appendChild(li);
+    };
+};
+
+// Отрисовка вкладки DETAILS
+function renderDetails(data) {
+
+    while(UI_ELEMENTS.TAB_DETAILS.firstChild){
+        UI_ELEMENTS.TAB_DETAILS.removeChild(UI_ELEMENTS.TAB_DETAILS.firstChild);
+    };
+
+    const dateInMsSunrise = data.sys.sunrise * 1000;
+    const hoursSunrise = new Date(dateInMsSunrise).getHours();
+    const minutesSunrise = new Date(dateInMsSunrise).getMinutes();
+    const dateInMsSunset = data.sys.sunset * 1000;
+    const hoursSunset = new Date(dateInMsSunset).getHours();
+    const minutesSunset = new Date(dateInMsSunset).getMinutes();
+
+    const arrData = [
+        `Температура: ${Math.round(data.main.temp)}°`,
+        `По ощущениям: ${Math.round(data.main.feels_like)}°`,
+        `Погода: ${data.weather[0].description}`,
+        `Восход: ${hoursSunrise}:${minutesSunrise}`,
+        `Закат: ${hoursSunset}:${minutesSunset}`,
+    ];
+
+    const p = document.createElement('p');
+    p.classList.add('tab-details__city');
+    p.textContent = data.name;
+    UI_ELEMENTS.TAB_DETAILS.appendChild(p);
+
+    const ul = document.createElement('ul');
+    ul.classList.add('tab-details__list');
+    
+    for (const item of arrData) {
+        const li = document.createElement('li');
+        li.classList.add('tab-details__item');
+        li.textContent = item;
+        ul.appendChild(li);
+    }
+
+    UI_ELEMENTS.TAB_DETAILS.appendChild(ul);
+};
+
+function renderForecast(data) {
+};
---
+++ view.js
@@ -0,0 +1,10 @@
+export const UI_ELEMENTS = {
+    TABS: document.querySelectorAll('.tab'),
+    TAB_ITEMS: document.querySelectorAll('.tab-item'),
+    FIND_FORM: document.querySelector('.find-form'),
+    FIND_CITY: document.querySelector('.find__button'),
+    FIND_INPUT: document.querySelector('.find__input'),
+    TAB_NOW: document.querySelector('.tab-now'),
+    TAB_DETAILS: document.querySelector('.tab-details'),
+    ADDED_CITIES_LIST: document.querySelector('.added-cities__list'),
+};

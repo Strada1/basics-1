2d82e2d0d31fd66c91d31f828140e5b259ddadab
Dariannyko <dariannyko@gmail.com>
2022-10-01 20:17:49 +0300

    fix: localStorage and details

--- main.js
+++ main.js
@@ -1,185 +1,224 @@
 const ELEMENTS = {
-    citySearch : document.querySelector('.forecast-form'),
-    cityInput : document.querySelector('.city-input'),
-    forecastDegrees : document.querySelector('.degrees-num'),
-    forecastIcon : document.querySelector('.cloud'),
-    forecastCity : document.querySelector('.city-name'),
-    buttonFavorite : document.querySelector('.like'),
-    iconFavorite: document.querySelector('.like svg'),
+  citySearch: document.querySelector(".forecast-form"),
+  cityInput: document.querySelector(".city-input"),
+  forecastDegrees: document.querySelector(".degrees-num"),
+  forecastIcon: document.querySelector(".cloud"),
+  forecastCity: document.querySelector(".city-name"),
+  buttonFavorite: document.querySelector(".like"),
+  iconFavorite: document.querySelector(".like svg"),
 
-
-    detailsTemperature : document.querySelector('.details-items li:nth-child(1) span'),
-    detailsFeelsLike : document.querySelector('.details-items li:nth-child(2) span'),
-    detailsWeather : document.querySelector('.details-items li:nth-child(3) span'),
-    detailsSunrise : document.querySelector('.details-items li:nth-child(4) span'),
-    detailsSunset : document.querySelector('.details-items li:nth-child(5) span'),
-    detailsCity : document.querySelector('.forecast-details p'),
-}
+  detailsTemperature: document.querySelector(
+    ".details-items li:nth-child(1) span"
+  ),
+  detailsFeelsLike: document.querySelector(
+    ".details-items li:nth-child(2) span"
+  ),
+  detailsWeather: document.querySelector(".details-items li:nth-child(3) span"),
+  detailsSunrise: document.querySelector(".details-items li:nth-child(4) span"),
+  detailsSunset: document.querySelector(".details-items li:nth-child(5) span"),
+  detailsCity: document.querySelector(".forecast-details p"),
+};
 
 console.log(ELEMENTS.detailsTemperature);
 
 const SERVER = {
-    serverUrl : 'http://api.openweathermap.org/data/2.5/weather',
-    apiKey : '6ca767a0f89bdb44703b66b9c5240f30',
+  serverUrl: "http://api.openweathermap.org/data/2.5/weather",
+  apiKey: "6ca767a0f89bdb44703b66b9c5240f30",
+};
 
+let list = ["Amur", "Samara", "Bali", "Dane", "Kilo", "Nur-Sultan"];
+const storageList = localStorage.getItem("newList");
+const storageCity = localStorage.getItem("cityName");
+let cityName;
+
+if (storageList) {
+  list = JSON.parse(storageList);
 }
 
-let list = ['Amur', 'Samara', 'Bali', 'Dane', 'Kilo', 'Nur-Sultan'];
-
-
-list = JSON.parse(localStorage.getItem('newList'))
-    console.log(list);
-
-let cityName = localStorage.getItem('cityName');
-console.log(cityName)
-cityNow(cityName);
+if (storageCity) {
+  cityName = storageCity;
+  console.log(cityName);
+  cityNow(cityName);
+}
+console.log(list);
 
 //Add city in Now
 
 function cityNow(cityName) {
-
-    if(!cityName) {
-        cityName = ELEMENTS.cityInput.value;
-    }
-    if(list.includes(cityName)) {
-    document.querySelector('.like svg').classList.add('heart');
+  if (!cityName) {
+    cityName = ELEMENTS.cityInput.value;
+  }
+  if (list.includes(cityName)) {
+    document.querySelector(".like svg").classList.add("heart");
     console.log(ELEMENTS.iconFavorite);
-    } else {
-        document.querySelector('.like svg').classList.remove('heart');
-    }
+  } else {
+    document.querySelector(".like svg").classList.remove("heart");
+  }
+  setLocal(cityName);
 
-    localStorage.setItem('cityName', cityName);
-    console.log(localStorage['cityName']);
+  const url = `${SERVER.serverUrl}?q=${cityName}&appid=${SERVER.apiKey}&units=metric`;
 
-const url = `${SERVER.serverUrl}?q=${cityName}&appid=${SERVER.apiKey}&units=metric`;
-
-
-fetch(url) 
-.then(response => {
-    if(response.ok) {
-       return response.json();
-    } else {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
         alert("Ошибка HTTP: " + response.status);
-    }
+      }
     })
-.then(forecast => {
-    let degrees = Math.round(forecast.main.temp);
-    let degreesFeelsLike = Math.round(forecast.main.feels_like);
-    let icon = forecast.weather[0].icon;
-    ELEMENTS.forecastDegrees.textContent = degrees;
-    ELEMENTS.forecastCity.textContent = forecast.name;
+    .then((forecast) => {
+      const FORECAST = {
+        degrees: Math.round(forecast.main.temp),
+        degreesFeelsLike: Math.round(forecast.main.feels_like),
+        icon: forecast.weather[0].icon,
+        forecastCity: forecast.name,
 
-    ELEMENTS.forecastIcon.removeAttribute('src');
-    ELEMENTS.forecastIcon.removeAttribute('width');
-    ELEMENTS.forecastIcon.removeAttribute('height');
+        currentTimeZone: forecast.timezone,
+        sunrise: forecast.sys.sunrise,
+        sunset: forecast.sys.sunset,
 
-    ELEMENTS.forecastIcon.setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
-    
-    
-    function cityTime(dayTime) {
-        let localDate = dayTime*1000 + (new Date(dayTime*1000).getTimezoneOffset())*60000 + forecast.timezone*1000;
-        const date = new Date(localDate).toLocaleTimeString()
-        let localTime =  date.replace(/:\d\d$/, '');
-        return localTime;
-    }
+        detailsWeather: forecast.weather[0].main,
+      };
 
-    let sunrise = forecast.sys.sunrise;
-    let sunset = forecast.sys.sunset;
+      const sunriseTime = cityTime(FORECAST.sunrise, FORECAST.currentTimeZone);
+      const sunsetTime = cityTime(FORECAST.sunset, FORECAST.currentTimeZone);
 
-    let sunriseTime = cityTime(sunrise);
-    let sunsetTime =  cityTime(sunset);
+      setNowHTML(FORECAST.degrees, FORECAST.icon, FORECAST.forecastCity);
+      setDetailHTML(
+        sunriseTime,
+        sunsetTime,
+        FORECAST.degrees,
+        FORECAST.degreesFeelsLike,
+        FORECAST.detailsWeather,
+        FORECAST.forecastCity
+      );
+    })
 
-    ELEMENTS.detailsSunrise.textContent = sunriseTime;
-    ELEMENTS.detailsSunset.textContent = sunsetTime;
-
-    ELEMENTS.detailsTemperature.textContent = degrees;
-    ELEMENTS.detailsFeelsLike.textContent = degreesFeelsLike;
-    ELEMENTS.detailsWeather.textContent = forecast.weather[0].main;
-
-    ELEMENTS.detailsCity.textContent = forecast.name;
-
-})
-.catch(err => alert(err));
-ELEMENTS.cityInput.value = '';
+    .catch((err) => alert(err));
+  ELEMENTS.cityInput.value = "";
 }
 
-
-ELEMENTS.citySearch.addEventListener('submit', (event) => {
-    event.preventDefault();
-    cityNow();
+ELEMENTS.citySearch.addEventListener("submit", (event) => {
+  event.preventDefault();
+  cityNow();
 });
-
 
 // Add favorite locations
 
 function addFavoriteLocation() {
-    if (list.includes(ELEMENTS.forecastCity.textContent)) {
-        let newList = list.filter(item => item !== ELEMENTS.forecastCity.textContent); 
-        list = newList;
-        document.querySelector('.like svg').classList.remove('heart');
-        
-    } else {
-        list.push(ELEMENTS.forecastCity.textContent);
-        document.querySelector('.like svg').classList.add('heart');
-
-    }
-
-
-    localStorage.setItem('newList', JSON.stringify(list));
-    localStorage.setItem('cityName', cityName);
-    console.log(localStorage['cityName']);
-    render();
-
-};
+  if (list.includes(ELEMENTS.forecastCity.textContent)) {
+    let newList = list.filter(
+      (item) => item !== ELEMENTS.forecastCity.textContent
+    );
+    list = newList;
+    document.querySelector(".like svg").classList.remove("heart");
+  } else {
+    list.push(ELEMENTS.forecastCity.textContent);
+    document.querySelector(".like svg").classList.add("heart");
+  }
+  setLocal(list);
+  setLocal(cityName);
+  render();
+}
 
 // delete favorite location
 
 function deleteFavoriteLocation(city) {
-    if (list.includes(city)) {
-        let newList = list.filter(item => item !== city);
-        list = newList;
-        console.log(list)
-      
-    }
-    localStorage.setItem('newList', JSON.stringify(list));
-    render();
-};
+  if (list.includes(city)) {
+    let newList = list.filter((item) => item !== city);
+    list = newList;
+    console.log(list);
+  }
+  setLocal(list);
+  render();
+}
 
 // Render favorite cities
 
-function render(){
-    let delCity = document.querySelectorAll('.locations-items li');
-    delCity.forEach((item) => item.remove());
+function render() {
+  let delCity = document.querySelectorAll(".locations-items li");
+  delCity.forEach((item) => item.remove());
 
-    for (let item of list) {
-        addFavoriteHTML(item);
-    }
+  for (let item of list) {
+    addFavoriteHTML(item);
+  }
 }
-render()
+render();
 
+function addFavoriteHTML(cityName) {
+  const ul = document.querySelector(".locations-items");
+  let li = document.createElement("li");
+  ul.prepend(li);
+  li.addEventListener("click", (event) => {
+    event.preventDefault();
+    cityNow(cityName);
+  });
 
+  let p = document.createElement("p");
+  li.prepend(p);
+  p.textContent = cityName;
 
-function addFavoriteHTML (cityName) {
-    const ul = document.querySelector('.locations-items');
-    let li = document.createElement('li');
-    ul.prepend(li);
-    li.addEventListener('click', (event) => {
-        event.preventDefault();
-        cityNow(cityName);
-    });
-
-    let p = document.createElement('p');
-    li.prepend(p);
-    p.textContent = cityName;
-
-    let button = document.createElement('button');
-    button.classList.add('button-exit');
-    li.append(button);
-    button.addEventListener('click', () => {
-        deleteFavoriteLocation(cityName);
-    });
+  let button = document.createElement("button");
+  button.classList.add("button-exit");
+  li.append(button);
+  button.addEventListener("click", () => {
+    deleteFavoriteLocation(cityName);
+  });
 }
 
-ELEMENTS.buttonFavorite.addEventListener('click', addFavoriteLocation);
+ELEMENTS.buttonFavorite.addEventListener("click", addFavoriteLocation);
 
+function setLocal(item) {
+  if (Array.isArray(item)) {
+    localStorage.setItem("newList", JSON.stringify(item));
+  } else {
+    localStorage.setItem("cityName", item);
+  }
+}
 
+// add sunset and sunrise time
+function cityTime(dayTime, currentTimeZone) {
+  let localDate =
+    dayTime * 1000 +
+    new Date(dayTime * 1000).getTimezoneOffset() * 60000 +
+    currentTimeZone * 1000;
+  const date = new Date(localDate).toLocaleTimeString();
+  let localTime = date.replace(/:\d\d$/, "");
+  return localTime;
+}
+
+// add forecast in Now
+
+function setNowHTML(degrees, icon, forecastCity) {
+  ELEMENTS.forecastDegrees.textContent = degrees;
+  ELEMENTS.forecastCity.textContent = forecastCity;
+
+  ELEMENTS.forecastIcon.removeAttribute("src");
+  ELEMENTS.forecastIcon.removeAttribute("width");
+  ELEMENTS.forecastIcon.removeAttribute("height");
+
+  ELEMENTS.forecastIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${icon}@2x.png`
+  );
+}
+
+// add forecast in Details
+
+function setDetailHTML(
+  sunriseTime,
+  sunsetTime,
+  degrees,
+  degreesFeelsLike,
+  detailsWeather,
+  forecastCity
+) {
+  ELEMENTS.detailsSunrise.textContent = sunriseTime;
+  ELEMENTS.detailsSunset.textContent = sunsetTime;
+
+  ELEMENTS.detailsTemperature.textContent = degrees;
+  ELEMENTS.detailsFeelsLike.textContent = degreesFeelsLike;
+  ELEMENTS.detailsWeather.textContent = detailsWeather;
+
+  ELEMENTS.detailsCity.textContent = forecastCity;
+}

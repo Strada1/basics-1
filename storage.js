0a94ead7e774f6ad7695731fbf379cce8611d9ac
pipupopi <sonalavrushin@gmail.com>
2022-10-13 19:25:29 +0300

    не рабает блинб

---
+++ storage.js
@@ -0,0 +1,27 @@
+
+let city = new Set();
+let CITYNAME = {}
+
+// function localGet() {
+//     if (localStorage.getItem("likedCity") !== null) {
+//       return JSON.parse(localStorage.getItem("likedCity"));
+//     } else {
+//       return [];
+//     }
+//   }
+  
+  // function localSet() {
+  //   localStorage.setItem("likedCity", JSON.stringify(city));
+  //   localStorage.setItem("CityNow", JSON.stringify(CITYNAME));
+  // }
+  
+  // function localGetNow() {
+  //   if (localStorage.getItem("CityNow") !== null) {
+  //     return JSON.parse(localStorage.getItem("CityNow"));
+  //   } else {
+  //     return {};
+  //   }
+  // }
+
+
+  export {city, CITYNAME}

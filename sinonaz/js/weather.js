3392860c00142b40b7e5c9de14ab04cc61a90c86
Isaac <aziksimpson@gmail.com>
2022-10-15 13:46:13 +0300

    add and delete scripts

--- sinonaz/js/weather.js
+++ sinonaz/js/weather.js
@@ -11,46 +11,112 @@ const weahter = ({
 }) => {
   const form = document.querySelector(formSelector);
   const input = form.querySelector(inputElement);
-  const serverUrl = `http://api.openweathermap.org/data/2.5/weather`;
-  const apiKey = `f660a2fb1e4bad108d6160b7f58c555f`;
-  const iconUrl = `https://openweathermap.org/img/wn/`;
-  const cityNameElements = form.querySelectorAll(`.js-city-name`);
-  const tempElements = form.querySelectorAll(temperature);
-  const icon = form.querySelector(`.js-icon`);
-  const feelsLikeElement = form.querySelector(`.js-feel`);
-  const conditionElement = form.querySelector(conditions);
-  const sunset = form.querySelector(`.js-sunset`);
-  const sunrise = form.querySelector(`.js-sunrise`);
+  const addBtn = form.querySelector(`.js-add`);
+  const cityArr = [];
 
-  async function onFormSubmit(evt) {
-    evt.preventDefault();
-    const url = `${serverUrl}?q=${input.value}&appid=${apiKey}`;
-
+  async function onFormSubmit(cityName) {
+    const serverUrl = "//api.openweathermap.org/data/2.5/weather";
+    const apiKey = "f660a2fb1e4bad108d6160b7f58c555f";
+    const url = `${serverUrl}?q=${cityName}&appid=${apiKey}`;
     fetch(url)
       .then((response) => response.json())
-      .then((json) => {
-        if (!input.value) {
-          throw new Error(`City field is empty`);
-        }
-        let dateSunset = new Date(json.sys.sunset * 1000);
-        let dateSunrise = new Date(json.sys.sunrise * 1000);
-        cityNameElements.forEach((cityName) => {
-          cityName.textContent = json.name;
-        });
-        tempElements.forEach((temp) => {
-          temp.innerHTML =
-            Math.round(json.main.temp - 273) + `<span>&#176</span>`;
-        });
-        icon.src = `${iconUrl}${json.weather[0].icon}@2x.png`;
-        feelsLikeElement.innerHTML =
-          Math.round(json.main.feels_like - 273) + `<span>&#176</span>`;
-        conditionElement.textContent = json.weather[0].main;
-        sunset.innerHTML = `${dateSunset.getMinutes()}:${dateSunset.getSeconds()}`;
-        sunrise.innerHTML = `${dateSunrise.getMinutes()}:${dateSunrise.getSeconds()}`;
-        console.log(json);
+      .then((data) => {
+        if (data.cod >= 300) throw new Error(data.message);
+        else render(data);
       })
-      .catch((error) => alert(error.message));
+      .catch((error) => console.log(error));
   }
-  form.addEventListener(`submit`, onFormSubmit);
+
+  function toHumanDate(timestamp) {
+    let hours = new Date(timestamp * 1000).getHours();
+    let minutes = new Date(timestamp * 1000).getMinutes();
+    return hours + ":" + minutes;
+  }
+
+  function render(data) {
+    let cityNameElements = form.querySelectorAll(`.js-city-name`);
+    const sunsetElement = form.querySelector(`.js-sunset`);
+    const sunriseElement = form.querySelector(`.js-sunrise`);
+    const conditionElement = form.querySelector(conditions);
+    const tempElements = form.querySelectorAll(temperature);
+    const feelsLikeElement = form.querySelector(`.js-feel`);
+    const iconUrl = `https://openweathermap.org/img/wn/`;
+    const icon = form.querySelector(`.js-icon`);
+    let sunrise = toHumanDate(data.sys.sunrise);
+    let sunset = toHumanDate(data.sys.sunset);
+
+    cityNameElements.forEach((cityName) => {
+      cityName.textContent = data.name;
+    });
+    icon.src = `${iconUrl}${data.weather[0].icon}@2x.png`;
+    tempElements.forEach((temp) => {
+      temp.innerHTML = Math.round(data.main.temp - 273) + `<span>&#176</span>`;
+    });
+    feelsLikeElement.innerHTML =
+      Math.round(data.main.feels_like - 273) + `<span>&#176</span>`;
+    conditionElement.textContent = data.weather[0].main;
+    sunriseElement.innerHTML = `${sunrise}`;
+    sunsetElement.innerHTML = `${sunset}`;
+    input.value = ``;
+  }
+
+  function addFavourite() {
+    let cityNameElements = form.querySelectorAll(`.js-city-name`);
+    let city = cityNameElements[0].textContent;
+    if (city !== "---") {
+      let position = cityArr.findIndex((anyCity) => anyCity == city);
+      if (position >= 0) {
+        cityArr.splice(position, 1);
+      } else {
+        cityArr.push(city);
+      }
+    }
+    renderFavorites();
+    console.log(cityArr);
+  }
+
+  function renderFavorites() {
+    let cityNameElements = form.querySelectorAll(`.js-city-name`);
+    const cityList = form.querySelector(`.js-list`);
+    let firstCityName = cityNameElements[0].textContent;
+    const newItem = document.createElement(`li`);
+    newItem.className = `weather__item`;
+    newItem.textContent = firstCityName;
+    const newBtn = document.createElement(`button`);
+    newBtn.type = `button`;
+    newBtn.className = `btn`;
+    newBtn.innerHTML = `<img src="img/close-icon.svg" style="pointer-events:none;touch-action:none;">`;
+    newItem.append(newBtn);
+    const deleteBtn = newItem.querySelector(`button`);
+    cityList.append(newItem);
+
+    deleteBtn.addEventListener(`click`, deleteFavourites);
+    newItem.addEventListener("click", renderData);
+  }
+
+  function renderData(evt) {
+    if (evt.target.className != `btn`) {
+      let name = evt.target.textContent;
+      onFormSubmit(name);
+    }
+  }
+
+  function deleteFavourites(evt) {
+    let item = evt.target.parentNode;
+    let city = item.textContent;
+    let index = cityArr.findIndex((anyCity) => anyCity == city);
+    cityArr.splice(index, 1);
+    item.remove();
+    console.log(cityArr);
+  }
+
+  function getWeather(evt) {
+    evt.preventDefault();
+    const cityName = input.value;
+    onFormSubmit(cityName);
+  }
+
+  addBtn.addEventListener(`click`, addFavourite);
+  form.addEventListener(`submit`, getWeather);
 };
 export default weahter;

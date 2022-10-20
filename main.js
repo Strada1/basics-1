7f631991d516ca6a426d76e113c4afc5eecca17d
Alina239 <alinochka239@gmail.com>
2022-10-20 01:49:10 -0400

    #61

--- main.js
+++ main.js
@@ -3,19 +3,31 @@ const apiKey = 'f660a2fb1e4bad108d6160b7f58c555f';
 const iconsUrl = 'https://openweathermap.org/img/wn/';
 
 
-
-
 async function OnButtonPress() {
     try {
         const inputCity = document.getElementById('inputName');
         const url = `${serverUrl}?q=${inputCity.value}&appid=${apiKey}`;
-        //todo
         let response = await fetch(url);
         let json = await response.json();
-        document.getElementById('now_city').innerHTML = `${json.name}`;
-        document.getElementById('now_temp').innerHTML = calcCelsiusTemp(json.main.temp) + '&#176';
+        console.log(json);
+        document.getElementById('now_city').innerHTML
+            = `${json.name}`;
+        document.getElementById('now_temp').innerHTML
+            = calcCelsiusTemp(json.main.temp) + '&deg';
         let icon = json.weather[0].icon;
-        document.getElementById('weatherPic').innerHTML = `<img src=\"${iconsUrl}${icon}@2x.png\" alt="weatherPic">`;
+        document.getElementById('weatherPic').innerHTML =
+            `<img src=\"${iconsUrl}${icon}@2x.png\" alt="weatherPic">`;
+        document.getElementById('details__city').innerHTML = `${json.name}`;
+        document.getElementById('details__temp').innerHTML =
+            `Temperature: ${calcCelsiusTemp(json.main.temp)}&deg`;
+        document.getElementById('details__feels_like').innerHTML =
+            `Feels like: ${calcCelsiusTemp(json.main.feels_like)}&deg`;
+        document.getElementById('details__weather').innerHTML =
+            `Weather: ${json.weather[0].main}`;
+        document.getElementById('details__sunrise').innerHTML =
+            `Sunrise: ${calcTime(json.sys.sunrise)}`;
+        document.getElementById('details__sunset').innerHTML =
+            `Sunset: ${calcTime(json.sys.sunset)}`;
     } catch (error) {
         alert(error.message);
     }
@@ -24,16 +36,16 @@ async function OnButtonPress() {
 let button = document.getElementById('searchButton');
 button.onclick = OnButtonPress;
 
-const CITYLIST = new Map();
+const setOfCity = new Set();
 updateListOfCities();
 
-function updateListOfCities (){
-    for (let key in localStorage){
-        if (key.startsWith('keyCity$')){
-            CITYLIST.set(key, '1');
+function updateListOfCities() {
+    for (let key in localStorage) {
+        if (key.startsWith('keyCity')) {
+
+            setOfCity.add(localStorage.getItem(key))
         }
     }
-
 }
 
 
@@ -42,19 +54,20 @@ showSavingCities();
 function deleteCityFromList(city) {
     let cityKey;
     let localStorageKeys = Object.keys(localStorage);
-    for (let key of localStorageKeys){
-        if (city === localStorage.getItem(key)){
+    for (let key of localStorageKeys) {
+        if (city === localStorage.getItem(key)) {
             cityKey = key;
         }
     }
-    CITYLIST.delete(city);
+    setOfCity.delete(city);
+    console.log("cityKey = " + cityKey);
     localStorage.removeItem(cityKey);
     render();
 }
 
 function showSavingCities() {
     let sampleForCity = document.getElementById("pattern");
-    for (let city of CITYLIST.keys()) {
+    for (let city of setOfCity) {
         let newDiv = sampleForCity.cloneNode(true);
         newDiv.className = "task";
         newDiv.removeAttribute('Style');
@@ -67,7 +80,7 @@ function showSavingCities() {
 }
 
 
-function removeCityList() {
+function removeCity() {
     let allTasks = document.getElementsByClassName('task');
     for (let i = allTasks.length - 1; i >= 0; i--) {
         allTasks[i].remove();
@@ -75,25 +88,24 @@ function removeCityList() {
 }
 
 function render() {
-    removeCityList();
+    removeCity();
     showSavingCities();
 }
+
+function calcTime(time) {
+    let date = new Date(time * 1000);
+    return date.toLocaleTimeString();
+    //return date.getHours() + ':' + date.getMinutes();
+}
+
 function calcCelsiusTemp(temp) {
     return Math.round(temp - 273);
 }
 
-function saveFavoriteCities(input){
-
+function saveFavoriteCities(input) {
     const city = document.getElementById(input).textContent;
     alert("city - " + city);
-    localStorage.setItem('favoriteCity', city);
     localStorage.setItem(`keyCity${city}`, city);
-    for (let key in localStorage) {
-        console.log(key);
-    }
-    CITYLIST.set(city, '1');
-    for (let key in CITYLIST.keys()) {
-        console.log(key);
-    }
+    setOfCity.add(city);
     render();
 }
